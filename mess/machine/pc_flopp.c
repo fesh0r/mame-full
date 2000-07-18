6b1263d8721f1a1c9997e92220cf631394849da8
hjb <hjb>
2000-07-18 17:34:32 +0000

    Kev's version - removed mess/ from includes

--- mess/machine/pc_flopp.c
+++ mess/machine/pc_flopp.c
@@ -1,14 +1,15 @@
 /* PC floppy disc image handling code */
 #include "driver.h"
 #include "includes/pc_flopp.h"
+#include "includes/flopdrv.h"
 // temp to include floppy stuff
-#include "includes/nec765.h"
+//#include "includes/nec765.h"
 
 void *pc_fdc_file[2];			   /* up to two floppy disk images */
 UINT8 pc_fdc_heads[2] = {2,2};	   /* 2 heads */
 UINT8 pc_fdc_spt[2] = {9,9};	   /* 9 sectors per track */
-UINT8 pc_gpl[2] = {42,42};		   /* gap III length */
-UINT8 pc_fill[2] = {0xf6,0xf6};    /* filler byte */
+/*UINT8 pc_gpl[2] = {42,42};*/	   /* gap III length */
+/*UINT8 pc_fill[2] = {0xf6,0xf6}; */   /* filler byte */
 UINT8 pc_fdc_scl[2] = {2,2};	   /* 512 bytes per sector */
 
 //static UINT8 drv;				   /* drive number (0..3) */
@@ -18,13 +19,13 @@ static UINT8 pc_fdd_head[2] = {0,0};	   /* floppy current head numbers */
 static UINT8 sector[2] = {0,0};    /* floppy current sector numbers */
 static int offset[2];			   /* current seek() offset into disk images */
 
-//static int FDC_floppy[2] =			/* floppy drive status flags */
-//	{ FAULT, FAULT };
-
 static void pc_fdc_seek_callback(int drv, int track);
 static int pc_fdc_get_spt_callback(int drive, int side);
-static void pc_fdc_get_id_callback(int drive, nec765_id *id, int id_index, int side);
-static void pc_fdc_get_sector_data_callback(int drive, int id, int side, char **ptr);
+static void pc_fdc_get_id_callback(int drive, struct chrn_id *id, int id_index, int side);
+static void pc_fdc_format_sector_callback(int drive, int side, int,int c,int h, int r, int n, int filler);
+
+static void pc_fdc_read_sector_into_buffer(int drive, int side, int id, char *,int);
+static void pc_fdc_write_sector_from_buffer(int drive, int side, int id, char *,int);
 
 
 static floppy_interface pc_floppy_interface=
@@ -32,7 +33,9 @@ static floppy_interface pc_floppy_interface=
 	pc_fdc_seek_callback,
 	pc_fdc_get_spt_callback,
 	pc_fdc_get_id_callback,
-	pc_fdc_get_sector_data_callback,
+	pc_fdc_read_sector_into_buffer,
+	pc_fdc_write_sector_from_buffer,
+	pc_fdc_format_sector_callback
 };
 
 int pc_floppy_init(int id)
@@ -82,10 +85,9 @@ int pc_floppy_init(int id)
 			osd_fseek(pc_fdc_file[id], 0x01a, SEEK_SET);
 			osd_fread(pc_fdc_file[id], &pc_fdc_heads[id], 1);
 		}
+		floppy_drive_set_interface(id, &pc_floppy_interface);
 
-		floppy_drive_setup_drive_status(id); /* mark disk as inserted */
-		floppy_set_interface(id, &pc_floppy_interface);
-		floppy_drive_set_geometry(id, FLOPPY_DRIVE_DS_80);
+		floppy_drive_set_flag_state(id, FLOPPY_DRIVE_DISK_PRESENT,1);
 
 	}
 	return INIT_OK;
@@ -96,21 +98,22 @@ void pc_floppy_exit(int id)
 	if( pc_fdc_file[id] )
 		osd_fclose(pc_fdc_file[id]);
 	pc_fdc_file[id] = NULL;
+	floppy_drive_set_flag_state(id, FLOPPY_DRIVE_DISK_PRESENT,0);
 }
 
 /* called by nec765 when seek has been performed */
 static void pc_fdc_seek_callback(int drv, int track)
 {
-	void *f = pc_fdc_file[drv];
+//	void *f = pc_fdc_file[drv];
 	pc_fdd_track[drv] = track;
-	//pc_fdd_head[drv] = side;
-
-	offset[drv] = (pc_fdd_track[drv] * pc_fdc_heads[drv] + pc_fdd_head[drv]) * pc_fdc_spt[drv] * pc_fdc_scl[drv] * 256;
+//	//pc_fdd_head[drv] = side;
+//
+//	offset[drv] = (pc_fdd_track[drv] * pc_fdc_heads[drv] + pc_fdd_head[drv]) * pc_fdc_spt[drv] * pc_fdc_scl[drv] * 256;
 //	FDC_LOG(1,"FDC_seek_execute",(errorlog, "T:%02d H:%d $%08x\n", pc_fdd_track[drv], pc_fdd_head[drv], offset[drv]));
-	if (f)
-	{
-		osd_fseek(f, offset[drv], SEEK_SET);
-	}
+//	if (f) 
+//	{
+//		osd_fseek(f, offset[drv], SEEK_SET);
+//	}
 }
 
 
@@ -121,57 +124,67 @@ static int pc_fdc_get_spt_callback(int drive, int side)
 }
 
 /* temp */
-static void pc_fdc_get_id_callback(int drive, nec765_id *id, int id_index, int side)
+static void pc_fdc_get_id_callback(int drive, chrn_id *id, int id_index, int side)
 {
 	/* temp */
 	id->C = pc_fdd_track[drive];
 	id->H = side;
 	id->R = id_index+1;
 	id->N = 2;
+	id->data_id = id_index;
+	id->flags = 0;
 }
 
-/* 8k enough for a sector? */
-static char sector_buffer[8*1024];
-static int cached_drive, cached_side, cached_id;
-
-/* temp */
-//static void pc_fdc_get_sector_data_callback(int drive, int id, int side, char **ptr)
-static void pc_fdc_get_sector_data_callback(int drv, int id, int side, char **ptr)
+static void pc_fdc_read_sector_into_buffer(int drv, int side, int id, char *ptr, int size)
 {
 	void *f = pc_fdc_file[drv];
 
 	pc_fdd_head[drv] = side;
 	sector[drv] = id+1;
 
-	/* reading a different sector from a different drive or side? */
-#if 0
-	if (
-		((drive!=cached_drive) && (cached_drive!=-1)) ||
-		((side!=cached_side) && (cached_side!=-1)) ||
-		((id!=cached_id) && (cached_id!=-1)))
-	{
-		if (f)
-		{
-			/* write back buffer */
-			osd_fseek(f, offset[drv], SEEK_SET);
-			osd_fwrite(f, sector_buffer, 512 /* temp */);
-		}
-	}
-#endif
 	/* seek to new one and read */
 	offset[drv] = ((((pc_fdd_track[drv] * pc_fdc_heads[drv]) + pc_fdd_head[drv]) * pc_fdc_spt[drv]) + (sector[drv] - 1)) * pc_fdc_scl[drv] * 256;
 
-//	cached_drive = drive;
-	cached_drive = drv;
-	cached_id = id;
-	cached_side = side;
+	if (f) 
+	{
+		osd_fseek(f, offset[drv], SEEK_SET);
+        osd_fread(f, ptr, size);
+	}
+}
+
+static void pc_fdc_write_sector_from_buffer(int drv, int side, int id, char *ptr, int size)
+{
+	void *f = pc_fdc_file[drv];
+
+	pc_fdd_head[drv] = side;
+	sector[drv] = id+1;
+
+	/* seek to new one and read */
+	offset[drv] = ((((pc_fdd_track[drv] * pc_fdc_heads[drv]) + pc_fdd_head[drv]) * pc_fdc_spt[drv]) + (sector[drv] - 1)) * pc_fdc_scl[drv] * 256;
+
+	if (f) 
+	{
+		osd_fseek(f, offset[drv], SEEK_SET);
+        osd_fwrite(f, ptr, size);
+	}
+}
+
+
+static void pc_fdc_format_sector_callback(int drv, int side, int sector_index,int c,int h, int r, int n, int filler)
+{
+	void *f = pc_fdc_file[drv];
+
+	/* seek to new one and read */
+	pc_fdd_head[drv] = side;
+	sector[drv] = sector_index + 1;
+	offset[drv] = ((((pc_fdd_track[drv] * pc_fdc_heads[drv]) + pc_fdd_head[drv]) * pc_fdc_spt[drv]) + (sector[drv] - 1)) * pc_fdc_scl[drv] * 256;
 
 	if (f)
 	{
+		char sector_buffer[8192];
+
 		osd_fseek(f, offset[drv], SEEK_SET);
-        osd_fread(f, sector_buffer, 512 /* temp */);
+		memset(sector_buffer, filler, 1<<(n+7));
+		osd_fwrite(f, sector_buffer, 1<<(n+7));
 	}
-
-	*ptr = sector_buffer;
-
 }
