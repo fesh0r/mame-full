b2a531a0c77217d08fff1da039f8945d7890289a
PeT <PeT>
2000-08-06 08:05:40 +0000

    split some functions into mess/osd_dir.h for imgtool

--- src/osdepend.h
+++ src/osdepend.h
@@ -351,16 +351,9 @@ int osd_net_game_exit(void);
 #endif /* MAME_NET */
 
 #ifdef MESS
-int osd_num_devices(void);
-const char *osd_get_device_name(int i);
-void osd_change_device(const char *vol);
-void *osd_dir_open(const char *mdirname, const char *filemask);
-int osd_dir_get_entry(void *dir, char *name, int namelength, int *is_dir);
-void osd_dir_close(void *dir);
-extern void osd_change_directory(const char *);
-extern const char *osd_get_cwd(void);
+/* this is here to follow the current mame file hierachi style */
+#include "osd_dir.h"
 #endif
-
 
 void CLIB_DECL logerror(const char *text,...);
 
