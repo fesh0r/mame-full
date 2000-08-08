a74278e5ec3c232b184dde8ee30ef837ee26fc01
kevt <kevt>
2000-08-08 19:39:11 +0000

    updated z88 includes

--- mess/includes/z88.h
+++ mess/includes/z88.h
@@ -9,3 +9,21 @@ int z88_vh_start(void);
 void z88_vh_stop(void);
 void z88_vh_screenrefresh(struct osd_bitmap *bitmap, int full_refresh);
 void z88_init_palette(unsigned char *sys_palette, unsigned short *sys_colortable, const unsigned char *color_prom);
+
+struct blink_hw
+{
+	int pb[4];
+	int sbr;
+        int sbf;
+
+	int com;
+	int ints;
+	int sta;
+	int ack;
+	int tack;
+	int tmk;
+	int mem[4];
+	int tim[5];
+	int tsta;
+};
+
