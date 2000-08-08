e38d7e82360ce1693e69f75fe3cca079113b8acf
kevt <kevt>
2000-08-08 19:49:49 +0000

    updated video code. still not working yet.

--- mess/vidhrdw/z88.c
+++ mess/vidhrdw/z88.c
@@ -45,8 +45,77 @@ void z88_init_palette(unsigned char *sys_palette, unsigned short *sys_colortable
         memcpy(sys_colortable, z88_colour_table, sizeof (z88_colour_table));
 }
 
-extern int z88_display_memory_start;
-extern char *z88_memory;
+extern unsigned char *z88_memory;
+extern struct blink_hw blink;
+
+/* temp - change to gfxelement structure */
+
+static void z88_render_8x8(struct osd_bitmap *bitmap, int x, int y, unsigned char *pData)
+{
+        int h,b;
+        int pen0, pen1;
+
+        pen0 = Machine->pens[0];
+        pen1 = Machine->pens[1];
+
+        for (h=0; h<8; h++)
+        {
+            UINT8 data;
+
+            data = pData[y];
+            for (b=0; b<8; b++)
+            {
+                int pen;
+
+                if (data & 0x080)
+                {
+                  pen = pen1;
+                }
+                else
+                {
+                  pen = pen0;
+                }
+
+                plot_pixel(bitmap, x+b, y+h, pen);
+
+                data = data<<1;
+            }
+        }
+}
+
+static void z88_render_6x8(struct osd_bitmap *bitmap, int x, int y, unsigned char *pData)
+{
+        int h,b;
+        int pen0, pen1;
+
+        pen0 = Machine->pens[0];
+        pen1 = Machine->pens[1];
+
+        for (h=0; h<8; h++)
+        {
+            UINT8 data;
+
+            data = pData[y];
+            for (b=0; b<6; b++)
+            {
+                int pen;
+                if (data & 0x080)
+                {
+                  pen = pen1;
+                }
+                else
+                {
+                  pen = pen0;
+                }
+
+                plot_pixel(bitmap, x+1+b, y+h, pen);
+                data = data<<1;
+
+            }
+        }
+}
+
+
 
 /***************************************************************************
   Draw the game screen in the given osd_bitmap.
@@ -55,48 +124,26 @@ extern char *z88_memory;
 ***************************************************************************/
 void z88_vh_screenrefresh(struct osd_bitmap *bitmap, int full_refresh)
 {
-        int y;
-        int b;
-        int x;
-        int pen0, pen1;
+        int x,y;
+        unsigned char *ptr = z88_memory + blink.sbf;
 
-        pen0 = Machine->pens[0];
-        pen1 = Machine->pens[1];
-
-        for (y=0; y<Z88_SCREEN_HEIGHT; y++)
+        for (y=0; y<(Z88_SCREEN_HEIGHT>>3); y++)
         {
-                int by;
-                /* 64 bytes per line */
-#if 0
-                char *line_ptr = z88_memory + z88_display_memory_start + (y<<6);
-#else
-				char *line_ptr = NULL;	/* DOH! :-) */
-#endif
-
-                x = 0;
-                for (by=0; by<Z88_SCREEN_WIDTH>>3; by++)
+                for (x=0; x<(Z88_SCREEN_WIDTH>>3); x++)
                 {
-                        unsigned char byte;
-        
-                        byte = line_ptr[0];
-        
-                        for (b=0; b<8; b++)
-                        {
-                                if (byte & 0x080)
-                                {
-                                        plot_pixel(bitmap,x+b, y, pen1);
-                                }
-                                else
-                                {
-                                        plot_pixel(bitmap,x+b, y, pen0);
-        
-                                }
-                                byte = byte<<1;
-                        }
-        
-                        x = x + 8;
-                                        
-                        line_ptr = line_ptr+1;
+                  int char_index;
+
+                  UINT8 byte0, byte1;
+
+                  byte0 = ptr[(x<<1)];
+                  byte1 = ptr[(x<<2)];
+
+                  char_index = (byte0 & 0x0ff) | ((byte1 & 0x01)<<8);
+
+
+
                 }
-         }
+
+                ptr+=256;
+        }
 }
