5876c62cf1e18bdbe2115addefdca6c7eb317ee3
kevt <kevt>
2000-08-08 19:40:08 +0000

    avigo 100 includes. driver not enabled

---
+++ mess/includes/avigo.h
@@ -0,0 +1,15 @@
+#include "driver.h"
+#include "osdepend.h"
+#define AVIGO_NUM_COLOURS 2
+
+#define AVIGO_SCREEN_WIDTH        160
+#define AVIGO_SCREEN_HEIGHT       240
+
+READ_HANDLER(avigo_vid_memory_r);
+WRITE_HANDLER(avigo_vid_memory_w);
+
+
+int avigo_vh_start(void);
+void avigo_vh_stop(void);
+void avigo_vh_screenrefresh(struct osd_bitmap *bitmap, int full_refresh);
+void avigo_init_palette(unsigned char *sys_palette, unsigned short *sys_colortable, const unsigned char *color_prom);
