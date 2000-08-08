3b5fd2ee010f74b775b3e4f9de04746bb460123a
kevt <kevt>
2000-08-08 20:03:53 +0000

    removed. Now in src\sound. (has been since last time I checked in the makefile)

--- mess/includes/beep.h
+++
@@ -1,6 +0,0 @@
-int beep_sh_start(const struct MachineSound *msound);
-void beep_sh_stop(void);
-void beep_sh_update(void);
-void beep_set_state(int);
-void beep_set_frequency(int);
-void beep_set_volume(int);
