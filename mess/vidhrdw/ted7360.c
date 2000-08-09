5ef3cd87511d34c8cf48cf3bae3510e5d081c970
PeT <PeT>
2000-08-09 16:11:48 +0000

    another interrupt quitting

--- mess/vidhrdw/ted7360.c
+++ mess/vidhrdw/ted7360.c
@@ -884,6 +884,7 @@ READ_HANDLER ( ted7360_port_r )
 		break;
 	case 9:
 		val = ted7360[offset] | 1;
+		val |= 4; //rasterirq, lone07 seams to need this
 		break;
 	case 0xa:
 		val = ted7360[offset];
