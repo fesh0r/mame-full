adec148aac8515b33a1a3f8d4391ba0f1fc4ea6e
kevt <kevt>
2000-08-28 18:39:52 +0000

    updated for IO_FLOPPY device status and wd179x changes.

--- mess/systems/mbee.c
+++ mess/systems/mbee.c
@@ -28,8 +28,9 @@
 #include "driver.h"
 #include "machine/z80fmly.h"
 #include "vidhrdw/generic.h"
-#include "machine/wd179x.h"
+#include "includes/wd179x.h"
 #include "machine/mbee.h"
+#include "includes/basicdsk.h"
 
 #define VERBOSE 1
 
@@ -409,8 +410,8 @@ static const struct IODevice io_mbee[] = {
 		IO_CARTSLOT,		/* type */
 		1,					/* count */
 		"rom\0",            /* file extensions */
-		IO_RESET_ALL,		/* reset if file changed */
-        mbee_rom_id,        /* id */
+		NULL,				/* private */
+		mbee_rom_id,		/* id */
 		mbee_rom_load,		/* init */
 		NULL,				/* exit */
 		NULL,				/* info */
@@ -429,14 +430,14 @@ static const struct IODevice io_mbee[] = {
 		IO_FLOPPY,			/* type */
 		4,					/* count */
 		"dsk\0",            /* file extensions */
-		IO_RESET_NONE,		/* reset if file changed */
-        NULL,               /* id */
-		mbee_floppy_init,	/* init */
-		NULL,				/* exit */
+		NULL,				/* private */
+                basicdsk_floppy_id,                         /* id */
+                basicdsk_floppy_init,       /* init */
+                basicdsk_floppy_exit,                               /* exit */
 		NULL,				/* info */
 		NULL,				/* open */
 		NULL,				/* close */
-		NULL,				/* status */
+                floppy_status,                           /* status */
 		NULL,				/* seek */
 		NULL,				/* tell */
         NULL,               /* input */
