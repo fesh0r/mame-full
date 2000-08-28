d21074e77912209a513ce87c71c518b89d8da3a9
kevt <kevt>
2000-08-28 18:32:21 +0000

    updated z88 video hardware

--- mess/vidhrdw/z88.c
+++ mess/vidhrdw/z88.c
@@ -50,7 +50,7 @@ extern struct blink_hw blink;
 
 /* temp - change to gfxelement structure */
 
-static void z88_render_8x8(struct osd_bitmap *bitmap, int x, int y, unsigned char *pData)
+static void z88_vh_render_8x8(struct osd_bitmap *bitmap, int x, int y, unsigned char *pData)
 {
         int h,b;
         int pen0, pen1;
@@ -62,7 +62,7 @@ static void z88_render_8x8(struct osd_bitmap *bitmap, int x, int y, unsigned cha
         {
             UINT8 data;
 
-            data = pData[y];
+            data = pData[h];
             for (b=0; b<8; b++)
             {
                 int pen;
@@ -83,7 +83,7 @@ static void z88_render_8x8(struct osd_bitmap *bitmap, int x, int y, unsigned cha
         }
 }
 
-static void z88_render_6x8(struct osd_bitmap *bitmap, int x, int y, unsigned char *pData)
+static void z88_vh_render_6x8(struct osd_bitmap *bitmap, int x, int y, unsigned char *pData)
 {
         int h,b;
         int pen0, pen1;
@@ -95,7 +95,7 @@ static void z88_render_6x8(struct osd_bitmap *bitmap, int x, int y, unsigned cha
         {
             UINT8 data;
 
-            data = pData[y];
+            data = pData[h];
             for (b=0; b<6; b++)
             {
                 int pen;
@@ -115,7 +115,33 @@ static void z88_render_6x8(struct osd_bitmap *bitmap, int x, int y, unsigned cha
         }
 }
 
+static void z88_vh_render_line(struct osd_bitmap *bitmap, int x, int y,int pen)
+{
+        plot_pixel(bitmap, x, y+7, pen);
+        plot_pixel(bitmap, x+1, y+7, pen);
+        plot_pixel(bitmap, x+2, y+7, pen);
+        plot_pixel(bitmap, x+3, y+7, pen);
+        plot_pixel(bitmap, x+4, y+7, pen);
+        plot_pixel(bitmap, x+5, y+7, pen);
+        plot_pixel(bitmap, x+6, y+7, pen);
+        plot_pixel(bitmap, x+7, y+7, pen);
+}
 
+
+/* convert absolute offset into correct address to get data from */
+unsigned  char *z88_convert_address(unsigned long offset)
+{
+        return z88_memory;
+
+        if (offset>(32*16384))
+        {
+                return z88_memory + offset - (32*16384);
+        }
+        else
+        {
+                return memory_region(REGION_CPU1) + 0x010000 + offset;
+        }
+}
 
 /***************************************************************************
   Draw the game screen in the given osd_bitmap.
@@ -125,21 +151,81 @@ static void z88_render_6x8(struct osd_bitmap *bitmap, int x, int y, unsigned cha
 void z88_vh_screenrefresh(struct osd_bitmap *bitmap, int full_refresh)
 {
         int x,y;
-        unsigned char *ptr = z88_memory + blink.sbf;
+        unsigned char *ptr = z88_convert_address(blink.sbf);
 
         for (y=0; y<(Z88_SCREEN_HEIGHT>>3); y++)
         {
                 for (x=0; x<(Z88_SCREEN_WIDTH>>3); x++)
                 {
-                  int char_index;
+                  int ch;
 
                   UINT8 byte0, byte1;
 
                   byte0 = ptr[(x<<1)];
-                  byte1 = ptr[(x<<2)];
+                  byte1 = ptr[(x<<1)+1];
 
-                  char_index = (byte0 & 0x0ff) | ((byte1 & 0x01)<<8);
 
+
+			/* hi-res? */
+			if (byte1 & Z88_SCR_HW_HRS)
+			{
+				if (byte1 & Z88_SCR_HW_REV)
+				{
+				
+				}
+				else
+                                {       int ch_index;
+					unsigned char *pCharGfx;
+
+                                        ch = (byte0 & 0x0ff) | ((byte1 & 0x03)<<8);
+
+                                        if (ch & 0x0300)
+					{
+                                                ch_index =ch & (~0x0300);
+                                                pCharGfx = z88_convert_address(blink.hires1);
+					}
+					else
+					{
+                                                ch_index = ch;
+                                                pCharGfx = z88_convert_address(blink.hires0);
+					}
+
+                                        pCharGfx += (ch_index<<3);
+
+					z88_vh_render_8x8(bitmap, (x<<3),(y<<3), pCharGfx);
+				}
+			}
+			else
+			{
+				unsigned char *pCharGfx;
+                                int ch_index;
+
+                                ch = (byte0 & 0x0ff) | ((byte1 & 0x03)<<8);
+
+				if ((ch & 0x01c0)==0x01c0)
+				{
+                                   ch_index = ch & (~0x01c0);
+
+                                   pCharGfx = z88_convert_address(blink.lores0);
+				}
+				else
+				{
+                                   ch_index = ch;
+
+                                   pCharGfx = z88_convert_address(blink.lores1);
+				}
+
+                                pCharGfx += (ch_index<<3);
+
+				z88_vh_render_6x8(bitmap, (x<<3),(y<<3), pCharGfx);
+
+			}
+
+			/* underline? */
+			if (byte1 & Z88_SCR_HW_UND)
+			{
+                                z88_vh_render_line(bitmap, (x<<3), (y<<3)+7, 1);
+			}
 
 
                 }
