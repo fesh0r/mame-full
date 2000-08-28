b9428897e84c7fbfa867458c20c1cb37d784c9a0
kevt <kevt>
2000-08-28 18:03:55 +0000

    headers for enterprise driver merged and now in includes

---
+++ mess/includes/enterp.h
@@ -0,0 +1,13 @@
+void enterprise_init_machine(void);
+void enterprise_shutdown_machine(void);
+int enterprise_floppy_init(int);
+int enterprise_rom_load(int);
+int enterprise_rom_id(int);
+
+void Enterprise_Initialise(void);
+
+int enterprise_vh_start(void);
+void enterprise_vh_stop(void);
+void enterprise_vh_screenrefresh(struct osd_bitmap *bitmap, int fullupdate);
+
+
