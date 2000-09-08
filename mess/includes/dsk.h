06526dc8389f120c146954bae67abf35a8b5b87f
kevt <kevt>
2000-09-08 19:43:09 +0000

    updated so ddam can be specified for sector write.

--- mess/includes/dsk.h
+++ mess/includes/dsk.h
@@ -5,5 +5,5 @@ void dsk_seek_callback(int drive, int physical_track);
 void dsk_get_id_callback(int drive, struct chrn_id *id, int id_index, int side);
 int    dsk_get_sectors_per_track(int drive, int side);
 
-void dsk_write_sector_data_from_buffer(int drive, int sector_index, int side, char *ptr, int length);
+void dsk_write_sector_data_from_buffer(int drive, int sector_index, int side, char *ptr, int length, int ddam);
 void dsk_read_sector_data_into_buffer(int drive, int sector_index, int side, char *ptr, int length);
