584d8ff036a3a41247c7619cead758b590b621d8
kevt <kevt>
2000-09-08 19:36:04 +0000

    1. removed dir_sector and dir_length from set_geometry 2. fixed some indentation

--- mess/machine/bbc.c
+++ mess/machine/bbc.c
@@ -490,10 +490,11 @@ void	bbc_i8271_interrupt(int state)
 	previous_i8271_int_state = state;
 }
 
+
 static i8271_interface bbc_i8271_interface=
 {
 	bbc_i8271_interrupt,
-	NULL
+    NULL
 };
 
 
@@ -555,7 +556,7 @@ int bbc_floppy_init(int id)
 	{
 		/* sector id's 0-9 */
 		/* drive, tracks, heads, sectors per track, sector length, dir_sector, dir_length, first sector id */
-		basicdsk_set_geometry(id,80,1,10,256,12,3,0);
+		basicdsk_set_geometry(id,80,1,10,256,0);
 
 		return INIT_OK;
 	}
