c2e80597e42368df45b13f08786365ef66a82671
kevt <kevt>
2000-09-08 19:45:44 +0000

    fixed indentation.

--- mess/machine/pc_flopp.c
+++ mess/machine/pc_flopp.c
@@ -21,57 +21,57 @@ static int common_length_spt_heads[][3] = {
 
 int pc_floppy_init(int id)
 {
-        if (basicdsk_floppy_init(id)==INIT_OK)
-        {
-                int i;
-                int scl, spt,heads;
-
-                void *file;
-
-                file = image_fopen(IO_FLOPPY, id, OSD_FILETYPE_IMAGE_R, OSD_FOPEN_READ);
-
-        /* find the sectors/track and bytes/sector values in the boot sector */
-        if( file )
+	if (basicdsk_floppy_init(id)==INIT_OK)
 	{
-		int length;
+		int i;
+		int scl, spt,heads;
 
-		/* tracks pre sector recognition with image size
-		   works only 512 byte sectors! and 40 or 80 tracks*/
-                scl = heads = 2;
-                length=osd_fsize(file);
-		for( i = sizeof(common_length_spt_heads)/sizeof(common_length_spt_heads[0])-1; i >= 0; --i )
+		void *file;
+
+		file = image_fopen(IO_FLOPPY, id, OSD_FILETYPE_IMAGE_R, OSD_FOPEN_READ);
+
+		/* find the sectors/track and bytes/sector values in the boot sector */
+		if( file )
 		{
-			if( length == common_length_spt_heads[i][0] )
+			int length;
+
+			/* tracks pre sector recognition with image size
+			works only 512 byte sectors! and 40 or 80 tracks*/
+			scl = heads = 2;
+			length=osd_fsize(file);
+			for( i = sizeof(common_length_spt_heads)/sizeof(common_length_spt_heads[0])-1; i >= 0; --i )
 			{
-                                spt = common_length_spt_heads[i][1];
-                                heads = common_length_spt_heads[i][2];
-				break;
+				if( length == common_length_spt_heads[i][0] )
+				{
+					spt = common_length_spt_heads[i][1];
+					heads = common_length_spt_heads[i][2];
+					break;
+				}
 			}
-		}
-		if( i < 0 )
-		{
-			/*
-			 * get info from boot sector.
-			 * not correct on all disks
-			 */
-                        osd_fseek(file, 0x0c, SEEK_SET);
-                        osd_fread(file, &scl, 1);
-                        osd_fseek(file, 0x018, SEEK_SET);
-                        osd_fread(file, &spt, 1);
-                        osd_fseek(file, 0x01a, SEEK_SET);
-                        osd_fread(file, &heads, 1);
-		}
+			if( i < 0 )
+			{
+				/*
+				 * get info from boot sector.
+				 * not correct on all disks
+				 */
+				osd_fseek(file, 0x0c, SEEK_SET);
+				osd_fread(file, &scl, 1);
+				osd_fseek(file, 0x018, SEEK_SET);
+				osd_fread(file, &spt, 1);
+				osd_fseek(file, 0x01a, SEEK_SET);
+				osd_fread(file, &heads, 1);
+			}
 
-                basicdsk_set_geometry(id, 80, heads, spt, 512, spt+1, 2, 01);
+			basicdsk_set_geometry(id, 80, heads, spt, 512, 01);
 
-                return INIT_OK;
-        }
-        }
-        return INIT_FAILED;
+			return INIT_OK;
+		}
+	}
+	return INIT_FAILED;
 }
 
 void pc_floppy_exit(int id)
 {
-         basicdsk_floppy_exit(id);
+	 basicdsk_floppy_exit(id);
 }
 
