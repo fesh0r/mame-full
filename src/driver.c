7b70b7fb7260efd4c1908569ad8a1ea55380c162
hjb <hjb>
2000-09-13 16:16:09 +0000

    Made macross a TESTDRIVER() again as it is not working.

--- src/driver.c
+++ src/driver.c
@@ -2944,7 +2944,7 @@ TESTDRIVER( msgundam )
 
 	/* NMK games */
 	DRIVER( hachamf )	/* (c) 1991 NMK */
-	DRIVER( macross )	/* (c) 1992 Banpresto */
+TESTDRIVER( macross )	/* (c) 1992 Banpresto */
 	DRIVER( macross2 )	/* (c) 1993 Banpresto */
 	DRIVER( bjtwin )	/* (c) 1993 NMK */
 	DRIVER( nouryoku )	/* (c) 1995 Tecmo */
