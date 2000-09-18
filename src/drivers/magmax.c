c3ccaf60827aa00e40049e49144bf30a1f6f0f94
hjb <hjb>
2000-09-18 16:17:57 +0000

    About 32 % faster (on P200).
    Jarek
    
    FYI.
    10% was gained by adding a blit line function instead of calling
    plot_pixel() 256 times per line.
    
    Function simply copies a (variable length) buffer of pens data to the
    specified bitmap line (y), starting from variable pixel offset (x).
    All 8/16-bits ror/rol/flip variants are covered, so it is just a matter of
    copy&paste to put it into drawgfx.c if more drivers would benefit from it.

--- src/drivers/magmax.c
+++ src/drivers/magmax.c
@@ -9,7 +9,6 @@ Additional tweaking by Jarek Burczynski
 ***************************************************************************/
 
 #include "driver.h"
-#include "vidhrdw/generic.h"
 #include "cpu/m68000/m68000.h"
 
 void magmax_vh_convert_color_prom(unsigned char *palette, unsigned short *colortable, const unsigned char *color_prom);
@@ -20,6 +19,9 @@ void magmax_vh_stop(void);
 extern unsigned short magmax_vreg;
 extern unsigned char magmax_scroll_x[];
 extern unsigned char magmax_scroll_y[];
+extern unsigned char * magmax_videoram;
+extern unsigned char * magmax_spriteram;
+extern size_t magmax_spriteram_size;
 
 
 static void * scanline_timer;
@@ -52,6 +54,7 @@ WRITE_HANDLER( ay8910_portB_0_w )
 	if (LS74_clr == 0)
 		LS74_q = 0;
 }
+
 static void scanline_callback(int scanline)
 {
 	/* bit 0 goes hi whenever line V6 from video part goes lo->hi */
@@ -82,7 +85,7 @@ WRITE_HANDLER( ay8910_portA_0_w )
 * amplifier 3 <- AY2 CHB + AY2 CHC
 *
 * Each of the amps has its own analog cuircit:
-* amp0, amp1 and amp2 are different from each other amp3 is the same as amp2
+* amp0, amp1 and amp2 are different from each other; amp3 is the same as amp2
 *
 * Outputs of those amps are inputs to post amps, each having own cuircit
 * that is partially controlled by AY #0 port A.
@@ -92,7 +95,7 @@ WRITE_HANDLER( ay8910_portA_0_w )
 * PORT A BIT 3 - control postamp 3
 *
 * The "control" means assert/clear input pins on chip called 4066 (it is analog switch)
-* This is NOT implemented here.
+* This is not implemented here.
 */
 
 
@@ -107,13 +110,13 @@ static WRITE_HANDLER( magmax_write )
 	switch (offset)
 	{
 		case 0x00: /* VRAM CONTROL REGISTER */
-			// bit0 - coin counter 1
-			// bit1 - coin counter 2
-			// bit2 - flip screen (INV)
-			// bit3 - page bank to be displayed (PG)
-			// bit4 - sprite bank LSB (DP0)
-			// bit5 - sprite bank MSB (DP1)
-			// bit6 - BG display enable (BE)
+			/* bit0 - coin counter 1    */
+			/* bit1 - coin counter 2    */
+			/* bit2 - flip screen (INV) */
+			/* bit3 - page bank to be displayed (PG) */
+			/* bit4 - sprite bank LSB (DP0) */
+			/* bit5 - sprite bank MSB (DP1) */
+			/* bit6 - BG display enable (BE)*/
 			magmax_vreg = COMBINE_WORD(magmax_vreg, data);
 			break;
 
@@ -173,8 +176,8 @@ static struct MemoryWriteAddress magmax_writemem[] =
 {
 	{ 0x000000, 0x013fff, MWA_ROM },
 	{ 0x018000, 0x018fff, MWA_BANK1 },
-	{ 0x020000, 0x0207ff, MWA_BANK2, &videoram, &videoram_size },
-	{ 0x028000, 0x0281ff, MWA_BANK3, &spriteram, &spriteram_size },
+	{ 0x020000, 0x0207ff, MWA_BANK2, &magmax_videoram },
+	{ 0x028000, 0x0281ff, MWA_BANK3, &magmax_spriteram, &magmax_spriteram_size },
 	{ 0x030010, 0x03001f, magmax_write },
 	{ -1 }
 };
@@ -317,40 +320,23 @@ static struct GfxLayout spritelayout =
 	64*8
 };
 
-static struct GfxLayout tilelayout =
-{
-	8, 8,	/* 8*8 characters */
-	1024,	/* 1024 characters */
-	4,	/* 4 bits per pixel */
-	{ 0, 1, 2, 3 },
-	{ 4, 0, 12, 8, 20, 16, 28, 24 },
-	{ 0*32, 1*32, 2*32, 3*32, 4*32, 5*32, 6*32, 7*32 },
-	32*8
-};
-
 static struct GfxDecodeInfo gfxdecodeinfo[] =
 {
 	{ REGION_GFX1, 0, &charlayout,           0,  1 }, /*no color codes*/
 	{ REGION_GFX2, 0, &spritelayout,      1*16, 16 }, /*16 color codes*/
-	{ REGION_GFX3, 0, &tilelayout,  1*16+16*16,  1 }, /*not used by emulation, here only to be able to view the graphics with F4*/
 	{ -1 }
 };
 
-
-WRITE_HANDLER( ay8910_data_w )
-{
-// ?
-}
 
 static struct AY8910interface ay8910_interface =
 {
 	3,			/* 3 chips */
 	10000000/8,		/* 1.25 MHz */
 	{ 35, 35, 35 },
-	{ 0, 0, 0 }, //read port A
-	{ 0, 0, 0 }, //read port B
-	{ ay8910_portA_0_w, 0, 0 }, //ay8910_data_w, ay8910_data_w, ay8910_data_w }, //write port A
-	{ ay8910_portB_0_w, 0, 0 } //ay8910_data_w, ay8910_data_w, ay8910_data_w } //write port B
+	{ 0, 0, 0 }, /*read port A*/
+	{ 0, 0, 0 }, /*read port B*/
+	{ ay8910_portA_0_w, 0, 0 }, /*write port A*/
+	{ ay8910_portB_0_w, 0, 0 }  /*write port B*/
 };
 
 
@@ -358,7 +344,6 @@ int magmax_interrupt(void)
 {
 	return MC68000_IRQ_1;
 }
-
 
 static const struct MachineDriver machine_driver_magmax =
 {
@@ -383,7 +368,7 @@ static const struct MachineDriver machine_driver_magmax =
 	/* video hardware */
 	32*8, 32*8, { 0*8, 32*8-1, 2*8, 30*8-1 },
 	gfxdecodeinfo,
-	256, 1*16 + 16*16 + 1*16,
+	256, 1*16 + 16*16,
 	magmax_vh_convert_color_prom,
 
 	VIDEO_TYPE_RASTER,
@@ -404,7 +389,7 @@ static const struct MachineDriver machine_driver_magmax =
 
 
 ROM_START( magmax )
-	ROM_REGION( 0x14000, REGION_CPU1 ) /* main cpu code */
+	ROM_REGION( 0x14000, REGION_CPU1 ) /* 68000 (main) cpu code */
 	ROM_LOAD_ODD ( "1.3b", 0x00000, 0x4000, 0x33793cbb )
 	ROM_LOAD_EVEN( "6.3d", 0x00000, 0x4000, 0x677ef450 )
 	ROM_LOAD_ODD ( "2.5b", 0x08000, 0x4000, 0x1a0c84df )
@@ -412,7 +397,7 @@ ROM_START( magmax )
 	ROM_LOAD_ODD ( "3.6b", 0x10000, 0x2000, 0xd06e6cae )
 	ROM_LOAD_EVEN( "8.6d", 0x10000, 0x2000, 0x790a82be )
 
-	ROM_REGION( 0x10000, REGION_CPU2 ) /* sound cpu code */
+	ROM_REGION( 0x10000, REGION_CPU2 ) /* Z80 (sound) cpu code */
 	ROM_LOAD( "15.17b", 0x00000, 0x2000, 0x19e7b983 )
 	ROM_LOAD( "16.18b", 0x02000, 0x2000, 0x055e3126 )
 
@@ -427,21 +412,19 @@ ROM_START( magmax )
 	ROM_LOAD( "21.5g",  0x0a000, 0x2000, 0xdd52eda4 )
 	ROM_LOAD( "22.6g",  0x0c000, 0x2000, 0x4afc98ff )
 
-	ROM_REGION( 0x08000, REGION_GFX3 ) /* background tiles (data used at runtime so no REGIONFLAG_DISPOSE)*/
-	ROM_LOAD( "11.15f", 0x00000, 0x2000, 0x91f3edb6 ) /* surface */
-	ROM_LOAD( "12.17f", 0x02000, 0x2000, 0x99771eff ) /* underground */
-	ROM_LOAD( "13.18f", 0x04000, 0x2000, 0x75f30159 ) /* surface of mechanical level */
-	ROM_LOAD( "14.20f", 0x06000, 0x2000, 0x96babcba ) /* underground of mechanical level */
+	ROM_REGION( 0x10000, REGION_USER1 ) /* surface scroll control */
+	ROM_LOAD_GFX_EVEN( "4.18b",  0x00000, 0x2000, 0x1550942e )
+	ROM_LOAD_GFX_ODD ( "5.20b",  0x00000, 0x2000, 0x3b93017f )
+	/* BG control data */
+	ROM_LOAD( "9.18d",  0x04000, 0x2000, 0x9ecc9ab8 ) /* surface */
+	ROM_LOAD( "10.20d", 0x06000, 0x2000, 0xe2ff7293 ) /* underground */
+	/* background tiles */
+	ROM_LOAD( "11.15f", 0x08000, 0x2000, 0x91f3edb6 ) /* surface */
+	ROM_LOAD( "12.17f", 0x0a000, 0x2000, 0x99771eff ) /* underground */
+	ROM_LOAD( "13.18f", 0x0c000, 0x2000, 0x75f30159 ) /* surface of mechanical level */
+	ROM_LOAD( "14.20f", 0x0e000, 0x2000, 0x96babcba ) /* underground of mechanical level */
 
-	ROM_REGION( 0x4000, REGION_USER1 ) /* surface scroll control */
-	ROM_LOAD( "4.18b",  0x00000, 0x2000, 0x1550942e )
-	ROM_LOAD( "5.20b",  0x02000, 0x2000, 0x3b93017f )
-
-	ROM_REGION( 0x4000, REGION_USER2 ) /* BG control data */
-	ROM_LOAD( "9.18d",  0x00000, 0x2000, 0x9ecc9ab8 ) /* surface */
-	ROM_LOAD( "10.20d", 0x02000, 0x2000, 0xe2ff7293 ) /* underground */
-
-	ROM_REGION( 0x0200, REGION_USER3 ) /* BG control data */
+	ROM_REGION( 0x0200, REGION_USER2 ) /* BG control data */
 	ROM_LOAD( "mag_b.14d",  0x0000, 0x0100, 0xa0fb7297 ) /* background control PROM */
 	ROM_LOAD( "mag_c.15d",  0x0100, 0x0100, 0xd84a6f78 ) /* background control PROM */
 
