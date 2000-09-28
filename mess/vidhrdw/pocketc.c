b982a16b766ab4aa4cf673b8db8b81e950f012c8
PeT <PeT>
2000-09-28 16:20:47 +0000

    moved pc1401,pc1350,pc1251 specific parts into own files

--- mess/vidhrdw/pocketc.c
+++ mess/vidhrdw/pocketc.c
@@ -4,20 +4,6 @@
 
 #include "includes/pocketc.h"
 
-/* pc140x
-   16 5x7 with space between char
-   6000 .. 6027, 6067.. 6040
-  603c: 3 STAT
-  603d: 0 BUSY, 1 DEF, 2 SHIFT, 3 HYP, 4 PRO, 5 RUN, 6 CAL
-  607c: 0 E, 1 M, 2 (), 3 RAD, 4 G, 5 DE, 6 PRINT */
-
-/* pc1421
-   16 5x7 with space between char
-   6000 .. 6027, 6067.. 6040
-  603c: 3 RUN
-  603d: 0 BUSY, 1 DEF, 2 SHIFT, 3 BGN, 4 STAT, 5 FIN, 6 PRINT
-  607c: 0 E, 1 M, 2 BAL, 3 INT, 4 PRN, 5 Sum-Sign, 6 PRO */
-
 /* PC126x
    24x2 5x7 space between char
    2000 .. 203b, 2800 .. 283b
@@ -25,9 +11,9 @@
   203d: 0 BUSY, 1 PRINT, 3 JAPAN, 4 SMALL, 5 SHIFT, 6 DEF
   207c: 1 DEF 1 RAD 2 GRAD 5 ERROR 6 FLAG */
 
-static struct artwork_info *backdrop;
+struct artwork_info *pocketc_backdrop;
 
-unsigned char pc1401_palette[248][3] =
+unsigned char pocketc_palette[248][3] =
 {
 	{ 99,107,99 },
 
@@ -42,7 +28,7 @@ unsigned char pc1401_palette[248][3] =
 	{ 0, 0, 0 }
 };
 
-unsigned short pc1401_colortable[8][2] = {
+unsigned short pocketc_colortable[8][2] = {
 	{ 0, 4 },
 	{ 0, 4 },
 	{ 0, 4 },
@@ -60,19 +46,19 @@ void pocketc_init_colors (unsigned char *sys_palette,
 	char backdrop_name[200];
 	int used=8;
 
-	memcpy (sys_palette, pc1401_palette, sizeof (pc1401_palette));
-	memcpy(sys_colortable,pc1401_colortable,sizeof(pc1401_colortable));
+	memcpy (sys_palette, pocketc_palette, sizeof (pocketc_palette));
+	memcpy(sys_colortable,pocketc_colortable,sizeof(pocketc_colortable));
 
     /* try to load a backdrop for the machine */
     sprintf (backdrop_name, "%s.png", Machine->gamedrv->name);
 
-    artwork_load (&backdrop, backdrop_name, used, Machine->drv->total_colors - used);
+    artwork_load (&pocketc_backdrop, backdrop_name, used, Machine->drv->total_colors - used);
 
-	if (backdrop)
+	if (pocketc_backdrop)
     {
         logerror("backdrop %s successfully loaded\n", backdrop_name);
-        memcpy (&sys_palette[used * 3], backdrop->orig_palette, 
-				backdrop->num_pens_used * 3 * sizeof (unsigned char));
+        memcpy (&sys_palette[used * 3], pocketc_backdrop->orig_palette, 
+				pocketc_backdrop->num_pens_used * 3 * sizeof (unsigned char));
     }
     else
     {
@@ -88,145 +74,22 @@ int pocketc_vh_start(void)
 	if (!videoram)
         return 1;
 
-    if (backdrop)
-        backdrop_refresh (backdrop);
+    if (pocketc_backdrop)
+        backdrop_refresh (pocketc_backdrop);
 
 	return generic_vh_start();
 }
 
 void pocketc_vh_stop(void)
 {
-    if (backdrop)
-        artwork_free (&backdrop);
+    if (pocketc_backdrop)
+        artwork_free (&pocketc_backdrop);
 
 	generic_vh_stop();
 }
 
-static struct {
-	UINT8 reg[0x100];
-} pc1401_lcd;
-
-READ_HANDLER(pc1401_lcd_read)
-{
-	offset&=0xff;
-	return pc1401_lcd.reg[offset];
-}
-
-WRITE_HANDLER(pc1401_lcd_write)
-{
-	offset&=0xff;
-	pc1401_lcd.reg[offset]=data;
-}
-
-typedef char *FIGURE[];
-
-static const FIGURE	 line={ /* simple line */
-	"11111",
-	"11111",
-	"11111e" 
-};
-static const FIGURE busy={ 
-	"11  1 1  11 1 1",
-	"1 1 1 1 1   1 1",
-	"11  1 1  1  1 1",
-	"1 1 1 1   1  1",
-	"11   1  11   1e"
-}, def={ 
-	"11  111 111",
-	"1 1 1   1",
-	"1 1 111 11",
-	"1 1 1   1",
-	"11  111 1e" 
-}, shift={
-	" 11 1 1 1 111 111",
-	"1   1 1 1 1    1",
-	" 1  111 1 11   1",
-	"  1 1 1 1 1    1",
-	"11  1 1 1 1    1e" 
-}, hyp={
-	"1 1 1 1 11",
-	"1 1 1 1 1 1",
-	"111 1 1 11",
-	"1 1  1  1",
-	"1 1  1  1e" 
-}, de={
-	"11  111",
-	"1 1 1",
-	"1 1 111",
-	"1 1 1",
-	"11  111e"
-}, g={
-	" 11",
-	"1",
-	"1 1",
-	"1 1",
-	" 11e" 
-}, rad={
-	"11   1  11",
-	"1 1 1 1 1 1",
-	"11  111 1 1",
-	"1 1 1 1 1 1",
-	"1 1 1 1 11e"
-}, braces={
-	" 1 1",
-	"1   1",
-	"1   1",
-	"1   1",
-	" 1 1e"
-}, m={
-	"1   1",
-	"11 11",
-	"1 1 1",
-	"1   1",
-	"1   1e"
-}, e={
-	"111",
-	"1",
-	"111",
-	"1",
-	"111e" 
-}, run={ 
-	"11  1 1 1  1",
-	"1 1 1 1 11 1",
-	"11  1 1 1 11",
-	"1 1 1 1 1  1",
-	"1 1  1  1  1e" 
-}, pro={ 
-	"11  11   1  ",
-	"1 1 1 1 1 1",
-	"11  11  1 1",
-	"1   1 1 1 1",
-	"1   1 1  1e" 
-}, japan={ 
-	"  1  1  11   1  1  1",
-	"  1 1 1 1 1 1 1 11 1",
-	"  1 111 11  111 1 11",
-	"1 1 1 1 1   1 1 1  1",
-	" 1  1 1 1   1 1 1  1e" 
-}, sml={ 
-	" 11 1 1 1",
-	"1   111 1",
-	" 1  1 1 1",
-	"  1 1 1 1",
-	"11  1 1 111e" 
-}, rsv={ 
-	"11   11 1   1",
-	"1 1 1   1   1",
-	"11   1   1 1",
-	"1 1   1  1 1",
-	"1 1 11    1e" 
-};
-
-/* size in reality
- 170x72.5 mm
-
- lcd 16 character 5x7 with 1 column between
-
- lcd upper left at 32.5x15mm
-
-*/
-static void pc1401_draw_special(struct osd_bitmap *bitmap,
-								int x, int y, const FIGURE fig, int color)
+void pocketc_draw_special(struct osd_bitmap *bitmap,
+						  int x, int y, const POCKETC_FIGURE fig, int color)
 {
 	int i,j;
 	for (i=0;fig[i];i++,y++) {
@@ -242,218 +105,3 @@ static void pc1401_draw_special(struct osd_bitmap *bitmap,
 	}
 }
 
-#define DOWN 57
-#define RIGHT 114
-void pc1401_vh_screenrefresh (struct osd_bitmap *bitmap, int full_refresh)
-{
-	int x, y, i, j;
-	int color[2];
-	/* HJB: we cannot initialize array with values from other arrays, thus... */
-    color[0] = Machine->pens[pc1401_colortable[CONTRAST][0]];
-	color[1] = Machine->pens[pc1401_colortable[CONTRAST][1]];
-
-    if (backdrop)
-        copybitmap (bitmap, backdrop->artwork, 0, 0, 0, 0, NULL, 
-					TRANSPARENCY_NONE, 0);
-	else
-		fillbitmap (bitmap, Machine->pens[0], &Machine->visible_area);
-
-	for (x=RIGHT,y=DOWN,i=0; i<0x28;x+=2) {
-		for (j=0; j<5;j++,i++,x+=2)
-			drawgfx(bitmap, Machine->gfx[CONTRAST], pc1401_lcd.reg[i],
-					0,0,0,
-					x,y,
-					0, TRANSPARENCY_NONE,0);
-	}
-	for (i=0x67; i>=0x40;x+=2) {
-		for (j=0; j<5;j++,i--,x+=2)
-			drawgfx(bitmap, Machine->gfx[CONTRAST], pc1401_lcd.reg[i],0,0,0,
-					x,y,
-					0, TRANSPARENCY_NONE,0);
-	}
-	pc1401_draw_special(bitmap,RIGHT+149,DOWN+24,line,
-						pc1401_lcd.reg[0x3c]&8?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT,DOWN-10,busy,
-						pc1401_lcd.reg[0x3d]&1?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT+18,DOWN-10,def,
-						pc1401_lcd.reg[0x3d]&2?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT+43,DOWN-10,shift,
-						pc1401_lcd.reg[0x3d]&4?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT+63,DOWN-10,hyp,
-						pc1401_lcd.reg[0x3d]&8?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT+38,DOWN+24,line,
-						pc1401_lcd.reg[0x3d]&0x10?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT+23,DOWN+24,line,
-						pc1401_lcd.reg[0x3d]&0x20?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT+8,DOWN+24,line,
-						pc1401_lcd.reg[0x3d]&0x40?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT+183,DOWN-10,e,
-						pc1401_lcd.reg[0x7c]&1?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT+176,DOWN-10,m,
-						pc1401_lcd.reg[0x7c]&2?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT+168,DOWN-10,braces,
-						pc1401_lcd.reg[0x7c]&4?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT+138,DOWN-10,rad,
-						pc1401_lcd.reg[0x7c]&8?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT+134,DOWN-10,g,
-						pc1401_lcd.reg[0x7c]&0x10?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT+126,DOWN-10,de,
-						pc1401_lcd.reg[0x7c]&0x20?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT+165,DOWN+24,line,
-						pc1401_lcd.reg[0x7c]&0x40?color[1]:color[0]);
-
-/*
-  603c: 3 STAT
-  603d: 0 BUSY, 1 DEF, 2 SHIFT, 3 HYP, 4 PRO, 5 RUN, 6 CAL
-  607c: 0 E, 1 M, 2 (), 3 RAD, 4 G, 5 DE, 6 PRINT
-*/
-}
-
-static struct {
-	UINT8 reg[0x100];
-} pc1251_lcd;
-
-READ_HANDLER(pc1251_lcd_read)
-{
-	int data;
-	data=pc1251_lcd.reg[offset&0xff];
-	logerror("pc1251 read %.3x %.2x\n",offset,data);
-	return data;
-}
-
-WRITE_HANDLER(pc1251_lcd_write)
-{
-	logerror("pc1251 write %.3x %.2x\n",offset,data);
-	pc1251_lcd.reg[offset&0xff]=data;
-}
-
-#undef DOWN
-#define DOWN 62
-#undef RIGHT
-#define RIGHT 68
-void pc1251_vh_screenrefresh (struct osd_bitmap *bitmap, int full_refresh)
-{
-	int x, y, i, j;
-	int color[2];
-	/* HJB: we cannot initialize array with values from other arrays, thus... */
-    color[0] = Machine->pens[pc1401_colortable[PC1251_CONTRAST][0]];
-	color[1] = Machine->pens[pc1401_colortable[PC1251_CONTRAST][1]];
-
-    if (backdrop)
-		copybitmap (bitmap, backdrop->artwork, 0, 0, 0, 0, NULL,
-					TRANSPARENCY_NONE, 0);
-	else
-		fillbitmap (bitmap, Machine->pens[0], &Machine->visible_area);
-
-	for (x=RIGHT,y=DOWN,i=0; i<60;x+=3) {
-		for (j=0; j<5;j++,i++,x+=3)
-			drawgfx(bitmap, Machine->gfx[PC1251_CONTRAST], pc1251_lcd.reg[i],
-					0,0,0,
-					x,y,
-					0, TRANSPARENCY_NONE,0);
-	}
-	for (i=0x7b; i>=0x40;x+=3) {
-		for (j=0; j<5;j++,i--,x+=3)
-			drawgfx(bitmap, Machine->gfx[PC1251_CONTRAST], pc1251_lcd.reg[i],
-					0,0,0,
-					x,y,
-					0, TRANSPARENCY_NONE,0);
-	}
-	pc1401_draw_special(bitmap,RIGHT+134,DOWN-10,de,
-						pc1251_lcd.reg[0x3c]&8?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT+142,DOWN-10,g,
-						pc1251_lcd.reg[0x3c]&4?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT+146,DOWN-10,rad,
-						pc1251_lcd.reg[0x3d]&4?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT+18,DOWN-10,def,
-						pc1251_lcd.reg[0x3c]&1?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT,DOWN-10,shift,
-						pc1251_lcd.reg[0x3d]&2?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT+38,DOWN-10,pro,
-						pc1251_lcd.reg[0x3e]&1?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT+53,DOWN-10,run,
-						pc1251_lcd.reg[0x3e]&2?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT+68,DOWN-10,rsv,
-						pc1251_lcd.reg[0x3e]&4?color[1]:color[0]);
-
-	/* 0x3c 1 def?, 4 g, 8 de
-	   0x3d 2 shift, 4 rad, 8 error
-	   0x3e 1 pro?, 2 run?, 4rsv?*/
-}
-
-static struct {
-	UINT8 reg[0x1000];
-} pc1350_lcd;
-
-READ_HANDLER(pc1350_lcd_read)
-{
-	int data;
-	data=pc1350_lcd.reg[offset&0xfff];
-	logerror("pc1350 read %.3x %.2x\n",offset,data);
-	return data;
-}
-
-WRITE_HANDLER(pc1350_lcd_write)
-{
-	logerror("pc1350 write %.3x %.2x\n",offset,data);
-	pc1350_lcd.reg[offset&0xfff]=data;
-}
-
-int pc1350_keyboard_line_r(void)
-{
-	return pc1350_lcd.reg[0xe00];
-}
-
-/* pc1350
-   24x4 5x8 no space between chars
-   7000 .. 701d, 7200..721d, 7400 ..741d, 7600 ..761d, 7800 .. 781d
-   7040 .. 705d, 7240..725d, 7440 ..745d, 7640 ..765d, 7840 .. 785d
-   701e .. 703b, 721e..723b, 741e ..743b, 761e ..763b, 781e .. 783b
-   705e .. 707b, 725e..727b, 745e ..747b, 765e ..767b, 785e .. 787b
-   783c: 0 SHIFT 1 DEF 4 RUN 5 PRO 6 JAPAN 7 SML */
-static int pc1350_addr[4]={ 0, 0x40, 0x1e, 0x5e };
-
-#undef DOWN
-#define DOWN 45
-#undef RIGHT
-#define RIGHT 76
-void pc1350_vh_screenrefresh (struct osd_bitmap *bitmap, int full_refresh)
-{
-	int x, y, i, j, k;
-	int color[2];
-	/* HJB: we cannot initialize array with values from other arrays, thus... */
-    color[0] = Machine->pens[pc1401_colortable[PC1350_CONTRAST][0]];
-	color[1] = Machine->pens[pc1401_colortable[PC1350_CONTRAST][1]];
-
-    if (backdrop)
-		copybitmap (bitmap, backdrop->artwork, 0, 0, 0, 0, NULL,
-					TRANSPARENCY_NONE, 0);
-	else
-		fillbitmap (bitmap, Machine->pens[0], &Machine->visible_area);
-
-	for (k=0, y=DOWN; k<4; y+=16,k++) {
-		for (x=RIGHT, i=pc1350_addr[k]; i<0xa00; i+=0x200) {
-			for (j=0; j<=0x1d; j++, x+=2) {
-				drawgfx(bitmap, Machine->gfx[PC1350_CONTRAST], 
-						pc1350_lcd.reg[j+i],
-						0,0,0,
-						x,y,
-						0, TRANSPARENCY_NONE,0);
-			}
-		}
-	}
-	/* 783c: 0 SHIFT 1 DEF 4 RUN 5 PRO 6 JAPAN 7 SML */
-	/* I don't know how they really look like in the lcd */
-	pc1401_draw_special(bitmap,RIGHT-30,DOWN+45,shift,
-						pc1350_lcd.reg[0x83c]&1?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT-30,DOWN+55,def,
-						pc1350_lcd.reg[0x83c]&2?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT-30,DOWN+5,run,
-						pc1350_lcd.reg[0x83c]&0x10?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT-30,DOWN+15,pro,
-						pc1350_lcd.reg[0x83c]&0x20?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT-30,DOWN+25,japan,
-						pc1350_lcd.reg[0x83c]&0x40?color[1]:color[0]);
-	pc1401_draw_special(bitmap,RIGHT-30,DOWN+35,sml,
-						pc1350_lcd.reg[0x83c]&0x80?color[1]:color[0]);
-}
