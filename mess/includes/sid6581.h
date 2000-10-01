22373601559a6009fbbeb77ca9a7eed77c736466
PeT <PeT>
2000-10-01 13:51:01 +0000

    sid inferface change

--- mess/includes/sid6581.h
+++ mess/includes/sid6581.h
@@ -3,42 +3,49 @@
 
 #include "driver.h"
 
-/* c64 / c128 sound interface
+/* 
+   c64 / c128 sound interface
    cbmb series interface
    c16 sound card
    c64 hardware modification for second sid
    c65 has 2 inside
 
-   selection between two type 6581 8580 would be nice */
+   selection between two type 6581 8580 would be nice 
 
-extern void sid6581_0_init (int (*paddle) (int offset), int pal);
-extern void sid6581_1_init (int (*paddle) (int offset), int pal);
+   sid6582 6581 with other input voltages 
+*/
+#define MAX_SID6581 2
 
 typedef enum { MOS6581, MOS8580 } SIDTYPE;
-extern void sid6581_0_configure (SIDTYPE type);
-extern void sid6581_1_configure (SIDTYPE type);
+typedef struct {
+	/* this is here, until this sound approximation is added to
+	   mame's sound devices */
+	struct CustomSound_interface custom;
 
-extern void sid6581_0_reset (void);
-extern void sid6581_1_reset (void);
+	int count;
+	struct {
+		/* bypassed to mixer_allocate_channel, so use
+		   the macros defined in src/sound/mixer.h to load values*/
+		int default_mixer_level;
+		SIDTYPE type;
+		int clock;
+		int (*ad_read)(int index);
+	} chips[MAX_SID6581];
+} SID6581_interface;
+
+extern void sid6581_set_type(int number, SIDTYPE type);
+
+extern void sid6581_reset (int number);
+
+extern void sid6581_update(void);
 
 extern WRITE_HANDLER ( sid6581_0_port_w );
 extern WRITE_HANDLER ( sid6581_1_port_w );
 extern READ_HANDLER  ( sid6581_0_port_r );
 extern READ_HANDLER  ( sid6581_1_port_r );
 
-extern void sid6581_update(void);
-
-extern struct CustomSound_interface sid6581_sound_interface;
-
-/* private area */
-typedef struct {
-	int (*paddle_read) (int offset);
-	int reg[0x20];
-	bool sidKeysOn[0x20], sidKeysOff[0x20];
-} SID6581;
-
-extern SID6581 sid6581[2];
-
-UINT16 sid6581_read_word(SID6581 *This, int offset);
+int sid6581_custom_start (const struct MachineSound *driver);
+void sid6581_custom_stop(void);
+void sid6581_custom_update(void);
 
 #endif
