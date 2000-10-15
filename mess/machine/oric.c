d1cf2006d30896e8f4a78d15085d7b18c19a4326
kevt <kevt>
2000-10-15 20:37:33 +0000

    updated for oric telestrat

--- mess/machine/oric.c
+++ mess/machine/oric.c
@@ -19,12 +19,35 @@
 #include "machine/6522via.h"
 #include "includes/mfmdisk.h"
 
+/* ==0 if oric1 or oric atmos, !=0 if telestrat */
+static int oric_is_telestrat;
+
+/* This does not exist in the real hardware. I have used it to
+know which sources are interrupting */
+/* bit 2 = telestrat 2nd via interrupt, 1 = microdisc interface,
+0 = oric 1st via interrupt */
+static unsigned char oric_irqs;
 enum
 {
 	ORIC_FLOPPY_NONE,
 	ORIC_FLOPPY_MFM_DISK,
 	ORIC_FLOPPY_BASIC_DISK
 };
+
+/* called when ints are changed - cleared/set */
+static void oric_refresh_ints(void)
+{
+
+	/* any irq set? */
+	if ((oric_irqs & 0x07)!=0)
+	{
+		cpu_set_irq_line(0,0, HOLD_LINE);
+	}
+	else
+	{
+		cpu_set_irq_line(0,0, CLEAR_LINE);
+	}
+}
 
 static int oric_floppy_type[4] = {ORIC_FLOPPY_NONE, ORIC_FLOPPY_NONE, ORIC_FLOPPY_NONE, ORIC_FLOPPY_NONE};
 
@@ -282,16 +305,19 @@ WRITE_HANDLER ( oric_via_out_cb2_func )
 	oric_psg_connection_refresh();
 }
 
+
 void	oric_via_irq_func(int state)
 {
+	oric_irqs &= ~(1<<0);
+
 	if (state)
 	{
-		cpu_set_irq_line(0,0, HOLD_LINE);
+		logerror("oric via1 interrupt\n");
+
+		oric_irqs |=(1<<0);
 	}
-	else
-	{
-		cpu_set_irq_line(0,0, CLEAR_LINE);
-	}
+
+	oric_refresh_ints();
 }
 
 
@@ -353,12 +379,6 @@ struct via6522_interface oric_6522_interface=
 	NULL
 };
 
-
-/* if defined, enable microdisc floppy disc interface */
-/* if not defined, enable Jasmin floppy disc interface */
-#define MICRODISC_FLOPPY_DISC_INTERFACE
-
-
 /* following are used for tape and need cleaning up */
 int oric_interrupt_counter;
 int oric_countdown_1;
@@ -373,7 +393,7 @@ char oric_rom_version[40];
 /*static const char *rom_name = NULL; */
 
 
-#ifdef MICRODISC_FLOPPY_DISC_INTERFACE
+/* MICRODISC INTERFACE variables */
 /* bit 7 is intrq state */
 unsigned char port_314_r;
 /* bit 7 is drq state (active low) */
@@ -383,80 +403,65 @@ unsigned char port_318_r;
 /* bit 3: double density enable */
 /* bit 0: enable FDC IRQ to trigger IRQ on CPU */
 unsigned char port_314_w;
-#else
-unsigned char port_3fa_w;
-unsigned char port_3fb_w;
-#endif
-
 
 static unsigned char oric_wd179x_int_state = 0;
 
-#ifdef MICRODISC_FLOPPY_DISC_INTERFACE
-static void oric_refresh_wd179x_ints(void)
+
+static void oric_microdisc_refresh_wd179x_ints(void)
 {
+	oric_irqs &=~(1<<1);
+
 	if ((oric_wd179x_int_state) && (port_314_w & (1<<0)))
 	{
-		cpu_set_irq_line(0,0,HOLD_LINE);
-	}
-	else
-	{
-		/* this might cause problems with via?? */
-		cpu_set_irq_line(0,0,CLEAR_LINE);
+		logerror("oric microdisc interrupt\n");
+
+		oric_irqs |=(1<<1);
 	}
 
+	oric_refresh_ints();
 }
-#endif
 
-static void oric_wd179x_callback(int State)
+static void oric_microdisc_wd179x_callback(int State)
 {
 	switch (State)
 	{
 		case WD179X_IRQ_CLR:
 		{
-#ifdef MICRODISC_FLOPPY_DISC_INTERFACE
 			port_314_r |=(1<<7);
 
 			oric_wd179x_int_state = 0;
 
-			oric_refresh_wd179x_ints();
-#endif
+			oric_microdisc_refresh_wd179x_ints();
 		}
 		break;
 
 		case WD179X_IRQ_SET:
 		{
-#ifdef MICRODISC_FLOPPY_DISC_INTERFACE
 			port_314_r &= ~(1<<7);
 
 			oric_wd179x_int_state = 1;
 
-			oric_refresh_wd179x_ints();
-#endif
+			oric_microdisc_refresh_wd179x_ints();
 		}
 		break;
 
 		case WD179X_DRQ_CLR:
 		{
-#ifdef MICRODISC_FLOPPY_DISC_INTERFACE
 			port_318_r |= (1<<7);
-#else
-			/* on Jasmin interface, DRQ is connected to IRQ on CPU */
-			cpu_set_irq_line(0,0, CLEAR_LINE);
-#endif
 		}
 		break;
 
 		case WD179X_DRQ_SET:
 		{
-#ifdef MICRODISC_FLOPPY_DISC_INTERFACE
 			port_318_r &=~(1<<7);
-#else
-			/* on Jasmin interface, DRQ is connected to IRQ on CPU */
-			cpu_set_irq_line(0,0, HOLD_LINE);
-#endif
 		}
 		break;
 	}
+}
+
+static void oric_wd179x_callback(int State)
+{
+	oric_microdisc_wd179x_callback(State);
 }
 
 int oric_floppy_init(int id)
@@ -523,9 +528,10 @@ int		oric_floppy_id(int id)
 	return basicdsk_floppy_id(id);
 }
 
-void	oric_set_mem_0x0c000(void)
+void	oric_microdisc_set_mem_0x0c000(void)
 {
-#ifdef MICRODISC_FLOPPY_DISC_INTERFACE
+	if (oric_is_telestrat)
+		return;
 
 	/* for 0x0c000-0x0dfff: */
 	/* if os disabled, ram takes priority */
@@ -588,71 +594,18 @@ void	oric_set_mem_0x0c000(void)
 			cpu_setbank(5, oric_ram_0x0c000+0x02000);
 		}
 	}
-#else
-	/* /ROMDIS */
-	if ((port_3fb_w & (1<<0))!=0)
-	{
-		/* rom disabled enable ram */
-		cpu_setbankhandler_r(1, MRA_BANK1);
-		cpu_setbankhandler_r(2, MRA_BANK2);
-		cpu_setbankhandler_w(5, MWA_BANK5);
-		cpu_setbankhandler_w(6, MWA_BANK6);
-		cpu_setbank(1, oric_ram_0x0c000);
-		cpu_setbank(2, oric_ram_0x0c000+0x02000);
-		cpu_setbank(5, oric_ram_0x0c000);
-		cpu_setbank(6, oric_ram_0x0c000+0x02000);
-
-/*		logerror("select ram\n"); */
-	}
-	else
-	{
-		unsigned char *rom_ptr;
-		/* ROM */
-
-		/* writes are ignored */
-		cpu_setbankhandler_w(5, MWA_NOP);
-		cpu_setbankhandler_w(6, MWA_NOP);
-		cpu_setbankhandler_r(1, MRA_BANK1);
-		cpu_setbankhandler_r(2, MRA_BANK2);
-
-		if ((port_3fa_w & (1<<0))==0)
-		{
-/*			logerror("Select disk rom\n"); */
-			/* enable rom of microdisc interface */
-			rom_ptr = memory_region(REGION_CPU1) + 0x014000;
-			cpu_setbank(2, rom_ptr);
-			cpu_setbank(1, rom_ptr);
-
-		}
-		else
-		{
-/*			logerror("Select OS rom\n"); */
-			/* basic rom */
-			rom_ptr = memory_region(REGION_CPU1) + 0x010000;
-			cpu_setbank(1, rom_ptr);
-			cpu_setbank(2, rom_ptr+0x02000);
-		}
-
-	}
-#endif
 }
 
-void oric_init_machine (void)
+void oric_common_init_machine(void)
 {
+    /* clear all irqs */
+	oric_irqs = 0;
 
-	oric_ram_0x0c000 = malloc(16384);
-
-#ifdef MICRODISC_FLOPPY_DISC_INTERFACE
+	oric_ram_0x0c000 = NULL;
+	
 	/* disable os rom, enable microdisc rom */
 	/* 0x0c000-0x0dfff will be ram, 0x0e000-0x0ffff will be microdisc rom */
-	port_314_w = 0x0ff^((1<<7)|(1<<0)|(1<<1));
-	oric_set_mem_0x0c000();
-#else
-	port_3fa_w = 0;
-	port_3fb_w = 0;
-	oric_set_mem_0x0c000();
-#endif
-
+    port_314_w = 0x0ff^((1<<7) | (1<<1));
 
 
 #if 0
@@ -745,6 +698,21 @@ void oric_init_machine (void)
 	wd179x_init(oric_wd179x_callback);
 }
 
+
+void oric_init_machine (void)
+{
+	oric_common_init_machine();
+
+	oric_is_telestrat = 0;
+
+	oric_ram_0x0c000 = malloc(16384);
+
+	/* disable os rom, enable microdisc rom */
+	/* 0x0c000-0x0dfff will be ram, 0x0e000-0x0ffff will be microdisc rom */
+    port_314_w = 0x0ff^((1<<7) | (1<<1));
+	oric_microdisc_set_mem_0x0c000();
+}
+
 void oric_shutdown_machine (void)
 {
 	if (oric_ram_0x0c000)
@@ -753,20 +721,12 @@ void oric_shutdown_machine (void)
 	wd179x_exit();
 }
 
-
-READ_HANDLER ( oric_IO_r )
+READ_HANDLER (oric_microdisc_r)
 {
 	unsigned char data = 0x0ff;
 
-	if (offset<0x010)
-	{
-		/* it is repeated 16 times */
-		data = via_0_r(offset & 0x0f);
-	}
-
 	switch (offset & 0x0ff)
 	{
-#ifdef MICRODISC_FLOPPY_DISC_INTERFACE
 		/* microdisc floppy disc interface */
 		case 0x010:
 			data = wd179x_status_r(0);
@@ -788,24 +748,79 @@ READ_HANDLER ( oric_IO_r )
 			data = port_318_r | 0x07f;
 /*			logerror("port_318_r: %02x\n",data); */
 			break;
-#else
-		/* Jasmin floppy disc interface */
-		case 0x0f4:
-			data = wd179x_status_r(0);
+
+		default:
+			data = via_0_r(offset & 0x0f);
 			break;
-		case 0x0f5:
-			data = wd179x_track_r(0);
-			break;
-		case 0x0f6:
-			data = wd179x_sector_r(0);
-			break;
-		case 0x0f7:
-			data = wd179x_data_r(0);
-			break;
-#endif
+
 	}
 
 	return data;
+}
+
+WRITE_HANDLER(oric_microdisc_w)
+{
+	switch (offset & 0x0ff)
+	{
+		/* microdisc floppy disc interface */
+		case 0x010:
+			wd179x_command_w(0,data);
+			break;
+		case 0x011:
+			wd179x_track_w(0,data);
+			break;
+		case 0x012:
+			wd179x_sector_w(0,data);
+			break;
+		case 0x013:
+			wd179x_data_w(0,data);
+			break;
+		case 0x014:
+		{
+			int density;
+
+			port_314_w = data;
+
+/*			logerror("port_314_w: %02x\n",data); */
+
+			/* bit 6,5: drive */
+			/* bit 4: side */
+			/* bit 3: double density enable */
+			/* bit 0: enable FDC IRQ to trigger IRQ on CPU */
+			wd179x_set_drive((data>>5) & 0x03);
+			wd179x_set_side((data>>4) & 0x01);
+			if (data & (1<<3))
+			{
+				density = DEN_MFM_LO;
+			}
+			else
+			{
+				density = DEN_FM_HI;
+			}
+
+			wd179x_set_density(density);
+	
+			oric_microdisc_set_mem_0x0c000();
+			oric_microdisc_refresh_wd179x_ints();
+		}
+		break;		
+	
+		default:
+			via_0_w(offset & 0x0f, data);
+			break;
+	}
+}
+
+
+READ_HANDLER ( oric_IO_r )
+{
+	if ((offset>=0x010) && (offset<=0x01f))
+	{
+		return oric_microdisc_r(offset);
+	}
+
+	/* it is repeated */
+	return via_0_r(offset & 0x0f);
 }
 
 WRITE_HANDLER ( oric_IO_w )
@@ -857,103 +872,14 @@ WRITE_HANDLER ( oric_IO_w )
 		}
 	}
 
-	if (offset<0x010)
+
+	if ((offset>=0x010) && (offset<=0x01f))
 	{
-		/* it is repeated 16 times, but for now only use first few entries */
-		via_0_w(offset & 0x0f,data);
+		oric_microdisc_w(offset, data);
+		return;
 	}
-	switch (offset & 0x0ff)
-	{
-#ifdef MICRODISC_FLOPPY_DISC_INTERFACE
-		/* microdisc floppy disc interface */
-		case 0x010:
-			wd179x_command_w(0,data);
-			break;
-		case 0x011:
-			wd179x_track_w(0,data);
-			break;
-		case 0x012:
-			wd179x_sector_w(0,data);
-			break;
-		case 0x013:
-			wd179x_data_w(0,data);
-			break;
-		case 0x014:
-		{
-			int density;
 
-			port_314_w = data;
-
-/*			logerror("port_314_w: %02x\n",data); */
-
-			/* bit 6,5: drive */
-			/* bit 4: side */
-			/* bit 3: double density enable */
-			/* bit 0: enable FDC IRQ to trigger IRQ on CPU */
-			wd179x_set_drive((data>>6) & 0x03);
-			wd179x_set_side((data>>4) & 0x01);
-			if (data & (1<<3))
-			{
-				density = DEN_MFM_LO;
-			}
-			else
-			{
-				density = DEN_FM_HI;
-			}
-
-			wd179x_set_density(density);
-
-			oric_set_mem_0x0c000();
-			oric_refresh_wd179x_ints();
-		}
-		break;
-#else
-		/* Jasmin floppy disc interface */
-		case 0x0f4:
-			wd179x_command_w(0);
-			break;
-		case 0x0f5:
-			wd179x_track_w(0);
-			break;
-		case 0x0f6:
-			wd179x_sector_w(0);
-			break;
-		case 0x0f7:
-			wd179x_data_w(0);
-			break;
-		case 0x0f8:
-		{
-			/* bit 0: side select */
-			wd179x_set_side(data & 0x01);
-		}
-		break;
-
-		/* bit 0 = overlay ram access 1 = overlay ram enabled */
-		case 0x0fa:
-		{
-			port_0x03fa_w = data;
-		}
-		break;
-
-		/* bit 0 = romdis, internal basic rom disabled */
-		case 0x0fb:
-		{
-			port_0x03fb_w = data;
-		}
-		break;
-
-
-
-		/* port 0x03fc selects drive 0, port 0x03fd selects drive 1.. */
-		case 0x0fc:
-		case 0x0fd:
-		case 0x0fe:
-		case 0x0ff:
-			wd179x_set_drive(offset & 0x03);
-			break;
-#endif
-
-	}
+	via_0_w(offset & 0x0f,data);
 }
 
 #if 0
@@ -1215,3 +1141,285 @@ void oric_cassette_exit(int id)
 {
 	device_close(IO_CASSETTE, id);
 }
+
+
+/**** TELESTRAT ****/
+
+/*
+VIA lines
+ Telestrat usage
+ 
+PA0..PA2
+ Memory bank selection
+ 
+PA3
+ "Midi" port pin 3
+ 
+PA4
+ RS232/Minitel selection
+ 
+PA5
+ Third mouse button (right joystick port pin 5)
+ 
+PA6
+ "Midi" port pin 5
+ 
+PA7
+ Second mouse button (right joystick port pin 9)
+ 
+CA1
+ "Midi" port pin 1
+ 
+CA2
+ not used ?
+ 
+PB0..PB4
+ Joystick ports
+ 
+PB5
+ Joystick doubler switch
+ 
+PB6
+ Select Left Joystick port
+ 
+PB7
+ Select Right Joystick port
+ 
+CB1
+ Phone Ring detection
+ 
+CB2
+ "Midi" port pin 4
+ 
+*/ 
+
+static unsigned char telestrat_bank_selection;
+static unsigned char telestrat_via2_port_b_data;
+
+enum
+{
+	TELESTRAT_MEM_BLOCK_UNDEFINED,
+	TELESTRAT_MEM_BLOCK_RAM,
+	TELESTRAT_MEM_BLOCK_ROM
+};
+
+struct  telestrat_mem_block
+{
+	int		MemType;
+	unsigned char *ptr;
+};
+
+
+static struct telestrat_mem_block	telestrat_blocks[8];
+
+static void	telestrat_refresh_mem(void)
+{
+	struct telestrat_mem_block *mem_block = &telestrat_blocks[telestrat_bank_selection];
+
+	switch (mem_block->MemType)
+	{
+		case TELESTRAT_MEM_BLOCK_RAM:
+		{
+			cpu_setbankhandler_r(1,MRA_BANK1);
+			cpu_setbankhandler_w(2,MWA_BANK2);
+			cpu_setbank(1, mem_block->ptr);
+			cpu_setbank(2, mem_block->ptr);
+		}
+		break;
+
+		case TELESTRAT_MEM_BLOCK_ROM:
+		{
+			cpu_setbankhandler_r(1,MRA_BANK1);
+			cpu_setbankhandler_w(2,MWA_NOP);
+			cpu_setbank(1, mem_block->ptr);
+		}
+		break;
+
+		default:
+		case TELESTRAT_MEM_BLOCK_UNDEFINED:
+		{
+			cpu_setbankhandler_r(1, MRA_NOP);
+			cpu_setbankhandler_w(2, MWA_NOP);
+		}
+		break;
+	}
+}
+
+
+static WRITE_HANDLER(telestrat_via2_out_a_func)
+{
+	telestrat_bank_selection = data & 0x07;
+
+	telestrat_refresh_mem();
+}
+
+static READ_HANDLER(telestrat_via2_in_b_func)
+{
+	unsigned char data = 0x01f;
+
+	/* left joystick selected? */
+	if (telestrat_via2_port_b_data & (1<<6))
+	{
+		data &= readinputport(8);
+	}
+
+	/* right joystick selected? */
+	if (telestrat_via2_port_b_data & (1<<7))
+	{
+		data &= readinputport(9);
+	}
+
+	data |= telestrat_via2_port_b_data & ((1<<7) | (1<<6) | (1<<5));
+
+	return data;
+}
+
+static WRITE_HANDLER(telestrat_via2_out_b_func)
+{
+	telestrat_via2_port_b_data = data;
+}
+
+
+void	telestrat_via2_irq_func(int state)
+{
+    oric_irqs &=~(1<<2);
+
+	if (state)
+	{
+        logerror("telestrat via2 interrupt\n");
+        
+        oric_irqs |=(1<<2);
+	}
+
+    oric_refresh_ints();
+}
+struct via6522_interface telestrat_via2_interface=
+{
+	NULL,
+	telestrat_via2_in_b_func,
+	NULL,				
+	NULL,
+	NULL,
+	NULL,
+	telestrat_via2_out_a_func,
+	telestrat_via2_out_b_func,
+	NULL,
+	NULL,
+	telestrat_via2_irq_func,
+	NULL,
+	NULL,
+	NULL,
+	NULL
+};
+
+
+void telestrat_init_machine(void)
+{
+	oric_common_init_machine();
+
+	oric_is_telestrat = 1;
+
+	/* initialise overlay ram */
+	telestrat_blocks[0].MemType = TELESTRAT_MEM_BLOCK_RAM;
+	telestrat_blocks[0].ptr = (unsigned char *)malloc(16384);
+
+	telestrat_blocks[1].MemType = TELESTRAT_MEM_BLOCK_UNDEFINED;
+	telestrat_blocks[2].MemType = TELESTRAT_MEM_BLOCK_UNDEFINED;
+	
+	/* initialise default cartridge */
+	telestrat_blocks[3].MemType = TELESTRAT_MEM_BLOCK_ROM;
+	telestrat_blocks[3].ptr = memory_region(REGION_CPU1)+0x010000;
+
+	telestrat_blocks[4].MemType = TELESTRAT_MEM_BLOCK_UNDEFINED;
+	
+	/* initialise default cartridge */
+	telestrat_blocks[5].MemType = TELESTRAT_MEM_BLOCK_ROM;
+	telestrat_blocks[5].ptr = memory_region(REGION_CPU1)+0x014000;
+
+	/* initialise default cartridge */
+	telestrat_blocks[6].MemType = TELESTRAT_MEM_BLOCK_ROM;
+	telestrat_blocks[6].ptr = memory_region(REGION_CPU1)+0x018000;
+
+	/* initialise default cartridge */
+	telestrat_blocks[7].MemType = TELESTRAT_MEM_BLOCK_ROM;
+	telestrat_blocks[7].ptr = memory_region(REGION_CPU1)+0x01c000;
+
+	telestrat_bank_selection = 7;
+	telestrat_refresh_mem();
+
+
+	via_config(1, &telestrat_via2_interface);
+	via_set_clock(1,1000000);
+	via_reset();
+}
+
+void	telestrat_shutdown_machine(void)
+{
+	int i;
+
+	/* this supports different cartridges now. In the init machine
+	a cartridge is hard-coded, but if other cartridges exist it could
+	be changed above */
+	for (i=0; i<8; i++)
+	{
+		if (telestrat_blocks[i].MemType == TELESTRAT_MEM_BLOCK_RAM)
+		{
+			if (telestrat_blocks[i].ptr!=NULL)
+			{
+				free(telestrat_blocks[i].ptr);
+				telestrat_blocks[i].ptr = NULL;
+			}
+		}
+	}
+
+	oric_shutdown_machine();
+}
+
+
+WRITE_HANDLER(telestrat_IO_w)
+{
+	if (offset<0x010)
+	{
+		/* it is repeated 16 times, but for now only use first few entries */
+		via_0_w(offset & 0x0f,data);
+		return;
+	}
+
+	if ((offset>=0x020) && (offset<=0x02f)) 
+	{
+		via_1_w(offset,data);
+		return;
+	}
+
+	if ((offset>=0x010) && (offset<=0x01f))
+	{
+		oric_microdisc_w(offset, data);
+		return;
+	}
+}
+
+READ_HANDLER(telestrat_IO_r)
+{
+	unsigned char data = 0x0ff;
+
+	if (offset<0x010)
+	{
+		/* it is repeated 16 times, but for now only use first few entries */
+		return via_0_r(offset & 0x0f);
+	}
+
+	if ((offset>=0x020) && (offset<=0x02f))
+	{
+		return via_1_r(offset);
+	}
+
+	if ((offset>=0x010) && (offset<=0x01f))
+	{
+		return oric_microdisc_r(offset);
+	}
+
+	return data;
+}
+
+
+
