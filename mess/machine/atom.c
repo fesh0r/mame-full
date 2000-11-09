5c5e91a2dc05532e8ec45d765d2db0d798f21778
kevt <kevt>
2000-11-09 20:42:47 +0000

    Updated. Floppy disc emulation working. 6522 now implemented. start of tape emulation (disabled for now) start of printer emulation (disabled for now).

--- mess/machine/atom.c
+++ mess/machine/atom.c
@@ -5,16 +5,99 @@
 	Functions to emulate general aspects of the machine (RAM, ROM,
 	interrupts, I/O ports)
 
+  TODO:
+	- Complete printer emulation
+	- Test cassette emulation
+	- 2.4khz timer correct?
+
 ***********************************************************************/
 
 #include "driver.h"
-#include "cpu/z80/z80.h"
 #include "machine/8255ppi.h"
 #include "vidhrdw/m6847.h"
 #include "includes/atom.h"
 #include "includes/i8271.h"
 #include "includes/basicdsk.h"
 #include "includes/flopdrv.h"
+#include "machine/6522via.h"
+
+/* printer data written */
+static char printer_data;
+/* 2.4khz timer state */
+static void *atom_timer;
+
+/* I am not sure if this is correct, the atom appears to have a 2.4Khz timer used for reading tapes?? */
+static int	timer_state = 0;
+
+static void atom_via_irq_func(int state)
+{
+	if (state)
+	{
+		cpu_set_irq_line(0,0, HOLD_LINE);
+	}
+	else
+	{
+		cpu_set_irq_line(0,0, CLEAR_LINE);
+	}
+
+
+}
+
+/* printer status */
+READ_HANDLER(atom_via_in_a_func)
+{
+	if (device_status(IO_PRINTER,0,0))
+	{
+		/* online */
+		return 0x07f;
+	}
+
+
+	return 0x0ff;
+}
+
+/* printer data */
+WRITE_HANDLER(atom_via_out_a_func)
+{
+	/* atom has a 7-bit printer port */
+	printer_data = data & 0x07f;
+
+	/* data out */
+
+	/* pcr of 6522 used to control printer */
+}
+
+struct via6522_interface atom_6522_interface=
+{
+	atom_via_in_a_func,
+	NULL,
+	NULL,			
+	NULL,
+	NULL,
+	NULL,
+	atom_via_out_a_func,
+	NULL,
+	NULL,
+	NULL,
+	atom_via_irq_func,
+	NULL,
+	NULL,
+	NULL,
+	NULL
+};
+
+READ_HANDLER(atom_via_r)
+{
+	return via_0_r(offset & 0x0f);
+}
+
+WRITE_HANDLER(atom_via_w)
+{
+	via_0_w(offset & 0x0f, data);
+
+}
+
+
 
 static	ppi8255_interface	atom_8255_int =
 {
@@ -65,6 +148,20 @@ static struct i8271_interface atom_8271_interface=
 	NULL
 };
 
+
+static void atom_timer_callback(int dummy)
+{
+	/* change timer state */
+	timer_state^=1;
+
+	/* Enable 2.4 kHz to cassette output */
+	if (atom_8255.atom_8255_portc & (1<<1))
+	{
+		device_output(IO_CASSETTE, 0, (timer_state) ? -32768 : 32767);
+	}
+
+}
+
 void atom_init_machine(void)
 {
 	ppi8255_init (&atom_8255_int);
@@ -73,10 +170,21 @@ void atom_init_machine(void)
 	atom_8255.atom_8255_portc = 0xff;
 
 	i8271_init(&atom_8271_interface);
+
+	via_config(0, &atom_6522_interface);
+	via_set_clock(0,1000000);
+	via_reset();
+
+	previous_i8271_int_state = 0;
+	timer_state = 0;
+	atom_timer = timer_pulse(TIME_IN_HZ(2400*2), 0, atom_timer_callback);
 }
 
 void atom_stop_machine(void)
 {
+	if (atom_timer)
+		timer_remove(atom_timer);
+
 	i8271_stop();
 }
 
@@ -176,7 +284,20 @@ int atom_8255_portc_r (int offset )
 {
 
 /* | */
-	atom_8255.atom_8255_portc &= 0x3f;
+	atom_8255.atom_8255_portc &= 0x0f;
+
+	/* cassette input */
+	if (device_input(IO_CASSETTE,0)>255)
+	{
+		atom_8255.atom_8255_portc |= (1<<5);
+	}
+
+	/* 2.4khz input */
+	if (timer_state)
+	{
+		atom_8255.atom_8255_portc |= (1<<4);
+	}
+
 	atom_8255.atom_8255_portc |= (readinputport (0) & 0x80);
 	atom_8255.atom_8255_portc |= (readinputport (12) & 0x40);
 	/* logerror("8255: Read port c (%02X)\n",atom_8255.atom_8255_portc); */
@@ -223,20 +344,23 @@ void atom_8255_porta_w (int offset, int data)
 		}
 	}
 	atom_8255.atom_8255_porta = data;
-	logerror("8255: Write port a, %02x\n", data);
+/*	logerror("8255: Write port a, %02x\n", data); */
 }
 
 void atom_8255_portb_w (int offset, int data)
 {
 	atom_8255.atom_8255_portb = data;
-	logerror("8255: Write port b, %02x\n", data);
+/*	logerror("8255: Write port b, %02x\n", data); */
 }
 
 void atom_8255_portc_w (int offset, int data)
 {
 	atom_8255.atom_8255_portc = data;
 	speaker_level_w(0, (data & 0x04) >> 2);
-	logerror("8255: Write port c, %02x\n", data);
+
+	/* tape output */
+	device_output(IO_CASSETTE, 0, (data & 0x01) ? -32768 : 32767);
+/*	logerror("8255: Write port c, %02x\n", data); */
 }
 
 
@@ -277,5 +401,45 @@ WRITE_HANDLER(atom_8271_w)
 		default:
 			break;
 	}
+}
+
+
+int atom_cassette_init(int id)
+{
+	void *file;
+
+	file = image_fopen(IO_CASSETTE, id, OSD_FILETYPE_IMAGE_RW, OSD_FOPEN_READ);
+	if (file)
+	{
+		struct wave_args wa = {0,};
+		wa.file = file;
+		wa.display = 1;
+
+		if (device_open(IO_CASSETTE, id, 0, &wa))
+			return INIT_FAILED;
+
+		return INIT_OK;
+	}
+
+	/* HJB 02/18: no file, create a new file instead */
+	file = image_fopen(IO_CASSETTE, id, OSD_FILETYPE_IMAGE_RW, OSD_FOPEN_WRITE);
+	if (file)
+	{
+		struct wave_args wa = {0,};
+		wa.file = file;
+		wa.display = 1;
+		wa.smpfreq = 22050; /* maybe 11025 Hz would be sufficient? */
+		/* open in write mode */
+        if (device_open(IO_CASSETTE, id, 1, &wa))
+            return INIT_FAILED;
+		return INIT_OK;
+    }
+
+	return INIT_FAILED;
+}
+
+void atom_cassette_exit(int id)
+{
+	device_close(IO_CASSETTE, id);
 }
 
