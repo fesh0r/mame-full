11a4970e14a93ca57c3104d14610279f2bfd5ec1
sean <sean>
2000-11-13 18:03:37 +0000

    Unix-ifed.

--- src/unix/osdutils.h
+++ src/unix/osdutils.h
@@ -1,10 +1,10 @@
-#include <direct.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <unistd.h>
 
-#define strcmpi		stricmp
-#define strncmpi	strnicmp
+#define strcmpi		strcasecmp
+#define strncmpi	strncasecmp
 
-#define osd_mkdir(dir)	mkdir(dir)
-#define PATH_SEPARATOR	'\\'
+#define osd_mkdir(dir)	mkdir(dir, 0)
+#define PATH_SEPARATOR	'/'
