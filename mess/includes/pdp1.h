169f55e663f0991de02e1d6d05f83bf494ef08d7
rnabet <rnabet>
2000-11-15 21:27:53 +0000

    Updated for b9.

--- mess/includes/pdp1.h
+++ mess/includes/pdp1.h
@@ -3,10 +3,8 @@
 #define READ18_HANDLER(name) READ32_HANDLER(name)
 #define WRITE18_HANDLER(name) WRITE32_HANDLER(name)
 
-#define MEMORY_READ_START18(name) const struct Memory_ReadAddress32 name[] = { \
-	{ MEMORY_MARKER, MEMORY_DIRECTION_READ | MEMORY_TYPE_MEM | MEMORY_WIDTH_32 },
-#define MEMORY_WRITE_START18(name) const struct Memory_WriteAddress32 name[] = { \
-	{ MEMORY_MARKER, MEMORY_DIRECTION_WRITE | MEMORY_TYPE_MEM | MEMORY_WIDTH_32 },
+#define MEMORY_READ_START18(name) MEMORY_READ32_START(name)
+#define MEMORY_WRITE_START18(name) MEMORY_WRITE32_START(name)
 
 /* From machine/pdp1.c */
 int pdp1_load_rom (int id);
