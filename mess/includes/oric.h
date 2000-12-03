36ef349a99738627c14333dd7b42c1004a102def
kevt <kevt>
2000-12-03 09:48:19 +0000

    changed printer emulation. removed some old code.

--- mess/includes/oric.h
+++ mess/includes/oric.h
@@ -2,7 +2,6 @@
 
 void oric_init_machine (void);
 void oric_shutdown_machine (void);
-int oric_load_rom (int id);
 int oric_interrupt(void);
 READ_HANDLER ( oric_IO_r );
 WRITE_HANDLER ( oric_IO_w );
@@ -12,15 +11,6 @@ void oric_vh_screenrefresh(struct osd_bitmap *bitmap, int full_refresh);
 int oric_vh_start(void);
 void oric_vh_stop(void);
 
-extern unsigned char *oric_IO;
-
-
-/* from vidhrdw */
-void oric_set_powerscreen_mode (int mode);
-void oric_set_flash_show (int mode);
-
-int oric_extract_file_from_tape (int filenum);
-
 extern WRITE_HANDLER(oric_psg_porta_write);
 
 int	oric_floppy_init(int id);
@@ -28,12 +18,9 @@ int oric_floppy_id(int id);
 void oric_floppy_exit(int id);
 
 void oric_vh_stop(void);
-void oric_init_char_attrs(void);
-
 
 extern int oric_cassette_init(int id);
 extern void oric_cassette_exit(int id);
-
 
 /* Telestrat specific */
 void	telestrat_init_machine(void);
