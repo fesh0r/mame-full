bbd558afef1c90f9d4304e65ab4111289d23ea4e
kevt <kevt>
2000-12-03 09:47:43 +0000

    changed printer emulation. Not working yet. some small bug-fixes.

--- mess/machine/oric.c
+++ mess/machine/oric.c
@@ -9,6 +9,11 @@
   Thankyou to Fabrice Frances for his ORIC documentation which helped with this driver
   http://oric.ifrance.com/oric/
 
+  TODO:
+   - there are problems loading some .wav's. Try to fix these.
+   - fix more graphics display problems
+   - check the printer works
+   - fix more disc drive/wd179x problems so more software will run
 */
 
 #include <stdio.h>
@@ -19,6 +24,8 @@
 #include "machine/6522via.h"
 #include "includes/mfmdisk.h"
 #include "includes/6551.h"
+#include "includes/centroni.h"
+#include "printer.h"
 
 /* timer used to refresh via cb input, which will trigger ints on pulses
 from tape */
@@ -41,10 +48,13 @@ enum
 /* called when ints are changed - cleared/set */
 static void oric_refresh_ints(void)
 {
-	/* if microdisc interface is disabled, do not allow interrupts from it */
-	if ((readinputport(9) & 0x01)==0)
+	if (oric_is_telestrat==0)
 	{
-		oric_irqs &=~(1<<1);
+		/* if microdisc interface is disabled, do not allow interrupts from it */
+		if ((readinputport(9) & 0x01)==0)
+		{
+			oric_irqs &=~(1<<1);
+		}
 	}
 
 	/* any irq set? */
@@ -60,42 +70,6 @@ static void oric_refresh_ints(void)
 
 static int oric_floppy_type[4] = {ORIC_FLOPPY_NONE, ORIC_FLOPPY_NONE, ORIC_FLOPPY_NONE, ORIC_FLOPPY_NONE};
 
-/*
-
-TODO's / BUGLIST
-
- ** done ** Multipart loads from .TAP file  (cload patched, look for next file / filename match
- ** done ** CLOAD"" loads next file from tape image if there are any, else hang on 'Searching....'
-cloads, check for autorun flag, ... to run, or not ... (need .TAP specification ...)
-HIRES attrs in mid/screen   (split hires modes ...)
- ** done ** HIRES colour attributes need attention ...
- ** done ** Inverse ATTRS's .. check behaviour of colours
-fix 'RUN' command for cloads to 0x0501 ... (works for cloads, not for command line yet ..
-FIX any broken keyboard mappings ....
-fix ROM patch to work on 1.1 roms, extraction of filename for cloads etc ..
- ** done ** set the timing of the IRQ's correctly (is set to 60hz, should be 100hz)
-introduce a pause after loading individual tape files to simulate effect of loading screens ...
-
-switch from HIRES to TEXT.. does not alter internal variables ..
-the ULTRA does not run,  does this need 6522 timers?
-RATSPLAT .. no colission detection?  timers?  other forms of IRQ??
-emulate 6522 timers properly ...
-NMI for reset button (map to f12 or something ..
-
-low priority,
-
-add Inport_ports structures so key info menu in mess GUI shows keys
-loading tape image to update MESS GUI settings for current image
-
-  KT:
-	Added preliminary floppy disc support from Fabrice Frances document.
-	rewrote code to use 6522 via.
-
-  TODO:
-  - Microdisc interface: ROMDIS and Eprom select
-  - Jasmin: overlay ram access, ROMDIS, fdc reset
-
-*/
 static	char *oric_ram_0x0c000;
 
 
@@ -109,7 +83,7 @@ static char oric_keyboard_mask;
 
 static unsigned char oric_via_port_a_data;
 
-//#define ORIC_DUMP_RAM
+#define ORIC_DUMP_RAM
 
 #ifdef ORIC_DUMP_RAM
 /* load image */
@@ -173,13 +147,6 @@ static void oric_keyboard_sense_refresh(void)
 		/* set sense result */
 		oric_key_sense_bit = (1<<3);
 	}
-#ifdef ORIC_DUMP_RAM
-	if (readinputport(5) & 0x01)
-	{
-		logerror("do dump");
-		oric_dump_ram();
-	}
-#endif
 }
 
 
@@ -187,9 +154,6 @@ static void oric_keyboard_sense_refresh(void)
 WRITE_HANDLER (oric_psg_porta_write)
 {
 	oric_keyboard_mask = data;
-
-	oric_keyboard_sense_refresh();
-
 }
 
 
@@ -223,6 +187,8 @@ READ_HANDLER ( oric_via_in_a_func )
 READ_HANDLER ( oric_via_in_b_func )
 {
 	int data;
+
+	oric_keyboard_sense_refresh();
 
 	data = oric_key_sense_bit;
 	data |= oric_keyboard_line & 0x07;
@@ -262,6 +228,15 @@ static void oric_psg_connection_refresh(void)
 WRITE_HANDLER ( oric_via_out_a_func )
 {
 	oric_via_port_a_data = data;
+
+	oric_psg_connection_refresh();
+
+
+	if (oric_psg_control==0)
+	{	
+		/* if psg not selected, write to printer */
+		centronics_write_data(0,data);
+	}
 }
 
 /*
@@ -285,6 +260,10 @@ PB7
 
  */
 
+#ifdef ORIC_DUMP_RAM
+static int previous_input_port5;
+#endif
+
 /* not called yet - this will update the via with the state of the tape data.
 This allows the via to trigger on bit changes and issue interrupts */
 static void    oric_refresh_tape(int dummy)
@@ -297,46 +276,82 @@ static void    oric_refresh_tape(int dummy)
 		data |=1;
 
     via_set_input_cb1(0, data);
+
+#ifdef ORIC_DUMP_RAM
+	{
+		int input_port_data = readinputport(5);
+
+		if (((previous_input_port5^input_port_data) & 0x01)!=0)
+		{
+			if (input_port_data & 0x01) 
+			{
+				logerror("do dump");
+				oric_dump_ram();
+			}
+		}
+		previous_input_port5 = input_port_data;
+	}
+#endif
 }
 
 static unsigned char previous_portb_data = 0;
 
 WRITE_HANDLER ( oric_via_out_b_func )
 {
+	int printer_handshake;
+
+	/* KEYBOARD */
 	oric_keyboard_line = data & 0x07;
 
+	/* CASSETTE */
 	/* cassette motor control */
 	device_status(IO_CASSETTE, 0, ((data>>6) & 0x01));
 
 	/* cassette data out */
 	device_output(IO_CASSETTE, 0, (data & (1<<7)) ? -32768 : 32767);
 
-	if (((previous_portb_data ^ data) & (1<<4))!=0)
+
+	/* PRINTER STROBE */
+	printer_handshake = 0;
+
+	/* normal value is 1, 0 is the strobe */
+	if ((data & (1<<4))!=0)
 	{
-		if (data & (1<<4))
+		printer_handshake = CENTRONICS_STROBE;
+	}
+
+	/* assumption: select is tied low */
+	centronics_write_handshake(0, CENTRONICS_SELECT | CENTRONICS_NO_RESET, CENTRONICS_SELECT| CENTRONICS_NO_RESET);
+	centronics_write_handshake(0, printer_handshake, CENTRONICS_STROBE);
+	
+	oric_psg_connection_refresh();
+	previous_portb_data = data;
+
+}
+
+static void oric_printer_handshake_in(int number, int data, int mask)
+{
+	int acknowledge;
+
+	acknowledge = 1;
+
+	if (mask & CENTRONICS_ACKNOWLEDGE)
+	{
+		if (data & CENTRONICS_ACKNOWLEDGE)
 		{
-			/* port a is also used for printer */
-			device_output(IO_PRINTER, 0,oric_via_port_a_data);
+			acknowledge = 0;
 		}
 	}
 
-	oric_psg_connection_refresh();
-	oric_keyboard_sense_refresh();
-	previous_portb_data = data;
+    via_set_input_ca1(0, acknowledge);
 }
 
-
-READ_HANDLER ( oric_via_in_ca1_func)
-{
-	/* printer on-line? */
-	if (device_status(IO_PRINTER, 0,0))
+static CENTRONICS_CONFIG oric_cent_config[1]={
 	{
-		/* return on-line status */
-		return 0;
-	}
-
-	return 1;
-}
+		PRINTER_CENTRONICS,
+		oric_printer_handshake_in
+	},
+};
 
 
 READ_HANDLER ( oric_via_in_ca2_func )
@@ -432,8 +447,8 @@ struct via6522_interface oric_6522_interface=
 {
 	oric_via_in_a_func,
 	oric_via_in_b_func,
-	oric_via_in_ca1_func,				/* printer acknowledge */
-	NULL,				/* tape input */
+	NULL,				/* printer acknowledge - handled by callback*/
+	NULL,				/* tape input - handled by timer */
 	oric_via_in_ca2_func,
 	oric_via_in_cb2_func,
 	oric_via_out_a_func,
@@ -446,20 +461,6 @@ struct via6522_interface oric_6522_interface=
 	NULL,
 	NULL
 };
-
-/* following are used for tape and need cleaning up */
-int oric_interrupt_counter;
-int oric_countdown_1;
-unsigned char *oric_tape_data;
-unsigned short oric_runadd;
-char oric_checkfor_filename[20];
-char oric_tape_filename[20];
-int oric_tape_datasize;
-int oric_tape_from_rom_patch;
-int oric_tape_position;
-char oric_rom_version[40];
-/*static const char *rom_name = NULL; */
-
 
 /* MICRODISC INTERFACE variables */
 /* bit 7 is intrq state */
@@ -671,101 +672,26 @@ void oric_common_init_machine(void)
 
 	oric_ram_0x0c000 = NULL;
 
-	oric_tape_timer = timer_pulse(TIME_IN_HZ(11000), 0, oric_refresh_tape);
+    oric_tape_timer = timer_pulse(TIME_IN_HZ(11025), 0, oric_refresh_tape);
 
 	/* disable os rom, enable microdisc rom */
 	/* 0x0c000-0x0dfff will be ram, 0x0e000-0x0ffff will be microdisc rom */
     port_314_w = 0x0ff^((1<<7) | (1<<1));
-
-
-#if 0
-	int i;
-
-	//int found,f2;
-	unsigned char *RAM;
-
-	RAM = memory_region(REGION_CPU1);
-	/*
-	 * gives those delightfull pages of 'U's when programms go belly-up!
-	 * The Kernal ROM does this as one of its first boot procedures.
-	 * we shall have to patch that out of the rom as it kicks the rom
-	 * once oric_load_rom() has loaded its rom, so we dont want the kernal
-	 * to splosh 0x55's all over our nice new image ...
-	 */
-	for (i = 0; i <= 0xbfff; i++)
-		RAM[i] = 0x55;
-
-	/* Boot ROM Patches ..
-	 * Patch 1 .. Temporary untill i work our how the timers and stuff
-	 * work,  the kernal sticks some values in memory at about 0x270
-	 * and seems to wait for it to hit zero before proceeding ..
-	 * perhapps its sound HW initializing? perhapps some weird timer?
-	 * who knows!!!!
-	 * we wont need this patch once ive got the IO kicking properly ..
-	 */
-
-	// dont need this patch, enabling IRQ's takes care of it ...
-	//RAM[0xedb5] = 0xea;
-	//RAM[0xedb6] = 0xea;
-	//RAM[0xedb9] = 0xea;
-	//RAM[0xedba] = 0xea;
-
-
-	// oric_load_rom(); # do this after a few seconds on a timer ..
-
-	/* Patch ROM
-	 * patch the rom's 'CLOAD' routine to push a write into a bogus IO region
-	 * trap this IO Write and action a tape image load ...
-	 *
-	 * Oric1  (1.0 Rom)
-	 *
-	 * $e4b2 :: LDA #$69;STA $03FA;RTS    (a9 69 8d fa 03 60)
-	 *
-	 * Oric Atmos (1.1 Rom)
-	 *
-	 * $e4b2 :: LDA #$69;STA $03FA;RTS    (a9 69 8d fa 03 60)
-	 *
-	 */
-
-	RAM[0xe4b2] = 0xa9;
-	RAM[0xe4b3] = 0x69;
-	RAM[0xe4b4] = 0x8d;
-	RAM[0xe4b5] = 0xfa;
-	RAM[0xe4b6] = 0x03;
-	RAM[0xe4b7] = 0x60;
-
-	RAM[0xe7b6] = 0xa9;
-	RAM[0xe7b7] = 0x69;
-	RAM[0xe7b8] = 0x8d;
-	RAM[0xe7b9] = 0xfa;
-	RAM[0xe7ba] = 0x03;
-	RAM[0xe7bb] = 0x28;
-	RAM[0xe7bc] = 0x4c;
-	RAM[0xe7bd] = 0xc8;
-	RAM[0xe7be] = 0xe7;
-
-	oric_tape_from_rom_patch = 0;
-	oric_tape_data = 0;
-
-	strcpy (oric_rom_version, "1.0");
-
-	// V1.1 ???
-
-	if (RAM[0xedaa] == 0x46)
-		if (RAM[0xedab] == 0x31)
-			if (RAM[0xedac] == 0x2e)
-				if (RAM[0xedad] == 0x31)
-					if (RAM[0xedae] == 0x0d)
-						if (RAM[0xedaf] == 0x0a)
-							strcpy (oric_rom_version, "1.1");
-#endif
 
 	via_reset();
 	via_config(0, &oric_6522_interface);
 	via_set_clock(0,1000000);
 
 
+	centronics_config(0, oric_cent_config);
+	/* assumption: select is tied low */
+	centronics_write_handshake(0, CENTRONICS_SELECT | CENTRONICS_NO_RESET, CENTRONICS_SELECT| CENTRONICS_NO_RESET);
+    via_set_input_ca1(0, 1);
+
 	wd179x_init(oric_wd179x_callback);
+#ifdef ORIC_DUMP_RAM
+	previous_input_port5 = readinputport(5);
+#endif
 }
 
 
@@ -798,6 +724,12 @@ void oric_shutdown_machine (void)
 		free(oric_ram_0x0c000);
 	oric_ram_0x0c000 = NULL;
 	wd179x_exit();
+
+	if (oric_tape_timer)
+	{
+		timer_remove(oric_tape_timer);
+		oric_tape_timer = NULL;
+	}
 }
 
 READ_HANDLER (oric_microdisc_r)
@@ -860,7 +792,7 @@ WRITE_HANDLER(oric_microdisc_w)
 
 			port_314_w = data;
 
-/*			logerror("port_314_w: %02x\n",data); */
+			logerror("port_314_w: %02x\n",data); 
 
 			/* bit 6,5: drive */
 			/* bit 4: side */
@@ -901,60 +833,14 @@ READ_HANDLER ( oric_IO_r )
 		}
 	}
 
+	logerror("via 0 r: %04x\n",offset);
+
 	/* it is repeated */
 	return via_0_r(offset & 0x0f);
 }
 
 WRITE_HANDLER ( oric_IO_w )
 {
-	/* Trap Bogus write to IO to load Tape Image!! */
-#if 0
-	if ((offset & 0xff) == 0xfa)
-	{
-		unsigned char *RAM;
-
-		RAM = memory_region(REGION_CPU1);
-
-		if (data == 0x69)
-		{
-			// CLOAD ROM PATCH HAS HIT HERE ....
-
-			int fnadd;
-			int i;
-
-			// *TODO*  .. change addr of filename for ATMOS rom..
-
-			fnadd = 0x35;
-			if (!strcmp (oric_rom_version, "1.1"))
-				fnadd = 0x27f;
-
-			i = 0;
-			while (RAM[fnadd] != 0)
-				oric_checkfor_filename[i++] = RAM[fnadd++];
-			oric_runadd = 0;
-
-			oric_tape_from_rom_patch = 1;
-
-			if (!oric_extract_file_from_tape (0))
-			{
-				device_filename_change(IO_CARTSLOT,0,oric_checkfor_filename);
-				if (!oric_load_rom(0))
-				{
-                    oric_checkfor_filename[i++] = '.';
-                    oric_checkfor_filename[i++] = 't';
-                    oric_checkfor_filename[i++] = 'a';
-                    oric_checkfor_filename[i++] = 'p';
-                    oric_checkfor_filename[i++] = 0;
-					device_filename_change(IO_CARTSLOT,0,oric_checkfor_filename);
-					oric_load_rom(0);
-				}
-			}
-			// if (oric_runadd != 0) cpu_set_pc(oric_runadd);
-			return;
-		}
-	}
-
-#endif
 	if (readinputport(9) & 0x01)
 	{
 		if ((offset>=0x010) && (offset<=0x01f))
@@ -964,227 +850,9 @@ WRITE_HANDLER ( oric_IO_w )
 		}
 	}
 
+	logerror("via 0 w: %04x %02x\n",offset,data);
+
 	via_0_w(offset & 0x0f,data);
-}
-
-#if 0
-int oric_interrupt (void)
-{
-
-	// Not Emulated .. Variable IRQ Rates,  100hz
-
-	/* Not a lot happening right now .... */
-
-	/* Probably use a timed trigger here to splosh the tape
-	 * image back into ram once the kernal has done its bits (give it 2
-	 * or 3 seconds...  will also use this to draw that real funky power
-	 * on screen (must be generated in HW on the real thing, the kernal
-	 * does nothing of the sort .... */
-
-	/*
-	 *
-	 * Defaults (altered by setting timers on 6522)
-	 * Fast IRQ 100 hz
-	 * Slow IRQ 1 hz
-	 *
-	 */
-
-	unsigned char *RAM;
-	int x, y;
-	int exec_irq;
-	int exec_slow_irq;
-
-	exec_irq = 0;
-	exec_slow_irq = 0;
-
-	oric_interrupt_counter++;
-	if (oric_interrupt_counter == 6)
-	{
-		exec_slow_irq = 1;
-		oric_interrupt_counter = 0;
-	}
-
-	if (exec_slow_irq == 0)
-		return 0;
-
-	exec_irq = 1;
-
-	RAM = memory_region(REGION_CPU1);
-
-	if (exec_irq == 1)
-	{
-		/* once this timer has dec'd we can splosh the tape image in
-		 * once the kernal has set up memory the way it likes it ...
-		 * saves patching the kernal too much ... */
-		if (oric_countdown_1 != 0)
-		{
-			oric_countdown_1--;
-			if (oric_countdown_1 == 0)
-			{
-				device_filename_change(IO_CASSETTE,0,rom_name);
-                // Try to load tape image and execute it ...
-				if (!oric_load_rom (0))
-				{
-					if (oric_runadd == 0x5555)
-						oric_runadd = 0;
-					if (oric_runadd != 0)
-						cpu_set_pc (oric_runadd);
-				}
-			}
-		}
-
-		// kick Interrupts back in
-		// nasty hack .. will hang this off some 6522 stuff later
-		//if (oric_countdown_3 != 0) {
-		//        oric_countdown_3--;
-		//        if (oric_countdown_3 == 0) interrupt_enable_w(0,1);
-		//}
-
-		/*
-		 * if (keyboard_pressed(KEYCODE_DEL)) {
-		 * unsigned char m,l;
-		 * unsigned short o;
-		 * l = RAM[0xfffc];
-		 * m = RAM[0xfffd];
-		 * o = (m * 0x100) + l;
-		 * cpu_set_pc(o);
-		 * return 0;
-		 * }
-		 */
-	}
-
-	return ignore_interrupt();
-}
-#endif
-
-int oric_extract_file_from_tape (int filenum)
-{
-	// Find Loading Address from TAP Image ...
-	// Splosh the Data into RAM ...
-
-	int z, fn;
-	int o;
-
-	// int i;
-	int notfinished;
-	int endadd;
-	unsigned char msblad, lsblad;
-	unsigned char msblend, lsblend;
-	unsigned char *RAM;
-
-	RAM = memory_region(REGION_CPU1);
-
-	if (oric_tape_data == NULL)
-		return 0;					   // no tape image in memory yet ..
-
-	if (oric_tape_position + 10 > oric_tape_datasize)
-		return 0;					   // We've Hit the END!!!
-
-	fn = 0;
-	z = oric_tape_position + 0x08;
-	msblend = oric_tape_data[z++];
-	lsblend = oric_tape_data[z++];
-	msblad = oric_tape_data[z++];
-	lsblad = oric_tape_data[z++];
-	z++;
-
-	while ((unsigned char) oric_tape_data[z++] != (unsigned char) 0)
-		oric_tape_filename[fn++] = oric_tape_data[z - 1];
-	oric_tape_filename[fn] = 0;
-	// if (z == 13) z++; // .TAP files with no filename have 2 0x00's
-	o = (msblad * 0x100) + lsblad;
-	oric_runadd = o;
-	//for (i=z;i<oric_tape_datasize;i++) RAM[o++] = oric_tape_data[i];
-	//i=z;
-	endadd = (msblend * 0x100) + lsblend;
-	notfinished = 1;
-	while (notfinished == 1)
-	{
-		RAM[o++] = oric_tape_data[z++];
-		if (z > oric_tape_datasize)
-			notfinished = 0;
-		if (o >= endadd)
-			notfinished = 0;
-	}
-	oric_tape_position = z + 1;
-	o += 2;
-	msblend = (o / 0x100);
-	lsblend = (o & 0xff);
-	RAM[0x9c] = lsblend;
-	RAM[0x9d] = msblend;
-	RAM[0x9e] = lsblend;
-	RAM[0x9f] = msblend;
-	RAM[0xa0] = lsblend;
-	RAM[0xa1] = msblend;
-	RAM[0x5f] = lsblad;
-	RAM[0x60] = msblad;
-	RAM[0x61] = lsblend;
-	RAM[0x62] = msblend;
-	RAM[0x63] = 0xff;				   // autorun flag
-	RAM[0x64] = 0xff;				   // basic or MC flag
-	if (oric_runadd == 0x501)
-		RAM[0x64] = 0;
-	// if (oric_runadd == 0x501) RAM[0x63]=0;
-
-	// 0xc773 -- list;
-	// 0xc733 -- run;
-	// 0xc708 -- run (Atmos 1.1 rom)
-	if (oric_runadd == 0x501)
-		oric_runadd = 0xc96b;
-
-	return 1;
-}
-
-int oric_load_rom(int id)
-{
-	void *file;
-
-    if (oric_tape_from_rom_patch == 0)
-	{
-		file = image_fopen (IO_CARTSLOT, id, OSD_FILETYPE_IMAGE_RW, 0);
-	}
-	else
-	{
-		file = image_fopen (IO_CASSETTE, id, OSD_FILETYPE_IMAGE_RW, 0);
-	}
-	//file = osd_fopen(Machine->gamedrv->name, "ultra.tap", OSD_FILETYPE_IMAGE_RW, 0);
-	//file = osd_fopen(Machine->gamedrv->name, "blitz.tap", OSD_FILETYPE_IMAGE_RW, 0);
-	//file = osd_fopen(Machine->gamedrv->name, "hopper.tap", OSD_FILETYPE_IMAGE_RW, 0);
-	//file = osd_fopen(Machine->gamedrv->name, "ratsplat.tap", OSD_FILETYPE_IMAGE_RW, 0);
-
-	if (file)
-	{
-		oric_tape_datasize = osd_fsize (file);
-
-		if (oric_tape_datasize != 0)
-		{
-			if (oric_tape_data == NULL)
-				oric_tape_data = malloc (oric_tape_datasize);
-
-			if (oric_tape_data != NULL)
-			{
-				osd_fread (file, oric_tape_data, oric_tape_datasize);
-				osd_fclose (file);
-
-				oric_tape_position = 0;
-
-				oric_checkfor_filename[0] = 0;
-				oric_extract_file_from_tape (0);
-
-				// free(data);
-				return 0;
-			}
-			else
-			{
-				osd_fclose (file);
-				return 1;			   // noway jose!!!
-			}
-		}
-
-	}
-
-	return 1;
-
 }
 
 
@@ -1279,6 +947,7 @@ CB2
 */
 
 static unsigned char telestrat_bank_selection;
+static unsigned char telestrat_via2_port_a_data;
 static unsigned char telestrat_via2_port_b_data;
 
 enum
@@ -1330,12 +999,25 @@ static void	telestrat_refresh_mem(void)
 	}
 }
 
+static READ_HANDLER(telestrat_via2_in_a_func)
+{
+	logerror("via 2 - port a %02x\n",telestrat_via2_port_a_data);
+	return telestrat_via2_port_a_data;
+}
+
 
 static WRITE_HANDLER(telestrat_via2_out_a_func)
 {
-	telestrat_bank_selection = data & 0x07;
+	logerror("via 2 - port a w: %02x\n",data);
 
-	telestrat_refresh_mem();
+	telestrat_via2_port_a_data = data;
+
+	if (((data^telestrat_bank_selection) & 0x07)!=0)
+	{
+		telestrat_bank_selection = data & 0x07;
+
+		telestrat_refresh_mem();
+	}
 }
 
 static READ_HANDLER(telestrat_via2_in_b_func)
@@ -1380,7 +1062,7 @@ void	telestrat_via2_irq_func(int state)
 }
 struct via6522_interface telestrat_via2_interface=
 {
-	NULL,
+	telestrat_via2_in_a_func,
 	telestrat_via2_in_b_func,
 	NULL,
 	NULL,
@@ -1465,6 +1147,12 @@ void	telestrat_shutdown_machine(void)
 
 WRITE_HANDLER(telestrat_IO_w)
 {
+	if (offset<0x010)
+	{
+		via_0_w(offset & 0x0f,data);
+		return;
+	}
+
 	if ((offset>=0x020) && (offset<=0x02f))
 	{
 		via_1_w(offset & 0x0f,data);
@@ -1477,18 +1165,23 @@ WRITE_HANDLER(telestrat_IO_w)
 		return;
 	}
 
-	if ((offset>=0x01c) && (offset<0x020))
+	if ((offset>=0x01c) && (offset<=0x01f))
 	{
 		acia_6551_w(offset, data);
 		return;
 	}
+	
+	logerror("null write %04x %02x\n",offset,data);
 
-	/* it is repeated 16 times, but for now only use first few entries */
-	via_0_w(offset & 0x0f,data);
 }
 
 READ_HANDLER(telestrat_IO_r)
 {
+	if (offset<0x010)
+	{
+		return via_0_r(offset & 0x0f);
+	}
+
 	if ((offset>=0x020) && (offset<=0x02f))
 	{
 		return via_1_r(offset & 0x0f);
@@ -1499,13 +1192,13 @@ READ_HANDLER(telestrat_IO_r)
 		return oric_microdisc_r(offset);
 	}
 
-	if ((offset>=0x01c) && (offset<=0x020))
+	if ((offset>=0x01c) && (offset<=0x01f))
 	{
 		return acia_6551_r(offset);
 	}
 
-	/* it is repeated 16 times, but for now only use first few entries */
-	return via_0_r(offset & 0x0f);
+	logerror("null read %04x\n",offset);
+	return 0x0ff;
 }
 
 
