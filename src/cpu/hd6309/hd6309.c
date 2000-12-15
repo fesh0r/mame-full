ebb9055796718f1e41d9f937fa98f602a1c4023c
rnabet <rnabet>
2000-12-15 18:27:32 +0000

    Replaced huge switch construct in hd6309_execute by a table of 256 function pointers

--- src/cpu/hd6309/hd6309.c
+++ src/cpu/hd6309/hd6309.c
@@ -949,6 +949,11 @@ unsigned hd6309_dasm(char *buffer, unsigned pc)
 #define IlegalInstructionError	{SEII;illegal();}
 #define DivisionByZeroError 	{SEDZ;illegal();}
 
+static void illegl2(void)
+{
+	IlegalInstructionError;
+}
+
 /* includes the actual opcode implementations */
 #include "6309ops.c"
 
@@ -973,264 +978,43 @@ int hd6309_execute(int cycles)	/* NS 970908 */
 			hd6309.ireg = ROP(PCD);
 			PC++;
 
-			switch( hd6309.ireg )
 			{
-			case 0x00: neg_di();   				break;
-			case 0x01: oim_di();   				break;
-			case 0x02: aim_di();   				break;
-			case 0x03: com_di();   				break;
-			case 0x04: lsr_di();   				break;
-			case 0x05: eim_di();   				break;
-			case 0x06: ror_di();   				break;
-			case 0x07: asr_di();   				break;
-			case 0x08: asl_di();   				break;
-			case 0x09: rol_di();   				break;
-			case 0x0a: dec_di();   				break;
-			case 0x0b: tim_di();   				break;
-			case 0x0c: inc_di();   				break;
-			case 0x0d: tst_di();   				break;
-			case 0x0e: jmp_di();   				break;
-			case 0x0f: clr_di();   				break;
-			case 0x10: pref10();				break;
-			case 0x11: pref11();				break;
-			case 0x12: nop();	   				break;
-			case 0x13: sync();	   				break;
-			case 0x14: sexw();	   				break;
-			case 0x15: IlegalInstructionError;	break;
-			case 0x16: lbra();	   				break;
-			case 0x17: lbsr();	   				break;
-			case 0x18: IlegalInstructionError;	break;
-			case 0x19: daa();	   				break;
-			case 0x1a: orcc();	   				break;
-			case 0x1b: IlegalInstructionError;	break;
-			case 0x1c: andcc();    				break;
-			case 0x1d: sex();	   				break;
-			case 0x1e: exg();	   				break;
-			case 0x1f: tfr();	   				break;
-			case 0x20: bra();	   				break;
-			case 0x21: brn();	   				break;
-			case 0x22: bhi();	   				break;
-			case 0x23: bls();	   				break;
-			case 0x24: bcc();	   				break;
-			case 0x25: bcs();	   				break;
-			case 0x26: bne();	   				break;
-			case 0x27: beq();	   				break;
-			case 0x28: bvc();	   				break;
-			case 0x29: bvs();	   				break;
-			case 0x2a: bpl();	   				break;
-			case 0x2b: bmi();	   				break;
-			case 0x2c: bge();	   				break;
-			case 0x2d: blt();	   				break;
-			case 0x2e: bgt();	   				break;
-			case 0x2f: ble();	   				break;
-			case 0x30: leax();	   				break;
-			case 0x31: leay();	   				break;
-			case 0x32: leas();	   				break;
-			case 0x33: leau();	   				break;
-			case 0x34: pshs();	   				break;
-			case 0x35: puls();	   				break;
-			case 0x36: pshu();	   				break;
-			case 0x37: pulu();	   				break;
-			case 0x38: IlegalInstructionError;	break;
-			case 0x39: rts();	   				break;
-			case 0x3a: abx();	   				break;
-			case 0x3b: rti();	   				break;
-			case 0x3c: cwai();					break;
-			case 0x3d: mul();					break;
-			case 0x3e: IlegalInstructionError;	break;
-			case 0x3f: swi();					break;
-			case 0x40: nega();	   				break;
-			case 0x41: IlegalInstructionError;	break;
-			case 0x42: IlegalInstructionError;	break;
-			case 0x43: coma();	   				break;
-			case 0x44: lsra();	   				break;
-			case 0x45: IlegalInstructionError;	break;
-			case 0x46: rora();	   				break;
-			case 0x47: asra();	   				break;
-			case 0x48: asla();	   				break;
-			case 0x49: rola();	   				break;
-			case 0x4a: deca();	   				break;
-			case 0x4b: IlegalInstructionError;	break;
-			case 0x4c: inca();	   				break;
-			case 0x4d: tsta();	   				break;
-			case 0x4e: IlegalInstructionError;	break;
-			case 0x4f: clra();	   				break;
-			case 0x50: negb();	   				break;
-			case 0x51: IlegalInstructionError;	break;
-			case 0x52: IlegalInstructionError;	break;
-			case 0x53: comb();	   				break;
-			case 0x54: lsrb();	   				break;
-			case 0x55: IlegalInstructionError;	break;
-			case 0x56: rorb();	   				break;
-			case 0x57: asrb();	   				break;
-			case 0x58: aslb();	   				break;
-			case 0x59: rolb();	   				break;
-			case 0x5a: decb();	   				break;
-			case 0x5b: IlegalInstructionError;	break;
-			case 0x5c: incb();	   				break;
-			case 0x5d: tstb();	   				break;
-			case 0x5e: IlegalInstructionError;	break;
-			case 0x5f: clrb();	   				break;
-			case 0x60: neg_ix();   				break;
-			case 0x61: oim_ix();   				break;
-			case 0x62: aim_ix();   				break;
-			case 0x63: com_ix();   				break;
-			case 0x64: lsr_ix();   				break;
-			case 0x65: eim_ix();   				break;
-			case 0x66: ror_ix();   				break;
-			case 0x67: asr_ix();   				break;
-			case 0x68: asl_ix();   				break;
-			case 0x69: rol_ix();   				break;
-			case 0x6a: dec_ix();   				break;
-			case 0x6b: tim_ix();   				break;
-			case 0x6c: inc_ix();   				break;
-			case 0x6d: tst_ix();   				break;
-			case 0x6e: jmp_ix();   				break;
-			case 0x6f: clr_ix();   				break;
-			case 0x70: neg_ex();   				break;
-			case 0x71: oim_ex();   				break;
-			case 0x72: aim_ex();   				break;
-			case 0x73: com_ex();   				break;
-			case 0x74: lsr_ex();   				break;
-			case 0x75: eim_ex();   				break;
-			case 0x76: ror_ex();   				break;
-			case 0x77: asr_ex();   				break;
-			case 0x78: asl_ex();   				break;
-			case 0x79: rol_ex();   				break;
-			case 0x7a: dec_ex();   				break;
-			case 0x7b: tim_ex();   				break;
-			case 0x7c: inc_ex();   				break;
-			case 0x7d: tst_ex();   				break;
-			case 0x7e: jmp_ex();   				break;
-			case 0x7f: clr_ex();   				break;
-			case 0x80: suba_im();  				break;
-			case 0x81: cmpa_im();  				break;
-			case 0x82: sbca_im();  				break;
-			case 0x83: subd_im();  				break;
-			case 0x84: anda_im();  				break;
-			case 0x85: bita_im();  				break;
-			case 0x86: lda_im();   				break;
-			case 0x87: IlegalInstructionError;  break;
-			case 0x88: eora_im();  				break;
-			case 0x89: adca_im();  				break;
-			case 0x8a: ora_im();   				break;
-			case 0x8b: adda_im();  				break;
-			case 0x8c: cmpx_im();  				break;
-			case 0x8d: bsr();	   				break;
-			case 0x8e: ldx_im();   				break;
-			case 0x8f: IlegalInstructionError;  break;
-			case 0x90: suba_di();  				break;
-			case 0x91: cmpa_di();  				break;
-			case 0x92: sbca_di();  				break;
-			case 0x93: subd_di();  				break;
-			case 0x94: anda_di();  				break;
-			case 0x95: bita_di();  				break;
-			case 0x96: lda_di();   				break;
-			case 0x97: sta_di();   				break;
-			case 0x98: eora_di();  				break;
-			case 0x99: adca_di();  				break;
-			case 0x9a: ora_di();   				break;
-			case 0x9b: adda_di();  				break;
-			case 0x9c: cmpx_di();  				break;
-			case 0x9d: jsr_di();   				break;
-			case 0x9e: ldx_di();   				break;
-			case 0x9f: stx_di();   				break;
-			case 0xa0: suba_ix();  				break;
-			case 0xa1: cmpa_ix();  				break;
-			case 0xa2: sbca_ix();  				break;
-			case 0xa3: subd_ix();  				break;
-			case 0xa4: anda_ix();  				break;
-			case 0xa5: bita_ix();  				break;
-			case 0xa6: lda_ix();   				break;
-			case 0xa7: sta_ix();   				break;
-			case 0xa8: eora_ix();  				break;
-			case 0xa9: adca_ix();  				break;
-			case 0xaa: ora_ix();   				break;
-			case 0xab: adda_ix();  				break;
-			case 0xac: cmpx_ix();  				break;
-			case 0xad: jsr_ix();   				break;
-			case 0xae: ldx_ix();   				break;
-			case 0xaf: stx_ix();   				break;
-			case 0xb0: suba_ex();  				break;
-			case 0xb1: cmpa_ex();  				break;
-			case 0xb2: sbca_ex();  				break;
-			case 0xb3: subd_ex();  				break;
-			case 0xb4: anda_ex();  				break;
-			case 0xb5: bita_ex();  				break;
-			case 0xb6: lda_ex();   				break;
-			case 0xb7: sta_ex();   				break;
-			case 0xb8: eora_ex();  				break;
-			case 0xb9: adca_ex();  				break;
-			case 0xba: ora_ex();   				break;
-			case 0xbb: adda_ex();  				break;
-			case 0xbc: cmpx_ex();  				break;
-			case 0xbd: jsr_ex();   				break;
-			case 0xbe: ldx_ex();   				break;
-			case 0xbf: stx_ex();   				break;
-			case 0xc0: subb_im();  				break;
-			case 0xc1: cmpb_im();  				break;
-			case 0xc2: sbcb_im();  				break;
-			case 0xc3: addd_im();  				break;
-			case 0xc4: andb_im();  				break;
-			case 0xc5: bitb_im();  				break;
-			case 0xc6: ldb_im();   				break;
-			case 0xc7: IlegalInstructionError;  break;
-			case 0xc8: eorb_im();  				break;
-			case 0xc9: adcb_im();  				break;
-			case 0xca: orb_im();   				break;
-			case 0xcb: addb_im();  				break;
-			case 0xcc: ldd_im();   				break;
-			case 0xcd: ldq_im();   				break; /* in m6809 was std_im */
-			case 0xce: ldu_im();   				break;
-			case 0xcf: IlegalInstructionError;  break;
-			case 0xd0: subb_di();  				break;
-			case 0xd1: cmpb_di();  				break;
-			case 0xd2: sbcb_di();  				break;
-			case 0xd3: addd_di();  				break;
-			case 0xd4: andb_di();  				break;
-			case 0xd5: bitb_di();  				break;
-			case 0xd6: ldb_di();   				break;
-			case 0xd7: stb_di();   				break;
-			case 0xd8: eorb_di();  				break;
-			case 0xd9: adcb_di();  				break;
-			case 0xda: orb_di();   				break;
-			case 0xdb: addb_di();  				break;
-			case 0xdc: ldd_di();   				break;
-			case 0xdd: std_di();   				break;
-			case 0xde: ldu_di();   				break;
-			case 0xdf: stu_di();   				break;
-			case 0xe0: subb_ix();  				break;
-			case 0xe1: cmpb_ix();  				break;
-			case 0xe2: sbcb_ix();  				break;
-			case 0xe3: addd_ix();  				break;
-			case 0xe4: andb_ix();  				break;
-			case 0xe5: bitb_ix();  				break;
-			case 0xe6: ldb_ix();   				break;
-			case 0xe7: stb_ix();   				break;
-			case 0xe8: eorb_ix();  				break;
-			case 0xe9: adcb_ix();  				break;
-			case 0xea: orb_ix();   				break;
-			case 0xeb: addb_ix();  				break;
-			case 0xec: ldd_ix();   				break;
-			case 0xed: std_ix();   				break;
-			case 0xee: ldu_ix();   				break;
-			case 0xef: stu_ix();   				break;
-			case 0xf0: subb_ex();  				break;
-			case 0xf1: cmpb_ex();  				break;
-			case 0xf2: sbcb_ex();  				break;
-			case 0xf3: addd_ex();  				break;
-			case 0xf4: andb_ex();  				break;
-			case 0xf5: bitb_ex();  				break;
-			case 0xf6: ldb_ex();   				break;
-			case 0xf7: stb_ex();   				break;
-			case 0xf8: eorb_ex();  				break;
-			case 0xf9: adcb_ex();  				break;
-			case 0xfa: orb_ex();   				break;
-			case 0xfb: addb_ex();  				break;
-			case 0xfc: ldd_ex();   				break;
-			case 0xfd: std_ex();   				break;
-			case 0xfe: ldu_ex();   				break;
-			case 0xff: stu_ex();   				break;
+				static void (* opcode_table[256])(void) =
+				{
+					neg_di,  oim_di,  aim_di,  com_di,  lsr_di,  eim_di,  ror_di,  asr_di,
+					asl_di,  rol_di,  dec_di,  tim_di,  inc_di,  tst_di,  jmp_di,  clr_di,
+					pref10,  pref11,  nop,     sync,    sexw,    illegl2, lbra,    lbsr,
+					illegl2, daa,     orcc,    illegl2, andcc,   sex,     exg,     tfr,
+					bra,     brn,     bhi,     bls,     bcc,     bcs,     bne,     beq,
+					bvc,     bvs,     bpl,     bmi,     bge,     blt,     bgt,     ble,
+					leax,    leay,    leas,    leau,    pshs,    puls,    pshu,    pulu,
+					illegl2, rts,     abx,     rti,     cwai,    mul,     illegl2, swi,
+					nega,    illegl2, illegl2, coma,    lsra,    illegl2, rora,    asra,
+					asla,    rola,    deca,    illegl2, inca,    tsta,    illegl2, clra,
+					negb,    illegl2, illegl2, comb,    lsrb,    illegl2, rorb,    asrb,
+					aslb,    rolb,    decb,    illegl2, incb,    tstb,    illegl2, clrb,
+					neg_ix,  oim_ix,  aim_ix,  com_ix,  lsr_ix,  eim_ix,  ror_ix,  asr_ix,
+					asl_ix,  rol_ix,  dec_ix,  tim_ix,  inc_ix,  tst_ix,  jmp_ix,  clr_ix,
+					neg_ex,  oim_ex,  aim_ex,  com_ex,  lsr_ex,  eim_ex,  ror_ex,  asr_ex,
+					asl_ex,  rol_ex,  dec_ex,  tim_ex,  inc_ex,  tst_ex,  jmp_ex,  clr_ex,
+					suba_im, cmpa_im, sbca_im, subd_im, anda_im, bita_im, lda_im,  illegl2,
+					eora_im, adca_im, ora_im,  adda_im, cmpx_im, bsr,     ldx_im,  illegl2,
+					suba_di, cmpa_di, sbca_di, subd_di, anda_di, bita_di, lda_di,  sta_di,
+					eora_di, adca_di, ora_di,  adda_di, cmpx_di, jsr_di,  ldx_di,  stx_di,
+					suba_ix, cmpa_ix, sbca_ix, subd_ix, anda_ix, bita_ix, lda_ix,  sta_ix,
+					eora_ix, adca_ix, ora_ix,  adda_ix, cmpx_ix, jsr_ix,  ldx_ix,  stx_ix,
+					suba_ex, cmpa_ex, sbca_ex, subd_ex, anda_ex, bita_ex, lda_ex,  sta_ex,
+					eora_ex, adca_ex, ora_ex,  adda_ex, cmpx_ex, jsr_ex,  ldx_ex,  stx_ex,
+					subb_im, cmpb_im, sbcb_im, addd_im, andb_im, bitb_im, ldb_im,  illegl2,
+					eorb_im, adcb_im, orb_im,  addb_im, ldd_im,  ldq_im /* in m6809 was std_im */, ldu_im, illegl2,
+					subb_di, cmpb_di, sbcb_di, addd_di, andb_di, bitb_di, ldb_di,  stb_di,
+					eorb_di, adcb_di, orb_di,  addb_di, ldd_di,  std_di,  ldu_di,  stu_di,
+					subb_ix, cmpb_ix, sbcb_ix, addd_ix, andb_ix, bitb_ix, ldb_ix,  stb_ix,
+					eorb_ix, adcb_ix, orb_ix,  addb_ix, ldd_ix,  std_ix,  ldu_ix,  stu_ix,
+					subb_ex, cmpb_ex, sbcb_ex, addd_ex, andb_ex, bitb_ex, ldb_ex,  stb_ex,
+					eorb_ex, adcb_ex, orb_ex,  addb_ex, ldd_ex,  std_ex,  ldu_ex,  stu_ex
+				};
+				(*opcode_table[hd6309.ireg]);
 			}
 			
 			hd6309_ICount -= cycle_counts_page0[hd6309.ireg];
