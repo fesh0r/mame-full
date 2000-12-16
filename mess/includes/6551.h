dca10da58164b1bd4c0ea57224a7c8c50c002f84
kevt <kevt>
2000-12-16 13:39:22 +0000

    updated, still not working. I'm working towards the same functionality as the msm8521 emulation (i.e. it will support "internal" serial transfers like the nc100 driver)

--- mess/includes/6551.h
+++ mess/includes/6551.h
@@ -5,5 +5,6 @@
 WRITE_HANDLER(acia_6551_w);
 READ_HANDLER(acia_6551_r);
 
-void	acia_6551_reset(void);
+void    acia_6551_init(void);
 void	acia_6551_set_irq_callback(void (*callback)(int));
+void    acia_6551_stop(void);
