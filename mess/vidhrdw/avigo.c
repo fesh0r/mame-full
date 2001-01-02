45b592efda1e6028c76518af0c7be4ff007293a3
kevt <kevt>
2001-01-02 00:52:34 +0000

    added #ifdefs around logerror messages

--- mess/vidhrdw/avigo.c
+++ mess/vidhrdw/avigo.c
@@ -25,6 +25,40 @@ static unsigned char *avigo_video_memory;
 /* current column to read/write */
 static UINT8 avigo_screen_column = 0;
 
+//#define AVIGO_VIDEO_DEBUG
+
+static int stylus_x;
+static int stylus_y;
+
+
+/* colour table filled in from avigo colour table*/
+static unsigned short stylus_color_table[3] = {0,0,0};
+
+static struct GfxLayout pointerlayout =
+{
+	8, 8,
+	1,
+	2,
+	{0, 64},
+	{0, 1, 2, 3, 4, 5, 6, 7},
+	{0 * 8, 1 * 8, 2 * 8, 3 * 8, 4 * 8, 5 * 8, 6 * 8, 7 * 8},
+	8 * 8
+};
+
+static UINT8 pointermask[] =
+{
+	0x00, 0x70, 0x60, 0x50, 0x08, 0x04, 0x00, 0x00,		/* blackmask */
+	0xf0, 0x80, 0x80, 0x80, 0x00, 0x00, 0x00, 0x00	/* whitemask */
+};
+
+static struct GfxElement *stylus_pointer;
+
+void	avigo_vh_set_stylus_marker_position(int x,int y)
+{
+	stylus_x = x;
+	stylus_y = y;
+}
+
 READ_HANDLER(avigo_vid_memory_r)
 {
         unsigned char *ptr;
@@ -36,51 +70,57 @@ READ_HANDLER(avigo_vid_memory_r)
 
         if ((offset<0x0100) || (offset>=0x01f0))
         {
-           logerror("vid mem read: %04x\r\n", offset);
-           return 0;
-        }
+#ifdef AVIGO_VIDEO_DEBUG
+			logerror("vid mem read: %04x\n", offset);
+#endif
+			return 0;
 
-        /* 0x0100-0x01f0 contains data for selected column */
-        ptr = avigo_video_memory + avigo_screen_column + ((offset-0x0100)*(AVIGO_SCREEN_WIDTH>>3));
+		}
 
-        return ptr[0];
+		/* 0x0100-0x01f0 contains data for selected column */
+		ptr = avigo_video_memory + avigo_screen_column + ((offset-0x0100)*(AVIGO_SCREEN_WIDTH>>3));
+
+		return ptr[0];
 }
 
 WRITE_HANDLER(avigo_vid_memory_w)
 {
-        if (offset==0)
-        {
-          /* select column to read/write */
-          avigo_screen_column = data;
-
-		  logerror("vid mem column write: %02x\r\n",data);
-
-          if (data>=(AVIGO_SCREEN_WIDTH>>3))
-          {
-			  logerror("error: vid mem column write: %02x\r\n",data);
-          }
-          return;
+		if (offset==0)
+		{
+			/* select column to read/write */
+			avigo_screen_column = data;
+#ifdef AVIGO_VIDEO_DEBUG
+			logerror("vid mem column write: %02x\n",data);
+#endif
+			if (data>=(AVIGO_SCREEN_WIDTH>>3))
+			{
+#ifdef AVIGO_VIDEO_DEBUG
+				logerror("error: vid mem column write: %02x\n",data);
+#endif
+			}
+			return;
         }
 
         if ((offset<0x0100) || (offset>=0x01f0))
         {
-           logerror("vid mem write: %04x %02x\r\n", offset, data);
-           return;
+#ifdef AVIGO_VIDEO_DEBUG
+			logerror("vid mem write: %04x %02x\n", offset, data);
+#endif
+			return;
         }
 
         if ((offset>=0x0100) && (offset<=0x01f0))
         {
-                unsigned char *ptr;
+			unsigned char *ptr;
 
-                /* 0x0100-0x01f0 contains data for selected column */
-                ptr = avigo_video_memory + avigo_screen_column + ((offset-0x0100)*(AVIGO_SCREEN_WIDTH>>3));
+			/* 0x0100-0x01f0 contains data for selected column */
+			ptr = avigo_video_memory + avigo_screen_column + ((offset-0x0100)*(AVIGO_SCREEN_WIDTH>>3));
 
-                ptr[0] = data;
+			ptr[0] = data;
 
-                return;
+			return;
         }
 }
-
 
 int avigo_vh_start(void)
 {
@@ -93,9 +133,13 @@ int avigo_vh_start(void)
 	if (avigo_backdrop)
 		backdrop_refresh(avigo_backdrop);
 
+	stylus_pointer = decodegfx(pointermask, &pointerlayout);
+	stylus_pointer->colortable = stylus_color_table;
+	stylus_pointer->total_colors = 3;
+	stylus_color_table[1] = Machine->pens[0];
+	stylus_color_table[2] = Machine->pens[1]; 
 	return 0;
 }
-
 void    avigo_vh_stop(void)
 {
     if (avigo_video_memory!=NULL)
@@ -107,6 +151,7 @@ void    avigo_vh_stop(void)
 	if (avigo_backdrop)
 		artwork_free(&avigo_backdrop);
 
+	freegfx(stylus_pointer);
 }
 
 /* two colours */
@@ -145,7 +190,9 @@ void avigo_init_palette(unsigned char *sys_palette, unsigned short *sys_colortab
 
 		if (avigo_backdrop)
 		{
+#ifdef AVIGO_VIDEO_DEBUG
 			logerror("backdrop %s successfully loaded\n", backdrop_name);
+#endif
             memcpy (&sys_palette[used * 3], avigo_backdrop->orig_palette, 
                     avigo_backdrop->num_pens_used * 3 * sizeof (unsigned char));
 		}
@@ -169,45 +216,55 @@ void avigo_init_palette(unsigned char *sys_palette, unsigned short *sys_colortab
 ***************************************************************************/
 void avigo_vh_screenrefresh(struct osd_bitmap *bitmap, int full_refresh)
 {
-        int y;
-        int b;
-        int x;
-        int pen0, pen1;
+    int y;
+    int b;
+    int x;
+    int pen0, pen1;
+	struct rectangle r;
 
-        pen0 = Machine->pens[0];
-        pen1 = Machine->pens[1];
+	/* draw avigo display */
+    pen0 = Machine->pens[0];
+    pen1 = Machine->pens[1];
 
-        for (y=0; y<AVIGO_SCREEN_HEIGHT; y++)
+    for (y=0; y<AVIGO_SCREEN_HEIGHT; y++)
+    {
+        int by;
+
+        unsigned char *line_ptr = avigo_video_memory +  (y*(AVIGO_SCREEN_WIDTH>>3));
+		
+        x = 0;
+        for (by=0; by<AVIGO_SCREEN_WIDTH>>3; by++)
         {
-                int by;
+            unsigned char byte;
 
-                unsigned char *line_ptr = avigo_video_memory +  (y*(AVIGO_SCREEN_WIDTH>>3));
-				
-                x = 0;
-                for (by=0; by<AVIGO_SCREEN_WIDTH>>3; by++)
+            byte = line_ptr[0];
+
+            for (b=0; b<8; b++)
+            {
+                if (byte & 0x080)
                 {
-                        unsigned char byte;
-        
-                        byte = line_ptr[0];
-        
-                        for (b=0; b<8; b++)
-                        {
-                                if (byte & 0x080)
-                                {
-                                        plot_pixel(bitmap,x+b, y, pen1);
-								}
-                                else
-                                {
-                                        plot_pixel(bitmap,x+b, y, pen0);
-        
-                                }
-								osd_mark_dirty(x+b,y,x+b,y);
-                                byte = byte<<1;
-                        }
-        
-                        x = x + 8;
-                                        
-                        line_ptr = line_ptr+1;
+					plot_pixel(bitmap,x+b, y, pen1);
+				}
+                else
+                {
+                    plot_pixel(bitmap,x+b, y, pen0);
                 }
-         }
+                byte = byte<<1;
+            }
+
+            x = x + 8;
+                            
+            line_ptr = line_ptr+1;
+        }
+     }
+
+	r.min_x = 0;
+	r.max_x = AVIGO_SCREEN_WIDTH;
+	r.min_y = 0;
+	r.max_y = AVIGO_SCREEN_HEIGHT;
+
+	/* draw stylus marker */
+	drawgfx (bitmap, stylus_pointer, 0, 0, 0, 0,
+			 stylus_x, stylus_y, &r, TRANSPARENCY_PEN, 0);
 }
+
