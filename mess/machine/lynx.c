20c8be813f6edba0260d70a481ff39f0c42697d3
PeT <PeT>
2001-01-11 23:08:15 +0000

    cosmetics

--- mess/machine/lynx.c
+++ mess/machine/lynx.c
@@ -374,8 +374,6 @@ static void lynx_blitter(void)
 	blitter.xoff=GET_WORD(suzy.data,4);
 	blitter.yoff=GET_WORD(suzy.data,6);
 
-//	logerror("blitter start\n");
-
 	for (blitter.cmd=GET_WORD(suzy.data, 0x10); blitter.cmd; 
 		 blitter.cmd=GET_WORD(blitter.mem, blitter.cmd+3) ) {
 
@@ -539,7 +537,7 @@ READ_HANDLER(suzy_read)
 WRITE_HANDLER(suzy_write)
 {
 	suzy.data[offset]=data;
-//	logerror("suzy write %.2x %.2x\n",offset,data);
+	logerror("suzy write %.2x %.2x\n",offset,data);
 	switch(offset) {
 	case 0x55: lynx_multiply();break;
 	case 0x63: lynx_divide();break;
@@ -564,6 +562,40 @@ WRITE_HANDLER(suzy_write)
 */
 MIKEY mikey={ { 0 } };
 
+
+/*
+HCOUNTER        EQU TIMER0
+VCOUNTER        EQU TIMER2
+SERIALRATE      EQU TIMER4
+
+TIM_BAKUP       EQU 0   ; backup-value (count+1)
+TIM_CNTRL1      EQU 1   ; timer-control register
+TIM_CNT EQU 2   ; current counter
+TIM_CNTRL2      EQU 3   ; dynamic control
+
+; TIM_CNTRL1
+TIM_IRQ EQU %10000000   ; enable interrupt (not TIMER4 !)
+TIM_RESETDONE   EQU %01000000   ; reset timer done
+TIM_MAGMODE     EQU %00100000   ; nonsense in Lynx !!
+TIM_RELOAD      EQU %00010000   ; enable reload
+TIM_COUNT       EQU %00001000   ; enable counter
+TIM_LINK        EQU %00000111   
+; link timers (0->2->4 / 1->3->5->7->Aud0->Aud1->Aud2->Aud3->1
+TIM_64us        EQU %00000110
+TIM_32us        EQU %00000101
+TIM_16us        EQU %00000100
+TIM_8us EQU %00000011
+TIM_4us EQU %00000010
+TIM_2us EQU %00000001
+TIM_1us EQU %00000000
+
+;TIM_CNTRL2 (read-only)
+; B7..B4 unused
+TIM_DONE        EQU %00001000   ; set if timer's done; reset with TIM_RESETDONE
+TIM_LAST        EQU %00000100   ; last clock (??)
+TIM_BORROWIN    EQU %00000010
+TIM_BORROWOUT   EQU %00000001
+*/
 typedef struct {
 	const int nr;
 	int counter;
@@ -596,6 +628,7 @@ static void lynx_timer_signal_irq(LYNX_TIMER *This)
 {
 //	if ((This->data[1]&0x80)&&!(mikey.data[0x81]&(1<<This->nr))) {
 	if ((This->data[1]&0x80)) { // irq flag handling later
+//	if ((This->data[1]&0x80)||(This->nr==4)) { // irq flag handling later
 //	{
 		cpu_set_irq_line(0, M65SC02_INT_IRQ, PULSE_LINE);
 		mikey.data[0x81]|=1<<This->nr; // vertical
@@ -670,8 +703,8 @@ static void lynx_timer_write(LYNX_TIMER *This, int offset, UINT8 data)
 	if (offset==0) This->counter=This->data[0]+1;
 	if (This->timer) { timer_remove(This->timer); This->timer=NULL; }
 //	if ((This->data[1]&0x80)&&(This->nr!=4)) { //timers are combined!
-	if ((This->data[1]&0x8)&&(This->nr!=4)) {
-//	if ((This->data[1]&0x8)) {
+//	if ((This->data[1]&0x8)||(This->nr==4)) {
+	if ((This->data[1]&0x8)) {
 		if ((This->data[1]&7)!=7) {
 			t=This->data[0]?This->data[0]:0x100;
 			if (This->data[1]&0x10) {
@@ -683,6 +716,30 @@ static void lynx_timer_write(LYNX_TIMER *This, int offset, UINT8 data)
 			}
 		}
 	}
+}
+
+typedef struct {
+	int nr;
+	UINT8 data[8];
+} LYNX_AUDIO;
+static LYNX_AUDIO lynx_audio[4]= { 
+	{ 0 },
+	{ 1 },
+	{ 2 },
+	{ 3 } 
+};
+
+static UINT8 lynx_audio_read(LYNX_AUDIO *This, int offset)
+{
+	UINT8 data=This->data[offset];
+	logerror("audio %d read %d %.2x\n", This->nr, offset, data);
+	return data;
+}
+
+static void lynx_audio_write(LYNX_AUDIO *This, int offset, UINT8 data)
+{
+	This->data[offset]=data;
+	logerror("audio %d write %d %.2x\n", This->nr, offset, data);
 }
 
 READ_HANDLER(mikey_read)
@@ -698,6 +755,18 @@ READ_HANDLER(mikey_read)
 	case 0x18: case 0x19: case 0x1a: case 0x1b:
 	case 0x1c: case 0x1d: case 0x1e: case 0x1f:
 		data=lynx_timer_read(lynx_timer+(offset/4), offset&3);
+		return data;
+//		break;
+	case 0x20: case 0x21: case 0x22: case 0x23: case 0x24: case 0x25: case 0x26: case 0x27:
+	case 0x28: case 0x29: case 0x2a: case 0x2b: case 0x2c: case 0x2d: case 0x2e: case 0x2f:
+	case 0x30: case 0x31: case 0x32: case 0x33:	case 0x34: case 0x35: case 0x36: case 0x37:
+	case 0x38: case 0x39: case 0x3a: case 0x3b: case 0x3c: case 0x3d: case 0x3e: case 0x3f:
+		data=lynx_audio_read(lynx_audio+(offset&0x1f)/8, offset&7);
+		return data;
+		break;
+	case 0x81:
+		data=mikey.data[offset];
+		mikey.data[offset]&=~0x10; // timer 4 autoquit!?
 		break;
 	case 0x8b:
 		data=mikey.data[offset];
@@ -709,14 +778,13 @@ READ_HANDLER(mikey_read)
 	default:
 		data=mikey.data[offset];
 	}
-//	logerror("mikey read %.2x %.2x\n",offset,data);
+	logerror("mikey read %.2x %.2x\n",offset,data);
 	return data;
 }
 
 WRITE_HANDLER(mikey_write)
 {
 	mikey.data[offset]=data;
-//	logerror("mikey write %.2x %.2x\n",offset,data);
 	switch (offset) {
 	case 0: case 1: case 2: case 3:
 	case 4: case 5: case 6: case 7:
@@ -727,7 +795,15 @@ WRITE_HANDLER(mikey_write)
 	case 0x18: case 0x19: case 0x1a: case 0x1b:
 	case 0x1c: case 0x1d: case 0x1e: case 0x1f:
 		lynx_timer_write(lynx_timer+(offset/4), offset&3, data);
-		break;
+		return;
+//		break;
+	case 0x20: case 0x21: case 0x22: case 0x23: case 0x24: case 0x25: case 0x26: case 0x27:
+	case 0x28: case 0x29: case 0x2a: case 0x2b: case 0x2c: case 0x2d: case 0x2e: case 0x2f:
+	case 0x30: case 0x31: case 0x32: case 0x33:	case 0x34: case 0x35: case 0x36: case 0x37:
+	case 0x38: case 0x39: case 0x3a: case 0x3b: case 0x3c: case 0x3d: case 0x3e: case 0x3f:
+		lynx_audio_write(lynx_audio+(offset&0x1f)/8, offset&7, data);
+		return;
+//break;		
 	case 0x80:
 		mikey.data[0x81]&=~data; // clear interrupt source
 		break;
@@ -742,7 +818,8 @@ WRITE_HANDLER(mikey_write)
 			suzy.high=0;
 			suzy.low=0;
 		}
-		break;
+		return;
+//		break;
 	case 0xa0: case 0xa1: case 0xa2: case 0xa3: case 0xa4: case 0xa5: case 0xa6: case 0xa7:
 	case 0xa8: case 0xa9: case 0xaa: case 0xab: case 0xac: case 0xad: case 0xae: case 0xaf:
 	case 0xb0: case 0xb1: case 0xb2: case 0xb3: case 0xb4: case 0xb5: case 0xb6: case 0xb7:
@@ -752,8 +829,10 @@ WRITE_HANDLER(mikey_write)
 							 (mikey.data[0xa0+(offset&0xf)]&0xf)<<4,
 							 (mikey.data[0xb0+(offset&0xf)]&0xf)<<4,
 							 mikey.data[0xb0+(offset&0xf)]&0xf0 );
-		break;
+		return;
+//		break;
 	}
+	logerror("mikey write %.2x %.2x\n",offset,data);
 }
 
 WRITE_HANDLER( lynx_memory_config )
