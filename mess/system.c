f9d6c9edae6dff64f0310e6627f7db73275d860d
rnabet <rnabet>
2001-02-12 00:58:48 +0000

    Promoted APEXC to full driver.

--- mess/system.c
+++ mess/system.c
@@ -639,7 +639,7 @@ TESTDRIVER( hp48g ) 	/* HP48 G/GX										*/
 	DRIVER( svi328a ) 	/* SVI-328	(BASIC 1.11)						  */
 
 	/* Booth (this is the builder, not a company) */
-TESTDRIVER( apexc )		/* 1951(?) APEXC : All-Purpose Electronic X-ray Computer */
+	DRIVER( apexc )		/* 1951(?) APEXC : All-Purpose Electronic X-ray Computer */
 
 #endif
 
