01ab7ce7428c898a75eebf7b55f71cea1b2bd97f
l_g <l_g>
2001-04-28 17:28:52 +0000

    Fixed a compile error under Tru64 UNIX.

--- src/unix/osd_cpu.h
+++ src/unix/osd_cpu.h
@@ -39,7 +39,7 @@
 #endif
 
 /* grrr work around some stupid header conflicts */
-#if !defined __XF86_DGA_C && !defined __XOPENGL_C_
+#if !defined __XF86_DGA_C && !defined __XOPENGL_C_ && !defined LONG64
 typedef signed   char      INT8;
 typedef signed   short     INT16;
 typedef signed   int       INT32;
@@ -50,7 +50,7 @@ typedef unsigned short     UINT16;
 typedef unsigned int       UINT32;
 typedef unsigned long long UINT64;
 
-/* Combine to 32-bit integers into a 64-bit integer */
+/* Combine two 32-bit integers into a 64-bit integer */
 #define COMBINE_64_32_32(A,B)     ((((UINT64)(A))<<32) | (UINT32)(B))
 #define COMBINE_U64_U32_U32(A,B)  COMBINE_64_32_32(A,B)
 
