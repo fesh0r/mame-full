5903efc807e5532fce57d008ee5656356d74f054
PeT <PeT>
2001-05-12 09:19:54 +0000

    sound added

--- mess/includes/svision.h
+++ mess/includes/svision.h
@@ -21,4 +21,15 @@ extern void svision_runtime_loader_init(void);
 # endif
 #endif
 
+typedef struct {
+    UINT8 reg[3];
+    int pos;
+    int size;
+} SVISION_CHANNEL;
+extern SVISION_CHANNEL svision_channel[2];
+extern int svision_custom_start (const struct MachineSound *driver);
+extern void svision_custom_stop (void);
+extern void svision_custom_update (void);
+extern void svision_soundport_w (SVISION_CHANNEL *channel, int offset, int data);
+
 #endif
