bd48decfeb55190d31480b513f26f462f742a6d7
PeT <PeT>
2001-06-23 06:24:49 +0000

    added partial port c functionality; off key; asic restructured

--- mess/machine/pc1403.c
+++ mess/machine/pc1403.c
@@ -7,50 +7,52 @@
 
 /* C-CE while reset, program will not be destroyed! */
 
-/* error codes
-1 syntax error
-2 calculation error
-3 illegal function argument
-4 too large a line number
-5 next without for
-  return without gosub
-6 memory overflow
-7 print using error
-8 i/o device error
-9 other errors*/
-
-static UINT8 keyb, outa;
+static UINT8 outa;
+UINT8 pc1403_portc;
 
 static int power=1; /* simulates pressed cce when mess is started */
 
-static UINT8 asic[0x800>>8];
+
+/* 
+   port 2:
+     bits 0,1: external rom a14,a15 lines
+   port 3: 
+     bits 0..6 keyboard output select matrix line
+*/
+static UINT8 asic[4];
 
 WRITE_HANDLER(pc1403_asic_write)
 {
-    asic[offset>>8]=data;
-    logerror ("asic write %.4x %.2x\n",offset, data);
-    switch( (offset>>8) ){
-    case 0xc-8:
-	cpu_setbank(1, memory_region(REGION_USER1)+((data&7)<<14));
-	break;
-    case 8-8:
+    asic[offset>>9]=data;
+    switch( (offset>>9) ){
+    case 0/*0x3800*/:
 	// output
+	logerror ("asic write %.4x %.2x\n",offset, data);
 	break;
-    case 0xe - 8:
-         keyb=data; break;
+    case 1/*0x3a00*/:
+	logerror ("asic write %.4x %.2x\n",offset, data);
+	break;
+    case 2/*0x3c00*/:
+	cpu_setbank(1, memory_region(REGION_USER1)+((data&7)<<14));
+	logerror ("asic write %.4x %.2x\n",offset, data);
+	break;
+    case 3/*0x3e00*/: break;
     }
 }
 
 READ_HANDLER(pc1403_asic_read)
 {
-    UINT8 data=asic[offset>>8];
-    logerror ("asic read %.4x %.2x\n",offset, data);
+    UINT8 data=asic[offset>>9];
+    switch( (offset>>9) ){
+    case 0: case 1: case 2:
+	logerror ("asic read %.4x %.2x\n",offset, data);
+	break;
+    }
     return data;
 }
 
 void pc1403_outa(int data)
 {
-    logerror ("a write %.2x\n",data);
     outa=data;
 }
 
@@ -58,7 +60,7 @@ int pc1403_ina(void)
 {
     UINT8 data=outa;
 
-    if (keyb&1) {
+    if (asic[3]&1) {
 	if (KEY_7) data|=1;
 	if (KEY_8) data|=2;
 	if (KEY_9) data|=4;
@@ -68,7 +70,7 @@ int pc1403_ina(void)
 	// 0x40
 	// 0x80
     }
-    if (keyb&2) {
+    if (asic[3]&2) {
 	if (KEY_4) data|=1;
 	if (KEY_5) data|=2;
 	if (KEY_6) data|=4;
@@ -78,7 +80,7 @@ int pc1403_ina(void)
 	if (KEY_DEF) data|=0x40;
 	if (KEY_SMALL) data|=0x80;
     }
-    if (keyb&4) {
+    if (asic[3]&4) {
 	if (KEY_1) data|=1;
 	if (KEY_2) data|=2;
 	if (KEY_3) data|=4;
@@ -88,7 +90,7 @@ int pc1403_ina(void)
 	if (KEY_A) data|=0x40;
 	if (KEY_Z) data|=0x80;
     }
-    if (keyb&8) {
+    if (asic[3]&8) {
 	if (KEY_0) data|=1;
 	if (KEY_SIGN) data|=2;
 	if (KEY_POINT) data|=4;
@@ -98,7 +100,7 @@ int pc1403_ina(void)
 	if (KEY_S) data|=0x40;
 	if (KEY_X) data|=0x80;
     }
-    if (keyb&0x10) {
+    if (asic[3]&0x10) {
 	if (KEY_HYP) data|=1;
 	if (KEY_SIN) data|=2;
 	if (KEY_COS) data|=4;
@@ -108,7 +110,7 @@ int pc1403_ina(void)
 	if (KEY_D) data|=0x40;
 	if (KEY_C) data|=0x80;
     }
-    if (keyb&0x20) {
+    if (asic[3]&0x20) {
 	if (KEY_HEX) data|=1;
 	if (KEY_DEG) data|=2;
 	if (KEY_LN) data|=4;
@@ -118,7 +120,7 @@ int pc1403_ina(void)
 	if (KEY_F) data|=0x40;
 	if (KEY_V) data|=0x80;
     }
-    if (keyb&0x40) {
+    if (asic[3]&0x40) {
 	if (KEY_EXP) data|=1;
 	if (KEY_POT) data|=2;
 	if (KEY_ROOT) data|=4;
@@ -168,9 +170,8 @@ int pc1403_ina(void)
 	if (KEY_CAL) data|=0x80;
     }
     if (outa&0x40) {
-	//0x80
+	if (KEY_OFF) data|=0x80;
     }
-    logerror ("a read %.2x\n",data);
     return data;
 }
 
@@ -183,12 +184,19 @@ int pc1403_inb(void)
 }
 #endif
 
-int pc1403_brk(void)
+void pc1403_outc(int data)
+{
+    pc1403_portc=data;
+    logerror("%g pc %.4x outc %.2x\n",timer_get_time(), cpu_get_pc(), data);
+}
+
+
+bool pc1403_brk(void)
 {
 	return KEY_BRK;
 }
 
-int pc1403_reset(void)
+bool pc1403_reset(void)
 {
 	return KEY_RESET;
 }
@@ -247,11 +255,11 @@ void pc1403_machine_init(void)
 {
 	cpu_setbank(1, memory_region(REGION_USER1));
 	if (RAM32K) {
-//		install_mem_read_handler (0, 0xa000, 0xffff, MRA_RAM);
-//		install_mem_write_handler (0, 0xa000, 0xffff, MWA_RAM);
+		install_mem_read_handler (0, 0x8000, 0xdfff, MRA_RAM);
+		install_mem_write_handler (0, 0x8000, 0xdfff, MWA_RAM);
 	} else {
-//		install_mem_read_handler (0, 0xa000, 0xffff, MRA_NOP);
-//		install_mem_write_handler (0, 0xa000, 0xffff, MWA_NOP);
+		install_mem_read_handler (0, 0x8000, 0xdfff, MRA_NOP);
+		install_mem_write_handler (0, 0x8000, 0xdfff, MWA_NOP);
 	}
 }
 
