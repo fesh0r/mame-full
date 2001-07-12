5da1dd7bd34e172270c280551da66a45ac0986ec
ben <ben>
2001-07-12 16:18:37 +0000

    Comment about usage of array

--- mess/device.c
+++ mess/device.c
@@ -8,20 +8,21 @@
 
 #include "device.h"
 
-/* The List of Devices, with Associated Names */
+/* The List of Devices, with Associated Names - Be careful to ensure that 	*/
+/* this list matches the ENUM from device.h, so searches can use IO_COUNT	*/
 const struct Devices devices[IO_COUNT] =
 {
-	{IO_END,		"NONE",			"NONE"},
-	{IO_CARTSLOT,	"cartridge",	"cart"},
-	{IO_FLOPPY,		"floppydisk",	"flop"},
-	{IO_HARDDISK,	"harddisk",		"hard"},
-	{IO_CYLINDER,	"cylinder",		"cyln"},
-	{IO_CASSETTE,	"cassette",		"cass"},
-	{IO_PUNCHCARD,	"punchcard",	"pcrd"},
-	{IO_PUNCHTAPE,	"punchtape",	"ptap"},
-	{IO_PRINTER,	"printer",		"prin"},
-	{IO_SERIAL,		"serial",		"serl"},
-	{IO_PARALLEL,   "parallel",		"parl"},
-	{IO_SNAPSHOT,	"snapshot",		"dump"},
-	{IO_QUICKLOAD,	"quickLoad",	"quik"}
+	{IO_END,		"NONE",			"NONE"}, /*  0 */
+	{IO_CARTSLOT,	"cartridge",	"cart"}, /*  1 */
+	{IO_FLOPPY,		"floppydisk",	"flop"}, /*  2 */
+	{IO_HARDDISK,	"harddisk",		"hard"}, /*  3 */
+	{IO_CYLINDER,	"cylinder",		"cyln"}, /*  4 */
+	{IO_CASSETTE,	"cassette",		"cass"}, /*  5 */
+	{IO_PUNCHCARD,	"punchcard",	"pcrd"}, /*  6 */
+	{IO_PUNCHTAPE,	"punchtape",	"ptap"}, /*  7 */
+	{IO_PRINTER,	"printer",		"prin"}, /*  8 */
+	{IO_SERIAL,		"serial",		"serl"}, /*  9 */
+	{IO_PARALLEL,   "parallel",		"parl"}, /* 10 */
+	{IO_SNAPSHOT,	"snapshot",		"dump"}, /* 11 */
+	{IO_QUICKLOAD,	"quickLoad",	"quik"}, /* 12 */
 };
--- mess/device.h
+++ mess/device.h
@@ -1,22 +1,23 @@
 #ifndef DEVICE_H
 #define DEVICE_H
 
+
 enum
-{
-	IO_END = 0,		/* Dummy type to end IODevice enumerations 										*/
-	IO_CARTSLOT,	/* Cartidge Port, as found on most console and on some computers 				*/
-	IO_FLOPPY,		/* Floppy Disk unit 															*/
-	IO_HARDDISK,	/* Hard Disk unit 																*/
-	IO_CYLINDER,	/* Magnetically-Coated Cylinder 												*/
-	IO_CASSETTE,	/* Cassette Recorder (common on early home computers) 							*/
-	IO_PUNCHCARD,	/* Card Puncher/Reader 															*/
-	IO_PUNCHTAPE,	/* Tape Puncher/Reader (reels instead of punchcards) 							*/
-	IO_PRINTER,		/* Printer device 																*/
-	IO_SERIAL,		/* some serial port 															*/
-	IO_PARALLEL,    /* Generic Parallel Port														*/
-	IO_SNAPSHOT,	/* Complete 'snapshot' of the state of the computer 							*/
-	IO_QUICKLOAD,	/* Allow to load program/data into memory, without matching any actual device	*/
-	IO_COUNT		/* Total Number of IO_devices for searching										*/
+{	/* List of all supported devices.  Refer to the device by these names only							*/
+	IO_END = 0,		/*  0 - Dummy type to end IODevice enumerations 									*/
+	IO_CARTSLOT,	/*  1 - Cartidge Port, as found on most console and on some computers 				*/
+	IO_FLOPPY,		/*  2 - Floppy Disk unit 															*/
+	IO_HARDDISK,	/*  3 - Hard Disk unit 																*/
+	IO_CYLINDER,	/*  4 - Magnetically-Coated Cylinder 												*/
+	IO_CASSETTE,	/*  5 - Cassette Recorder (common on early home computers) 							*/
+	IO_PUNCHCARD,	/*  6 - Card Puncher/Reader 															*/
+	IO_PUNCHTAPE,	/*  7 - Tape Puncher/Reader (reels instead of punchcards) 							*/
+	IO_PRINTER,		/*  8 - Printer device 																*/
+	IO_SERIAL,		/*  9 - some serial port 															*/
+	IO_PARALLEL,    /* 10 - Generic Parallel Port														*/
+	IO_SNAPSHOT,	/* 11 - Complete 'snapshot' of the state of the computer 							*/
+	IO_QUICKLOAD,	/* 12 - Allow to load program/data into memory, without matching any actual device	*/
+	IO_COUNT		/* 13 - Total Number of IO_devices for searching										*/
 };
 
 struct Devices {
