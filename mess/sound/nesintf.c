0ac8cef0ba24050428e380104e880cb082de5f4a
npwoods <npwoods>
2001-10-12 00:52:49 +0000

    Fixed bug that caused NES to crash when sound is disabled

--- mess/sound/nesintf.c
+++ mess/sound/nesintf.c
@@ -74,27 +74,32 @@ int NESPSG_sh_start(const struct MachineSound *msound)
 		apu[i] = NULL;
 	}
 
-	if (Machine->sample_rate == 0)
-		return 0;
-
 	intf = msound->sound_interface;
 
 	for (i = 0; i < intf->num; i++)
 	{
+		int sample_rate;
+
+		sample_rate = Machine->sample_rate;
+
 		/* hack it out as 60Hz for now - bleh! */
-		apu[i] = apu_create(intf->cpunum[i], intf->basefreq, Machine->sample_rate, Machine->drv->frames_per_second, 16);
+		/* NPW 11-Oct-2001 - special case for when sample_rate is zero */
+		apu[i] = apu_create(intf->cpunum[i], intf->basefreq, sample_rate ? sample_rate : 44100, Machine->drv->frames_per_second, 16);
 		if (NULL == apu[i])
 			return 1;
 
 		apu_setcontext(apu[i]);
 		apu_reset();
 
-		sprintf(buf, "NES APU %d", i);
-		channel[i] = stream_init(buf, intf->volume[i], Machine->sample_rate,
-					i, process);
+		if (sample_rate != 0)
+		{
+			sprintf(buf, "NES APU %d", i);
+			channel[i] = stream_init(buf, intf->volume[i], Machine->sample_rate,
+						i, process);
 
-		if (-1 == channel[i])
-			return 1;
+			if (-1 == channel[i])
+				return 1;
+		}
 	}
 
 	return 0;
