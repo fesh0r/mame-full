d348d48243d952fc607056b9a08a1b4a7d6707ff
l_g <l_g>
2001-11-19 08:01:54 +0000

    OpenGL fixes from Sven Goethel.

--- src/unix/main.c
+++ src/unix/main.c
@@ -9,11 +9,6 @@
 #include <sys/mman.h>
 #endif
 
-#ifdef xgl
-	#include "driver.h"
-	#include <math.h>
-#endif
-
 /* put here anything you need to do when the program is started. Return 0 if */
 /* initialization was successful, nonzero otherwise. */
 int osd_init(void)
@@ -43,10 +38,6 @@ int main (int argc, char **argv)
 {
 	int res;
 
-#ifdef xgl
-	int i, color_depth;
-#endif
-
 #ifdef __QNXNTO__
 	printf("info: Trying to enable swapfile.... ");
 	munlockall();
@@ -75,59 +66,6 @@ int main (int argc, char **argv)
         /* Check the colordepth we're requesting */
         if (!options.color_depth && !sysdep_display_16bpp_capable())
            options.color_depth = 8;
-#ifdef xgl
-
-	/**
-	 * this hack is just to offer the
-	 * game's true color as the default,
-	 * if -bpp does not tell us otherwise ...
-	 *
-	 * just the options.color_depth is written,
-	 * if not set by the user ...
-	 *
-	 * diff's to mame.c:
-	 * the usage of the video attribute 'VIDEO_NEEDS_6BITS_PER_GUN'
-	 * is not logical for me (the opengl display) .. ??
-	 */
-	Machine->gamedrv = drivers[game_index];
-	Machine->drv = Machine->gamedrv->drv;
-
-	i=0; 
-	color_depth=1;
-	while(i<Machine->drv->color_table_len)
-	{
-		color_depth*=2;
-		i=(int)pow(2,color_depth);
-	}
-
-	fprintf(stderr,"**** machine orig. color number: %d\n", 
-		Machine->drv->color_table_len);
-
-	fprintf(stderr,"**** machine's orig. color_depth: %d\n", color_depth);
-
-	if( (Machine->drv->video_attributes & VIDEO_RGB_DIRECT) >0 )
-	{
-		if(color_depth==16) color_depth=15;
-		else color_depth=32;
-	} else {
-		if(Machine->drv->color_table_len>256)
-			color_depth=16;
-	}
-
-	fprintf(stderr,"**** color_depth regarding video attr.: %d\n", color_depth);
-
-	if(  options.color_depth==0 ||
-	    (options.color_depth==16 && color_depth==15)
-	  )
-	{
-		options.color_depth = color_depth;
-		fprintf(stderr,"****  setting options.color_depth := %d\n",
-			options.color_depth);
-	} else {
-		fprintf(stderr,"****  given options.color_depth := %d\n",
-			options.color_depth);
-	}
-#endif
 
 	/* go for it */
 	res = run_game (game_index);
