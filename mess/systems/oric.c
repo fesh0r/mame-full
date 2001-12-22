1ebc8b6959f3235af1555fecb8aaf8c193b6fced
kevt <kevt>
2001-12-22 10:39:06 +0000

    added .tap to the list of supported cassette formats in pravetz drivers.

--- mess/systems/oric.c
+++ mess/systems/oric.c
@@ -438,7 +438,7 @@ static const struct IODevice io_oric1[] =
 
 static const struct IODevice io_prav8[] =
 {
-	IO_CASSETTE_WAVE(1,"wav\0",NULL,oric_cassette_init,oric_cassette_exit),
+	IO_CASSETTE_WAVE(1,"tap\0wav\0",NULL,oric_cassette_init,oric_cassette_exit),
  	IO_PRINTER_PORT(1,"prn\0"),
 	{
 		IO_FLOPPY,				/* type */
