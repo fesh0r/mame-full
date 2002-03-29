065ada904d761135cd358be0f92c66cb20c7bd3b
JoJo <JoJo>
2002-03-29 06:28:24 +0000

    UK101 Cassette initialization fixed

--- mess/machine/uk101.c
+++ mess/machine/uk101.c
@@ -85,6 +85,13 @@ int	uk101_init_cassette(int id)
 {
 	void	*file;
 
+        /* a cassette for the uk101 isnt needed */
+        if (!device_filename(IO_CASSETTE,id) || !strlen(device_filename(IO_CASSETTE,id) ))
+        {
+                logerror("UK101/Superboard - warning: no cassette specified!\n");
+                return INIT_PASS;
+        }                                                                                                                                     
+
 	file = image_fopen(IO_CASSETTE, id, OSD_FILETYPE_IMAGE, OSD_FOPEN_READ);
 	if (file)
 	{
