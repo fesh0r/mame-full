5b63fbff8276f728cd7b691254ed08dd9a8caf83
tdr <tdr>
2002-03-29 06:57:09 +0000

    MBC1 RAM/ROM mode select handler. Updated scnaline interrupt function name for LCD status timing change.

--- mess/machine/gb.h
+++ mess/machine/gb.h
@@ -77,13 +77,14 @@ EXTERN UINT8 gb_tile_no_mod;
 
 extern WRITE_HANDLER ( gb_rom_bank_select );
 extern WRITE_HANDLER ( gb_ram_bank_select );
+extern WRITE_HANDLER ( gb_mem_mode_select );
 extern WRITE_HANDLER ( gb_w_io );
 extern READ_HANDLER  ( gb_r_divreg );
 extern READ_HANDLER  ( gb_ser_regs );
 extern READ_HANDLER  ( gb_r_timer_cnt );
 extern int gb_load_rom (int id);
 extern int gb_scanline_interrupt(void);
-extern void gb_scanline_interrupt_set_mode2(int param);
+extern void gb_scanline_interrupt_set_mode0(int param);
 extern void gb_scanline_interrupt_set_mode3(int param);
 extern int gb_vh_start(void);
 extern void gb_vh_stop(void);
