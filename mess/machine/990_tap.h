6a900955c9447f103b535726325c72c3e146b6b8
rnabet <rnabet>
2002-04-24 15:55:09 +0000

    Renamed mt3200.[ch] to 990_tap.[ch], as I do not intend to implement mt3200-speific features.  Additionnally, fixed a few bugs and added interrupt support.

---
+++ mess/machine/990_tap.h
@@ -0,0 +1,13 @@
+/*
+	990_tap.h: include file for 990_tap.c
+*/
+
+extern int ti990_tape_init(int id);
+extern void ti990_tape_exit(int id);
+
+void ti990_tpc_init(void (*interrupt_callback)(int state));
+void ti990_tpc_exit(void);
+
+extern READ16_HANDLER(ti990_tpc_r);
+extern WRITE16_HANDLER(ti990_tpc_w);
+
