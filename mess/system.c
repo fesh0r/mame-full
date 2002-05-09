e1a58d3b8e76c295fcdee009ee8851594076e34c
rnabet <rnabet>
2002-05-09 17:48:18 +0000

    Inserted ti99_4ev, and promoted ti990_10 from TESTDRIVER to DRIVER.

--- mess/system.c
+++ mess/system.c
@@ -469,15 +469,17 @@ TESTDRIVER( mz800  )	/* 1982 Sharp MZ800 							  */
 /*	DRIVER( x68000 )*/	/* X68000										  */
 
 	/* TEXAS INSTRUMENTS */
-TESTDRIVER( ti990_4 )	/* 197? TI 990/4								  */
-TESTDRIVER( ti990_10 )	/* 197? TI 990/10								  */
-TESTDRIVER( ti99_224 )	/* 1983 TI 99/2 								  */
-TESTDRIVER( ti99_232 )	/* 1983 TI 99/2 								  */
-	DRIVER( ti99_4 )	/* 1978 TI 99/4 								  */
-	DRIVER( ti99_4e )	/* 1980 TI 99/4E								  */
+	DRIVER( ti990_10 )	/* 1975 TI 990/10								  */
+TESTDRIVER( ti990_4 )	/* 1976 TI 990/4								  */
+
+TESTDRIVER( ti99_224 )	/* 1983 TI 99/2 (24kb ROMs) 					  */
+TESTDRIVER( ti99_232 )	/* 1983 TI 99/2 (32kb ROMs) 					  */
+	DRIVER( ti99_4 )	/* 1979 TI 99/4 								  */
+	DRIVER( ti99_4e )	/* 1980 TI 99/4 with 50Hz video					  */
 	DRIVER( ti99_4a )	/* 1981 TI 99/4A								  */
-	DRIVER( ti99_4ae )	/* 1981 TI 99/4AE								  */
-TESTDRIVER( ti99_4p )	/* 199x 99/4P									  */
+	DRIVER( ti99_4ae )	/* 1981 TI 99/4A with 50Hz video				  */
+	DRIVER( ti99_4ev)	/* 1994 TI 99/4A with EVPC video card			  */
+TESTDRIVER( ti99_4p )	/* 1996 SNUG 99/4P (a.k.a. SGCPU)				  */
 
     DRIVER( avigo )     /*                                                */
 
