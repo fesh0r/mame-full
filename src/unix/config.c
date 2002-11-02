cb04542fc7843c20e6ee2ebd1d2205c7b81deac5
l_g <l_g>
2002-11-02 19:21:58 +0000

    Replaced add_device() with the newer, slimmer version from the Windows code. This fixes a compile error.

--- src/unix/config.c
+++ src/unix/config.c
@@ -712,33 +712,23 @@ void show_usage(void)
 }
 
 #ifdef MESS
-/* add_device() is called when the MESS CLI option has been identified 		*/
-/* This searches throught the devices{} struct array to grab the ID of the 	*/
-/* option, which then registers the device using register_device()			*/
+/*	add_device() is called when the MESS CLI option has been identified
+ *	This searches throught the devices{} struct array to grab the ID of the
+ *	option, which then registers the device using register_device()
+ */
 static int add_device(struct rc_option *option, const char *arg, int priority)
 {
-	extern const struct Devices devices[]; /* from mess device.c */
-	int i=0;
-
-	/* First, we need to find the ID of this option - kludge alert!			*/
-	while(devices[i].id != IO_COUNT)
+	int id;
+	id = device_typeid(option->name);
+	if (id < 0)
 	{
-		if (!stricmp(option->name, devices[i].name)  		||
-			!stricmp(option->shortname, devices[i].shortname))
-		{
-			/* A match!  we now know the ID of the device */
-			option->priority = priority;
-			return (register_device (devices[i].id, arg));
-		}
-		else
-		{
-			/* No match - keep looking */
-			i++;
-		}
+		/* If we get to here, log the error - This is mostly due to a mismatch in the array */
+		logerror("Command Line Option [-%s] not a valid device - ignoring\n", option->name);
+		return -1;
 	}
 
-	/* If we get to here, log the error - This is mostly due to a mismatch in the array */
-	logerror("Command Line Option [-%s] not a valid device - ignoring\n", option->name);
-    return -1;
+	/* A match!  we now know the ID of the device */
+	option->priority = priority;
+	return register_device(id, arg);
 }
 #endif
