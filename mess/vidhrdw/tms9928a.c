c03f95897fdfbc4b8b6585b53d0140e063fd603a
rnabet <rnabet>
2002-12-07 21:05:40 +0000

    Shrank screen borders to respect 4:3 ratio.

--- mess/vidhrdw/tms9928a.c
+++ mess/vidhrdw/tms9928a.c
@@ -939,8 +939,12 @@ void mdrv_tms9928a(struct InternalMachineDriver *machine, const TMS9928a_interfa
 	/* video hardware */
 	MDRV_VIDEO_ATTRIBUTES(VIDEO_UPDATE_BEFORE_VBLANK | VIDEO_TYPE_RASTER)
 	MDRV_SCREEN_SIZE(LEFT_BORDER+32*8+RIGHT_BORDER, top_border+24*8+bottom_border)
+	/* display no border */
 	/*MDRV_VISIBLE_AREA(LEFT_BORDER+0*8, LEFT_BORDER+32*8-1, top_border+0*8, top_border+24*8-1)*/
-	MDRV_VISIBLE_AREA(0, LEFT_BORDER+32*8+RIGHT_BORDER-1, 0, top_border+24*8+bottom_border-1)
+	/* display max borders */
+	/*MDRV_VISIBLE_AREA(0, LEFT_BORDER+32*8+RIGHT_BORDER-1, 0, top_border+24*8+bottom_border-1)*/
+	/* display fixed (12+12)*(9+9) borders */
+	MDRV_VISIBLE_AREA(LEFT_BORDER-12, LEFT_BORDER+32*8+12-1, top_border-9, top_border+24*8+9-1)
 	MDRV_PALETTE_LENGTH(TMS9928A_PALETTE_SIZE)
 	MDRV_COLORTABLE_LENGTH(0)
 	MDRV_PALETTE_INIT(tms9928a)
