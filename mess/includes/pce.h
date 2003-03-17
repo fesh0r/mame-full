96159e90a8c0cee1c786affbab5ab1f8939650e1
rnabet <rnabet>
2003-03-17 16:45:08 +0000

    Fixed build error.

--- mess/includes/pce.h
+++ mess/includes/pce.h
@@ -1,6 +1,6 @@
 /* from machine\pce.c */
-unsigned char *pce_user_ram; /* scratch RAM at F8 */
-unsigned char *pce_save_ram; /* battery backed RAM at F7 */
+extern unsigned char *pce_user_ram; /* scratch RAM at F8 */
+extern unsigned char *pce_save_ram; /* battery backed RAM at F7 */
 int pce_cart_load(int id, mame_file *fp, int open_mode);
 NVRAM_HANDLER( pce );
 WRITE_HANDLER ( pce_joystick_w );
