860ae68550ee7fb143ea2fd24f266a06b24ef9da
rnabet <rnabet>
2003-05-31 08:53:52 +0000

    Disabled the old MESS disk image format, and enabled the more common V9T9 image format instead.  This will enable the many third-party tools that handle V9T9 images to operate with MESS.

--- mess/machine/99_dsk.c
+++ mess/machine/99_dsk.c
@@ -9,8 +9,9 @@
 	  sectors per track.  (Not emulated)
 	* CorComp FDC.  Specs unknown, but it supports DD.  (Not emulated)
 	* SNUG BwG.  A nice DD disk controller.  Supports 18 sectors per track.
-	* Myarc HFDC.  Extremely elaborate, supports DD (a HD upgrade may have
-	  existed) and MFM harddisks.  (Preliminary emulation)
+	* Myarc HFDC.  Extremely elaborate, supports DD (and even HD, provided that
+	  a) the data separator is a 9216B and not a 9216, and b) that the OS
+	  overrides the hfdc DSR, whose HD support is buggy) and MFM harddisks.
 
 	An alternative was installing the hexbus controller (which was just a
 	prototype, and is not emulated) and connecting a hexbus floppy disk
@@ -42,6 +43,29 @@ static struct
 	UINT8 density;
 	/*mess_image *img;*/
 } floppy[MAX_FLOPPIES];
+
+/*
+	Convert physical sector address to offset in disk image
+*/
+static unsigned long ti99_calcoffset(UINT8 t, UINT8 h, UINT8 s,
+										UINT8 tracks, UINT8 heads, UINT8 sec_per_track, UINT16 sector_length, UINT8 first_sector_id, UINT16 offset_track_zero)
+{
+	unsigned long reply;
+
+#if 0
+	/* old MESS format */
+	reply = ((((unsigned long) t*heads) + h)*sec_per_track + s)*sector_length;
+#else
+	/* V9T9 format */
+	if (h & 1)
+		/* on side 1, tracks are stored in the reverse order */
+		reply = (((h*tracks) + (tracks-1 - t))*sec_per_track + s)*sector_length;
+	else
+		reply = (((h*tracks) + t)*sec_per_track + s)*sector_length;
+#endif
+
+	return reply;
+}
 
 DEVICE_LOAD( ti99_floppy )
 {
@@ -193,6 +217,7 @@ DEVICE_LOAD( ti99_floppy )
 	if (done && (device_load_basicdsk_floppy(image, file) == INIT_PASS))
 	{
 		basicdsk_set_geometry(image, floppy[id].tracksperside, floppy[id].sides, floppy[id].secspertrack, 256, 0, 0, use_80_track_drives && (floppy[id].density < 3));
+		basicdsk_set_calcoffset(image, ti99_calcoffset);
 
 		return INIT_PASS;
 	}
@@ -209,7 +234,7 @@ static void ti99_floppy_reset_geometries(void)
 	{
 		image = image_from_devtype_and_index(IO_FLOPPY, id);
 		if (image_exists(image))
-		basicdsk_set_geometry(image, floppy[id].tracksperside, floppy[id].sides, floppy[id].secspertrack, 256, 0, 0, use_80_track_drives && (floppy[id].density < 3));
+			basicdsk_set_geometry(image, floppy[id].tracksperside, floppy[id].sides, floppy[id].secspertrack, 256, 0, 0, use_80_track_drives && (floppy[id].density < 3));
 	}
 }
 
