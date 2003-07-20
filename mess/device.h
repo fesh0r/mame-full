13b01c328ad2d7bd119f5bfcc31db9a172a593e1
rnabet <rnabet>
2003-07-20 08:03:48 +0000

    Fixed a few typos.

--- mess/device.h
+++ mess/device.h
@@ -5,19 +5,19 @@
 
 enum
 {	/* List of all supported devices.  Refer to the device by these names only							*/
-	IO_CARTSLOT,	/*  0 - Cartidge Port, as found on most console and on some computers 				*/
+	IO_CARTSLOT,	/*  0 - Cartridge Port, as found on most console and on some computers 				*/
 	IO_FLOPPY,		/*  1 - Floppy Disk unit 															*/
 	IO_HARDDISK,	/*  2 - Hard Disk unit 																*/
 	IO_CYLINDER,	/*  3 - Magnetically-Coated Cylinder 												*/
 	IO_CASSETTE,	/*  4 - Cassette Recorder (common on early home computers) 							*/
-	IO_PUNCHCARD,	/*  5 - Card Puncher/Reader 															*/
+	IO_PUNCHCARD,	/*  5 - Card Puncher/Reader 														*/
 	IO_PUNCHTAPE,	/*  6 - Tape Puncher/Reader (reels instead of punchcards) 							*/
 	IO_PRINTER,		/*  7 - Printer device 																*/
 	IO_SERIAL,		/*  8 - some serial port 															*/
 	IO_PARALLEL,    /*  9 - Generic Parallel Port														*/
 	IO_SNAPSHOT,	/* 10 - Complete 'snapshot' of the state of the computer 							*/
 	IO_QUICKLOAD,	/* 11 - Allow to load program/data into memory, without matching any actual device	*/
-	IO_COUNT		/* 12 - Total Number of IO_devices for searching										*/
+	IO_COUNT		/* 12 - Total Number of IO_devices for searching									*/
 };
 
 /* Call this from the CLI to add a DEVICE (with its arg) to the options struct */
