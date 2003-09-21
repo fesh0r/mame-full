8feeb955d8729e3f810830a1dea0691f6b7914a2
l_g <l_g>
2003-09-21 17:30:58 +0000

    Just testing to see if I receive commit emails again.

--- src/unix/nec765_dummy.c
+++ src/unix/nec765_dummy.c
@@ -15,7 +15,8 @@ void osd_fdc_motors(int unit, int state)
 {
 }
 
-void osd_fdc_density(int unit, int density, int tracks, int spt, int eot, int secl)
+void osd_fdc_density(int unit, int density, int tracks, int spt, int eot, 
+		int secl)
 {
 }
 
@@ -27,11 +28,13 @@ void osd_fdc_format(int t, int h, int spt, UINT8 *fmt)
 {
 }
 
-void osd_fdc_put_sector(int unit, int side, int C, int H, int R, int N, UINT8 *buff, int ddma)
+void osd_fdc_put_sector(int unit, int side, int C, int H, int R, int N, 
+		UINT8 *buff, int ddma)
 {
 }
 
-void osd_fdc_get_sector(int unit, int side, int C, int H, int R, int N, UINT8 *buff, int ddma)
+void osd_fdc_get_sector(int unit, int side, int C, int H, int R, int N, 
+		UINT8 *buff, int ddma)
 {
 }
 
