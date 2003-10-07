c392ec5b2c05145ace1a35ee0d8f82b7410455e1
npwoods <npwoods>
2003-10-07 23:13:09 +0000

    Making sure that pool code is in...

---
+++ mess/pool.h
@@ -0,0 +1,38 @@
+#ifndef POOL_H
+#define POOL_H
+
+#include <stdlib.h>
+
+/***************************************************************************
+
+	Pool code
+
+***************************************************************************/
+
+typedef struct memory_pool_header *memory_pool;
+
+void pool_init(memory_pool *pool);
+void pool_exit(memory_pool *pool);
+void *pool_malloc(memory_pool *pool, size_t size);
+void *pool_realloc(memory_pool *pool, void *ptr, size_t size);
+char *pool_strdup(memory_pool *pool, const char *src);
+void pool_freeptr(memory_pool *pool, void *ptr);
+
+/***************************************************************************
+
+	Tagpool code
+
+***************************************************************************/
+
+typedef struct
+{
+	memory_pool mempool;
+	struct tag_pool_header *header;
+} tag_pool;
+
+void tagpool_init(tag_pool *tpool);
+void tagpool_exit(tag_pool *tpool);
+void *tagpool_alloc(tag_pool *tpool, const char *tag, size_t size);
+void *tagpool_lookup(tag_pool *tpool, const char *tag);
+
+#endif /* POOL_H */
