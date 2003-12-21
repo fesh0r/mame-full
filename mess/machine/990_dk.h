bc53623013b234967736aef86b2dcb5281c94676
rnabet <rnabet>
2003-12-21 14:07:00 +0000

    Implemented a FD800 emulation core for use with all 990 emulators (990/4 and 990/10).

---
+++ mess/machine/990_dk.h
@@ -0,0 +1,13 @@
+/*
+	990_dk.h: include file for 990_dk.c
+*/
+
+DEVICE_INIT( fd800 );
+DEVICE_LOAD( fd800 );
+/*DEVICE_UNLOAD( fd800 );*/
+
+void fd800_machine_init(void (*interrupt_callback)(int state));
+
+extern READ16_HANDLER(fd800_cru_r);
+extern WRITE16_HANDLER(fd800_cru_w);
+
