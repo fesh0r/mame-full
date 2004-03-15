39d07b4bdf64872bb61f0601474481c749a17851
npwoods <npwoods>
2004-03-15 04:54:46 +0000

    Fixed a duplicate pit8253_init() invocation

--- mess/machine/at.c
+++ mess/machine/at.c
@@ -46,7 +46,6 @@ void init_atcga(void)
 
 	init_pc_common(PCCOMMON_KEYBOARD_AT | PCCOMMON_DMA8237_AT);
 
-	pit8253_init(1);
 	pit8253_config(0, &at_pit8253_config);
 
 	pc_cga_init();
@@ -76,7 +75,6 @@ void init_at_vga(void)
 	init_pc_common(PCCOMMON_KEYBOARD_AT | PCCOMMON_DMA8237_AT);
 	pc_turbo_setup(0, 3, 0x02, 4.77/12, 1);
 
-	pit8253_init(1);
 	pit8253_config(0, &at_pit8253_config);
 
 	mc146818_init(MC146818_STANDARD);
