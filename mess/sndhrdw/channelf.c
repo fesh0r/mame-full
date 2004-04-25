a061032ffb3563583a9d60b979153ca5be548201
fmp <fmp>
2004-04-25 11:34:41 +0000

    Bug fix/Rewrite - sound HW now matches Channel F System 1 Still need additional code to fix PWM music on the System 2 (as in cart #24)

--- mess/sndhrdw/channelf.c
+++ mess/sndhrdw/channelf.c
@@ -1,33 +1,44 @@
 #include "driver.h"
 #include "includes/channelf.h"
 
+#include <math.h>
+
 static int channel;
 static int sound_mode;
-static int hrdwlim;
+static int incr;
+static float decay_mult;
+static int envelope;
+static UINT32 sample_counter = 0;
+
+const int max_amplitude = 0x7fff;
 
 void channelf_sh_update(int param, INT16 *buffer, int length);
-
-static int rate;
-static int period_2V;
-static int period_4V;
-static int period_8V;
-static int period_32V;
-static int half_2V;
-static int half_4V;
-static int half_8V;
-static int half_32V;
 
 void channelf_sound_w(int mode)
 {
 	if (mode == sound_mode)
 		return;
 
-	sound_mode = mode;
     stream_update(channel,0);
+	sound_mode = mode;
+
+    switch(mode)
+    {
+		case 0:
+			envelope = 0;
+			break;
+		case 1:
+		case 2:
+		case 3:
+			envelope = max_amplitude;
+			break;
+	}
 }
 
 static int channelf_sh_start(void)
 {
+	int rate;
+
 	channel = stream_init("Digital out", 50, Machine->sample_rate, 0, channelf_sh_update);
 	rate = Machine->sample_rate;
 
@@ -36,17 +47,30 @@ static int channelf_sh_start(void)
 	 * Note 2V on the schematic is not the 2V scanline counter -
 	 *      it is the 2V vertical pixel counter
 	 *      1 pixel = 4 scanlines high
+	 *
+     *
+	 * This is a convenient way to generate the relevant frequencies,
+	 * using a DDS (Direct Digital Synthesizer)
+	 *
+	 * Essentially, you want a counter to overflow some bit position
+	 * at a fixed rate.  So, you figure out a number which you can add
+	 * to the counter at every sample, so that you will achieve this
+	 *
+	 * In this case, we want to overflow bit 16 and the 2V rate, 1000Hz.
+	 * This means we also get bit 17 = 4V, bit 18 = 8V, etc.
 	 */
 
-	period_2V = rate/1000;
-	period_4V = rate/500;
-	period_8V = rate/250;
-	period_32V = rate/62.5;
-	half_2V = period_2V/2;
-	half_4V = period_4V/2;
-	half_8V = period_8V/2;
-	half_32V = period_32V/2;
-    return 0;
+	/* This is the proper value to add per sample */
+	incr = 65536.0/(rate/1000.0/2.0);
+
+	/* This was measured, decay envelope with half life of ~9ms */
+	/* (this is decay multiplier per sample) */
+	decay_mult = exp((-0.693/9e-3)/rate);
+
+	/* initial conditions */
+	envelope = 0;
+
+	return 0;
 }
 
 int channelf_sh_custom_start(const struct MachineSound* driver)
@@ -62,63 +86,38 @@ void channelf_sh_custom_update(void) {}
 
 void channelf_sh_update(int param, INT16 *buffer, int length)
 {
-	static INT16 max = 0x7fff;
-    static int incr = 0;
+	UINT32 mask, target;
 
 	INT16 *sample = buffer;
 
 	switch( sound_mode )
 	{
-	case 0: /* sound off */
-		hrdwlim=period_32V;  /* SKR - hardware only produces short tones */
-		while( length-- > 0 )
+		case 0: /* sound off */
+			memset(buffer,0,sizeof(INT16)*length);
+			return;
+
+		case 1: /* high tone (2V) - 1000Hz */
+			mask   = 0x00010000;
+			target = 0x00010000;
+			break;
+		case 2: /* medium tone (4V) - 500Hz */
+			mask   = 0x00020000;
+			target = 0x00020000;
+			break;
+		case 3: /* low (wierd) tone (32V & 8V) */
+			mask   = 0x00140000;
+			target = 0x00140000;
+			break;
+	}
+
+	while (length-- > 0)
+	{
+		if ((sample_counter & mask) == target)
+			*sample++ = envelope;
+		else
 			*sample++ = 0;
-			incr++;
-		break;
-	case 1: /* high tone (2V) - 1000Hz */
-		while( length-- > 0 )
-		{
-			if (((incr%period_2V) < half_2V) ||(hrdwlim<=0))
-			{
-				*sample++ = 0;
-			}
-			else
-			{
-				*sample++ = max;
-				hrdwlim--;
-			}
-			incr++;
-		}
-	case 2: /* medium tone (4V) - 500Hz */
-		while( length-- > 0 )
-		{
-			if (((incr%period_4V) < half_4V) || (hrdwlim<=0))
-			{
-				*sample++ = 0;
-			}
-			else
-			{
-				*sample++ = max;
-				hrdwlim--;
-			}
-			incr++;
-		}
-		break;
-	case 3: /* low (wierd) tone (32V & 8V) */
-		while( length-- > 0 )
-		{
-			if ((((incr%period_32V) >= half_32V) && ((incr%period_8V) >= half_8V)) && (hrdwlim>0))
-			{
-				*sample++ = max;
-				hrdwlim--;
-			}
-			else
-			{
-				*sample++ = 0x0;
-			}
-			incr++;
-		}
-		break;
+		sample_counter += incr;
+		envelope *= decay_mult;
 	}
 }
 
