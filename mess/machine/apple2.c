b5dd154df5cfd4b21123d6eb97fdc6dbdcf0bd7f
npwoods <npwoods>
2005-02-17 18:50:56 +0000

    [APPLE2] Doh...

--- mess/machine/apple2.c
+++ mess/machine/apple2.c
@@ -1062,7 +1062,7 @@ READ8_HANDLER ( apple2_c06x_r )
 
 READ8_HANDLER ( apple2_c07x_r )
 {
-	double calibration = TIME_IN_USEC(15.0);
+	double calibration = TIME_IN_USEC(16.0);
 
 	if (offset == 0)
 	{
