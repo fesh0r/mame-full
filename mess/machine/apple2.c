fd708fb008d10b3784db129a8397723ff52c558a
tkc <tkc>
2005-02-25 08:21:39 +0000

    Set joystick calibration down to 13usec since this seems to work best with both Drol and CarmenUSA.

--- mess/machine/apple2.c
+++ mess/machine/apple2.c
@@ -1067,7 +1067,7 @@ READ8_HANDLER ( apple2_c06x_r )
 
 READ8_HANDLER ( apple2_c07x_r )
 {
-	double calibration = TIME_IN_USEC(16.0);
+	double calibration = TIME_IN_USEC(13.0);
 
 	if (offset == 0)
 	{
