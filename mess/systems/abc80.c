b8b126bcc9408131a0914fb4b579060fb4bcc063
gargamel <gargamel>
2005-05-30 19:45:12 +0000

    Minor updates

--- mess/systems/abc80.c
+++ mess/systems/abc80.c
@@ -1,10 +1,7 @@
 /*
-
 	abc80.c
 
-
 	MESS Driver by Curt Coder
-
 
 	Luxor ABC 80
 	------------
@@ -13,7 +10,7 @@
 	CPU:			Z80 @ 3 MHz
 	ROM:			32 KB
 	RAM:			16 KB, 1 KB frame buffer
-	CRT:			74S264
+	CRTC:			74S264
 	Resolution:		240x240
 	Colors:			2
 
@@ -24,7 +21,7 @@
 	CPU:			Z80 @ 3 MHz
 	ROM:			32 KB
 	RAM:			16 KB, 1 KB frame buffer, 16 KB high-resolution videoram (800C/HR)
-	CRT:			6845
+	CRTC:			6845
 	Resolution:		240x240
 	Colors:			8
 
@@ -35,7 +32,7 @@
 	CPU:			Z80 @ 3 MHz
 	ROM:			32 KB
 	RAM:			16 KB, 2 KB frame buffer, 16 KB high-resolution videoram (800M/HR)
-	CRT:			6845
+	CRTC:			6845
 	Resolution:		480x240, 240x240 (HR)
 	Colors:			2
 
@@ -46,7 +43,7 @@
 	CPU:			Z80 @ 3 MHz
 	ROM:			32 KB
 	RAM:			16 KB, 2 KB frame buffer, 16 KB ram-floppy
-	CRT:			6845
+	CRTC:			6845
 	Resolution:		480x240
 	Colors:			2
 
@@ -57,21 +54,18 @@
 	CPU:			Z80 @ 3 MHz
 	ROM:			32 KB
 	RAM:			32 KB, 4 KB frame buffer, 128 KB scratch pad ram
-	CRT:			6845
+	CRTC:			6845
 	Resolution:		240x240, 256x240, 512x240
 	Colors:			8
 
-
 	http://www.devili.iki.fi/Computers/Luxor/
 	http://hem.passagen.se/mani/abc/
-
 */
 
 #include "driver.h"
 #include "vidhrdw/generic.h"
 #include "includes/crtc6845.h"
 #include "includes/centroni.h"
-#include "sound/sn76477.h"
 #include "devices/basicdsk.h"
 #include "devices/cassette.h"
 #include "devices/printer.h"
@@ -274,7 +268,7 @@ static WRITE8_HANDLER( ram_ctrl_w )
 {
 }
 
-// CTC
+/* CTC */
 
 static READ8_HANDLER( ctc_r )
 {
@@ -305,7 +299,21 @@ static READ8_HANDLER( io_reset_r )
 	return 0;
 }
 
-static WRITE8_HANDLER( sound_w )
+/* ABC 80 - Sound */
+
+/*
+  Bit Name     Description
+   0  SYSENA   1 On, 0 Off (inverted)
+   1  EXTVCO   00 High freq, 01 Low freq
+   2  VCOSEL   10 SLF cntrl, 11 SLF ctrl
+   3  MIXSELB  000 VCO, 001 Noise, 010 SLF
+   4  MIXSELA  011 VCO+Noise, 100 SLF+Noise, 101 SLF+VCO
+   5  MIXSELC  110 SLF+VCO+Noise, 111 Quiet
+   6  ENVSEL2  00 VCO, 01 Rakt igenom
+   7  ENVSEL1  10 Monovippa, 11 VCO alt.pol.
+*/
+
+static void abc80_sound_w(UINT8 data)
 {
 	// TODO: beep sound won't stop after it starts
 
@@ -327,7 +335,7 @@ static READ8_HANDLER( keyboard_r )
 	return 0;
 }
 
-// CRTC6845
+/* ABC 80x - CRTC 6845 */
 
 static READ8_HANDLER( crtc_r )
 {
@@ -349,14 +357,62 @@ static WRITE8_HANDLER( crtc_w )
 	}
 }
 
+/* ABC 80 - Keyboard */
+
+static const UINT8 abc80_keycodes[7][8] = {
+{0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38}, 
+{0x39, 0x30, 0x2B, 0x60, 0x3C, 0x71, 0x77, 0x65}, 
+{0x72, 0x74, 0x79, 0x75, 0x69, 0x6F, 0x70, 0x7D}, 
+{0x7E, 0x0D, 0x61, 0x73, 0x64, 0x66, 0x67, 0x68}, 
+{0x6A, 0x6B, 0x6C, 0x7C, 0x7B, 0x27, 0x08, 0x7A}, 
+{0x78, 0x63, 0x76, 0x62, 0x6E, 0x6D, 0x2C, 0x2E}, 
+{0x2D, 0x09, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00}
+};
+
+static void abc80_keyboard_scan(void)
+{
+	UINT8 keycode = 0;
+	UINT8 data;
+	int irow, icol;
+
+	for (irow = 0; irow < 7; irow++)
+	{
+		data = readinputport(irow);
+		if (data != 0)
+		{
+			UINT8 ibit = 1;
+			for (icol = 0; icol < 8; icol++)
+			{
+				if (data & ibit)
+					keycode = abc80_keycodes[irow][icol];
+				ibit <<= 1;
+			}			
+		}
+	}
+	
+	/* shift, upper case */
+//	if (readinputport(7) & 0x07)
+	
+	/* ctrl */
+//	if (readinputport(7) & 0x08)
+	
+	if (keycode != 0)
+		z80pio_d_w(0, 0, keycode | 0x80);
+}
+
 // PIO
 
-static READ8_HANDLER( pio_r )
+static UINT8 abc80_pio_r(UINT16 offset)
 {
 	switch (offset)
 	{
 	case 0:
-		return z80pio_d_r(0, 0);
+	{
+		UINT8 data;
+		data = z80pio_d_r(0, 0);
+//		z80pio_d_w(0, 0, data & ~0x80);
+		return data;
+	}
 	case 1:
 		return z80pio_c_r(0, 0);
 	case 2:
@@ -365,10 +421,10 @@ static READ8_HANDLER( pio_r )
 		return z80pio_c_r(0, 1);
 	}
 
-	return 0;
+	return 0xff;
 }
 
-static WRITE8_HANDLER( pio_w )
+static void abc80_pio_w(UINT16 offset, UINT8 data)
 {
 	switch (offset)
 	{
@@ -419,6 +475,17 @@ static WRITE8_HANDLER( sio2_w )
 }
 
 // DART
+
+/*
+typedef struct
+{
+	UINT8 latch;
+	UINT8 wreg[8];
+	UINT8 rreg[3];	
+} typDART;
+
+static typDART abc80_dart[2];
+*/
 
 static READ8_HANDLER( dart_r )
 {
@@ -476,23 +543,73 @@ static WRITE8_HANDLER( abc77_keyboard_status_w )
 {
 }
 
+static READ8_HANDLER( abc80_io_r )
+{
+	if (offset & 0x10)
+		return abc80_pio_r(offset & 0x03);
+
+	switch (offset)
+	{
+	case 0x00: /* ABC Bus - Data in */
+		break;
+	case 0x01: /* ABC Bus - Status in */
+		break;
+	case 0x07: /* ABC Bus - Reset */
+		break;
+	}
+	return 0xff;
+}
+
+static WRITE8_HANDLER( abc80_io_w )
+{
+	if (offset & 0x10)
+	{
+		abc80_pio_w(offset & 0x03, data);
+		return;
+	}
+
+	switch (offset)
+	{
+	case 0x00: /* ABC Bus - Data out */
+		break;
+	case 0x01: /* ABC Bus - Channel select */
+		break;
+	case 0x02: /* ABC Bus - Command C1 */
+		break;
+	case 0x03: /* ABC Bus - Command C2 */
+		break;
+	case 0x04: /* ABC Bus - Command C3 */
+		break;
+	case 0x05: /* ABC Bus - Command C4 */
+		break;
+	case 0x06: /* SN76477 Sound */
+		abc80_sound_w(data);
+		break;
+	}
+}
+
 /* Memory Maps */
 
 // ABC 80
 
 static ADDRESS_MAP_START( abc80_map, ADDRESS_SPACE_PROGRAM, 8 )
-	AM_RANGE(0x0000, 0x3fff) AM_ROM
-	AM_RANGE(0x604b, 0x604b) AM_READ(status_r) // ???
+	AM_RANGE(0x0000, 0x3fff) AM_ROM // ROM Basic
+//	AM_RANGE(0x6000, 0x6fff) AM_ROM // ROM Floppy
+//	AM_RANGE(0x7800, 0x7bff) AM_ROM // ROM Printer
 	AM_RANGE(0x7c00, 0x7fff) AM_RAM AM_WRITE(abc80_videoram_w) AM_BASE(&videoram)
-	AM_RANGE(0x8000, 0xbfff) AM_RAM	// Work RAM
-	AM_RANGE(0xc000, 0xffff) AM_RAM	// Expanded RAM
+	AM_RANGE(0x8000, 0xbfff) AM_RAM	// RAM Expanded
+	AM_RANGE(0xc000, 0xffff) AM_RAM	// RAM Internal
 ADDRESS_MAP_END
 
 static ADDRESS_MAP_START( abc80_io_map, ADDRESS_SPACE_IO, 8 )
+	AM_RANGE(0x00, 0xff) AM_WRITE(abc80_io_w)
+	AM_RANGE(0x00, 0xff) AM_READ(abc80_io_r)
+/*
 	AM_RANGE(0x06, 0x06) AM_WRITE(sound_w)
 	AM_RANGE(0x07, 0x07) AM_READ(io_reset_r)
 	AM_RANGE(0x38, 0x38) AM_READ(status_r)
 	AM_RANGE(0x38, 0x3b) AM_WRITE(pio_w)
+*/	
 ADDRESS_MAP_END
 
 // ABC 800
@@ -723,7 +840,78 @@ INPUT_PORTS_START( abc77 )
 INPUT_PORTS_END
 
 INPUT_PORTS_START( abc80 )
-	PORT_INCLUDE(abc77)
+ PORT_START /* 0 */
+  PORT_BIT (0x01, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("1 !") PORT_CODE(KEYCODE_1)
+  PORT_BIT (0x02, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("2 \"") PORT_CODE(KEYCODE_2)
+  PORT_BIT (0x04, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("3 #") PORT_CODE(KEYCODE_3)
+  PORT_BIT (0x08, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("4 ¤") PORT_CODE(KEYCODE_4)
+  PORT_BIT (0x10, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("5 %") PORT_CODE(KEYCODE_5)
+  PORT_BIT (0x20, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("6 &") PORT_CODE(KEYCODE_6)
+  PORT_BIT (0x40, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("7 /") PORT_CODE(KEYCODE_7)
+  PORT_BIT (0x80, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("8 (") PORT_CODE(KEYCODE_8)
+ PORT_START /* 1 */
+  PORT_BIT (0x01, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("9 )") PORT_CODE(KEYCODE_9)
+  PORT_BIT (0x02, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("0 =") PORT_CODE(KEYCODE_0)
+  PORT_BIT (0x04, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("+ ?") PORT_CODE(KEYCODE_MINUS)
+  PORT_BIT (0x08, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("é É") PORT_CODE(KEYCODE_EQUALS)
+  PORT_BIT (0x10, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("< >") PORT_CODE(KEYCODE_BACKSLASH)
+  PORT_BIT (0x20, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("q Q") PORT_CODE(KEYCODE_Q)
+  PORT_BIT (0x40, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("w W") PORT_CODE(KEYCODE_W)
+  PORT_BIT (0x80, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("e E") PORT_CODE(KEYCODE_E)
+ PORT_START /* 2 */
+  PORT_BIT (0x01, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("r R") PORT_CODE(KEYCODE_R)
+  PORT_BIT (0x02, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("t T") PORT_CODE(KEYCODE_T)
+  PORT_BIT (0x04, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("y Y") PORT_CODE(KEYCODE_Y)
+  PORT_BIT (0x08, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("u U") PORT_CODE(KEYCODE_U)
+  PORT_BIT (0x10, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("i I") PORT_CODE(KEYCODE_I)
+  PORT_BIT (0x20, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("o O") PORT_CODE(KEYCODE_O)
+  PORT_BIT (0x40, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("p P") PORT_CODE(KEYCODE_P)
+  PORT_BIT (0x80, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("å Å") PORT_CODE(KEYCODE_OPENBRACE)
+ PORT_START /* 3 */
+  PORT_BIT (0x01, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("ü Ü") PORT_CODE(KEYCODE_CLOSEBRACE)
+  PORT_BIT (0x02, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("RETURN") PORT_CODE(KEYCODE_ENTER)
+  PORT_BIT (0x04, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("a A") PORT_CODE(KEYCODE_A)
+  PORT_BIT (0x08, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("s S") PORT_CODE(KEYCODE_S)
+  PORT_BIT (0x10, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("d D") PORT_CODE(KEYCODE_D)
+  PORT_BIT (0x20, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("f F") PORT_CODE(KEYCODE_F)
+  PORT_BIT (0x40, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("g G") PORT_CODE(KEYCODE_G)
+  PORT_BIT (0x80, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("h H") PORT_CODE(KEYCODE_H)
+ PORT_START /* 4 */
+  PORT_BIT (0x01, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("j J") PORT_CODE(KEYCODE_J)
+  PORT_BIT (0x02, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("l L") PORT_CODE(KEYCODE_L)
+  PORT_BIT (0x04, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("ö Ö") PORT_CODE(KEYCODE_COLON)
+  PORT_BIT (0x08, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("ä Ä") PORT_CODE(KEYCODE_QUOTE)
+  PORT_BIT (0x10, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("' *") PORT_CODE(KEYCODE_BACKSLASH)
+  PORT_BIT (0x20, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("LEFT") PORT_CODE(KEYCODE_LEFT)
+  PORT_BIT (0x40, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("z Z") PORT_CODE(KEYCODE_Z)
+  PORT_BIT (0x80, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("x X") PORT_CODE(KEYCODE_X)
+ PORT_START /* 5 */
+  PORT_BIT (0x01, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("c C") PORT_CODE(KEYCODE_C)
+  PORT_BIT (0x02, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("v V") PORT_CODE(KEYCODE_V)
+  PORT_BIT (0x04, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("b B") PORT_CODE(KEYCODE_B)
+  PORT_BIT (0x08, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("n N") PORT_CODE(KEYCODE_N)
+  PORT_BIT (0x10, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("m M") PORT_CODE(KEYCODE_M)
+  PORT_BIT (0x20, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME(", ;") PORT_CODE(KEYCODE_COMMA)
+  PORT_BIT (0x40, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME(". :") PORT_CODE(KEYCODE_STOP)
+  PORT_BIT (0x80, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("- _") PORT_CODE(KEYCODE_SLASH)
+ PORT_START /* 6 */
+  PORT_BIT (0x01, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("RIGHT") PORT_CODE(KEYCODE_RIGHT)
+  PORT_BIT (0x02, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("SPACE") PORT_CODE(KEYCODE_SPACE)
+  PORT_BIT (0x04, IP_ACTIVE_HIGH, IPT_UNUSED)   
+  PORT_BIT (0x08, IP_ACTIVE_HIGH, IPT_UNUSED)   
+  PORT_BIT (0x10, IP_ACTIVE_HIGH, IPT_UNUSED)   
+  PORT_BIT (0x20, IP_ACTIVE_HIGH, IPT_UNUSED)   
+  PORT_BIT (0x40, IP_ACTIVE_HIGH, IPT_UNUSED)   
+  PORT_BIT (0x80, IP_ACTIVE_HIGH, IPT_UNUSED)   
+ PORT_START /* 7 */
+  PORT_BIT (0x01, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("LEFT SHIFT") PORT_CODE(KEYCODE_LSHIFT)
+  PORT_BIT (0x02, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("RIGHT SHIFT") PORT_CODE(KEYCODE_RSHIFT)
+  PORT_BIT (0x04, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("UPPER CASE") PORT_CODE(KEYCODE_CAPSLOCK)
+  PORT_BIT (0x08, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("CTRL") PORT_CODE(KEYCODE_LCONTROL)
+  PORT_BIT (0x10, IP_ACTIVE_HIGH, IPT_UNUSED)   
+  PORT_BIT (0x20, IP_ACTIVE_HIGH, IPT_UNUSED)   
+  PORT_BIT (0x40, IP_ACTIVE_HIGH, IPT_UNUSED)   
+  PORT_BIT (0x80, IP_ACTIVE_HIGH, IPT_UNUSED)   
 INPUT_PORTS_END
 
 INPUT_PORTS_START( abc800 )
@@ -790,33 +978,34 @@ static void abc80_pio_interrupt( int state )
 {
 	logerror("pio irq state: %02x\n",state);
 //	cpu_set_irq_line(0, 3, state);
+//	cpunum_set_input_line (0, 0, HOLD_LINE);
+
+	cpunum_set_input_line(0, 0, (state ? HOLD_LINE : CLEAR_LINE));
 //	cpu_set_irq_line(0, Z80_IRQ_STATE, state ? ASSERT_LINE : CLEAR_LINE);
 }
 
 /*
+	PIO Channel A
 
-	PIOA
+	0  R	Keyboard
+	1  R
+	2  R
+	3  R
+	4  R
+	5  R
+	6  R
+	7  R	Strobe
 
-	0	keyboard
-	1
-	2
-	3
-	4
-	5
-	6
-	7
-
-	PIOB
-
-	0	RxD
-	1	_CTS
-	2	_DCD
-	3	TxD
-	4	_RTS
-	5	Cassette Memory Control Relay
-	6	Cassette Memory 1
-	7	Cassette Memory 5
-
+	PIO Channel B
+	
+	0  R	RS-232C RxD
+	1  R	RS-232C _CTS
+	2  R	RS-232C _DCD
+	3  W	RS-232C TxD
+	4  W	RS-232C _RTS
+	5  W	Cassette Memory Control Relay
+	6  W	Cassette Memory 1
+	7  R	Cassette Memory 5
 */
 
 static z80pio_interface abc80_pio_interface = 
@@ -831,25 +1020,31 @@ static z80pio_interface abc80_pio_interface =
 
 static struct SN76477interface sn76477_interface =
 {
-	RES_K(47),		//	4  noise_res			R26 47k
-	RES_K(330),		//	5  filter_res			R24 330k
-	CAP_P(390),		//	6  filter_cap			C52 390
-	RES_K(47),		//	7  decay_res			R23	47k
-	CAP_U(10),		//	8  attack_decay_cap		C50 10u/35V
-	RES_K(22),		// 10  attack_res			R21	22k
+	RES_K(47),		//  4  noise_res		R26 47k
+	RES_K(330),		//  5  filter_res		R24 330k
+	CAP_P(390),		//  6  filter_cap		C52 390p
+	RES_K(47),		//  7  decay_res		R23 47k
+	CAP_U(10),		//  8  attack_decay_cap		C50 10u/35V
+	RES_K(2.2),		// 10  attack_res		R21 2.2k
 	RES_K(33),		// 11  amplitude_res		R19 33k
-	RES_K(10),		// 12  feedback_res			R18 10k
-	0,				// 16  vco_voltage			0V or 2.5V
-	CAP_N(10) ,		// 17  vco_cap				C48 10n
-	RES_K(100),		// 18  vco_res				R20 100k
-	0,				// 19  pitch_voltage		N/C
-	RES_K(220),		// 20  slf_res				R22 220k
-	CAP_U(1),		// 21  slf_cap				C51 1u/35V
-	CAP_U(0.1),		// 23  oneshot_cap			C53 0.1u
-	RES_K(330)		// 24  oneshot_res			R25 330k
+	RES_K(10),		// 12  feedback_res		R18 10k
+	0,			// 16  vco_voltage		0V or 2.5V
+	CAP_N(10) ,		// 17  vco_cap			C48 10n
+	RES_K(100),		// 18  vco_res			R20 100k
+	0,			// 19  pitch_voltage		N/C
+	RES_K(220),		// 20  slf_res			R22 220k
+	CAP_U(1),		// 21  slf_cap			C51 1u/35V
+	CAP_U(0.1),		// 23  oneshot_cap		C53 0.1u
+	RES_K(330)		// 24  oneshot_res		R25 330k
 };
 
 /* Interrupt Generators */
+
+INTERRUPT_GEN( abc80_nmi_interrupt )
+{
+	abc80_keyboard_scan();
+	cpunum_set_input_line(0, INPUT_LINE_NMI, PULSE_LINE);
+}
 
 /*static INTERRUPT_GEN( abc800_interrupt )
 {
@@ -878,7 +1073,8 @@ static MACHINE_DRIVER_START( abc80 )
 	MDRV_CPU_ADD(Z80, 11980800/2/2)	// 2.9952 MHz
 	MDRV_CPU_PROGRAM_MAP(abc80_map, 0)
 	MDRV_CPU_IO_MAP(abc80_io_map, 0)
-	MDRV_CPU_PERIODIC_INT(nmi_line_assert, 50)
+
+	MDRV_CPU_PERIODIC_INT(abc80_nmi_interrupt, 50)
 
 	MDRV_FRAMES_PER_SECOND(50)
 	MDRV_VBLANK_DURATION(DEFAULT_60HZ_VBLANK_DURATION)
@@ -961,14 +1157,45 @@ MACHINE_DRIVER_END
 
 /* Devices */
 
-DEVICE_LOAD( abc800_floppy )
+DEVICE_LOAD( abc80_floppy )
 {
+	int size, tracks, heads, sectors;
+
+	if (image_has_been_created(image))
+		return INIT_FAIL;
+
+	size = mame_fsize (file);
+	switch (size)
+	{
+	case 80*1024: /* Scandia Metric FD2 */
+		tracks = 40;
+		heads = 1;
+		sectors = 8;
+		break;
+	case 160*1024: /* ABC 830 */
+		tracks = 40;
+		heads = 1;
+		sectors = 16;
+		break;
+	case 640*1024: /* ABC 832/834 */
+		tracks = 80;
+		heads = 2;
+		sectors = 16;
+		break;
+	case 1001*1024: /* ABC 838 */
+		tracks = 77;
+		heads = 2;
+		sectors = 26;
+		break;
+	default:
+		return INIT_FAIL;
+	}
+
 	if (device_load_basicdsk_floppy(image, file)==INIT_PASS)
 	{
 		/* sector id's 0-9 */
 		/* drive, tracks, heads, sectors per track, sector length, dir_sector, dir_length, first sector id */
-		basicdsk_set_geometry(image, 80, 1, 10, 256, 0, 0, FALSE);
-
+		basicdsk_set_geometry(image, tracks, heads, sectors, 256, 0, 0, FALSE);
 		return INIT_PASS;
 	}
 
@@ -1068,11 +1295,39 @@ ROM_START( abc806 )
 	ROM_LOAD( "char.7c",  0x0000, 0x1000, CRC(b17c51c5) SHA1(e466e80ec989fbd522c89a67d274b8f0bed1ff72) )
 ROM_END
 
+static void abc80_printer_getinfo(struct IODevice *dev)
+{
+	/* printer */
+	printer_device_getinfo(dev);
+	dev->count = 1;
+}
+
+/*
+static void abc80_cassette_getinfo(struct IODevice *dev)
+{
+	// cassette
+	cassette_device_getinfo(dev, abc80_cassette_formats, NULL, (cassette_state) -1);
+	dev->count = 1;
+}
+*/
+
+static void abc80_floppy_getinfo(struct IODevice *dev)
+{
+	/* floppy */
+	legacybasicdsk_device_getinfo(dev);
+	dev->count = 2;
+	dev->file_extensions = "dsk\0";
+	dev->load = device_load_abc80_floppy;
+}
+
 /* System Configuration */
 
 SYSTEM_CONFIG_START( abc80 )
 	CONFIG_RAM_DEFAULT	( 16 * 1024)
-	CONFIG_RAM			( 32 * 1024)
+	CONFIG_RAM		( 32 * 1024)
+	CONFIG_DEVICE(abc80_printer_getinfo)
+//	CONFIG_DEVICE(abc80_cassette_getinfo)
+	CONFIG_DEVICE(abc80_floppy_getinfo)
 SYSTEM_CONFIG_END
 
 SYSTEM_CONFIG_START( abc800 )
@@ -1085,7 +1340,7 @@ SYSTEM_CONFIG_END
 
 SYSTEM_CONFIG_START( abc806 )
 	CONFIG_RAM_DEFAULT	(160 * 1024)
-	CONFIG_RAM			(544 * 1024)
+	CONFIG_RAM		(544 * 1024)
 SYSTEM_CONFIG_END
 
 /* Drivers */
