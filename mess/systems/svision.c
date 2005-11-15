2e2f7f2b196f24a52102f998bc0d99dd22f6b5f8
npwoods <npwoods>
2005-11-15 00:39:27 +0000

    [SVISION] Fixed 0.101u1 regression

--- mess/systems/svision.c
+++ mess/systems/svision.c
@@ -217,20 +217,13 @@ static WRITE8_HANDLER(svision_w)
 	}
 }
 
-static ADDRESS_MAP_START( readmem , ADDRESS_SPACE_PROGRAM, 8)
-    AM_RANGE( 0x0000, 0x1fff) AM_READ( MRA8_RAM )
-    AM_RANGE( 0x2000, 0x3fff) AM_READ( svision_r )
-    AM_RANGE( 0x4000, 0x5fff) AM_READ( MRA8_RAM ) //?
-	AM_RANGE( 0x6000, 0x7fff) AM_READ( MRA8_ROM )
-	AM_RANGE( 0x8000, 0xbfff) AM_READ( MRA8_BANK1 )
-	AM_RANGE( 0xc000, 0xffff) AM_READ( MRA8_BANK2 )
-ADDRESS_MAP_END
-
-static ADDRESS_MAP_START( writemem , ADDRESS_SPACE_PROGRAM, 8)
-	AM_RANGE( 0x0000, 0x1fff) AM_WRITE( MWA8_RAM )
-    AM_RANGE( 0x2000, 0x3fff) AM_WRITE( svision_w) AM_BASE( &svision_reg )
-	AM_RANGE( 0x4000, 0x5fff) AM_WRITE( MWA8_RAM )
-	AM_RANGE( 0x6000, 0xffff) AM_WRITE( MWA8_ROM )
+static ADDRESS_MAP_START( svision_mem , ADDRESS_SPACE_PROGRAM, 8)
+    AM_RANGE( 0x0000, 0x1fff) AM_RAM
+    AM_RANGE( 0x2000, 0x3fff) AM_READWRITE(svision_r, svision_w) AM_BASE(&svision_reg)
+    AM_RANGE( 0x4000, 0x5fff) AM_RAM AM_BASE(&videoram)
+	AM_RANGE( 0x6000, 0x7fff) AM_ROM
+	AM_RANGE( 0x8000, 0xbfff) AM_ROMBANK(1)
+	AM_RANGE( 0xc000, 0xffff) AM_ROMBANK(2)
 ADDRESS_MAP_END
 
 INPUT_PORTS_START( svision )
@@ -279,7 +272,7 @@ static PALETTE_INIT( svision )
 static VIDEO_UPDATE( svision )
 {
 	int x, y;
-	UINT8 *vram = memory_region(REGION_CPU1) + 0x4000 + (svision_reg[2] / 4);
+	UINT8 *vram = videoram + (svision_reg[2] / 4);
 	UINT8 *vram_line;
 	UINT16 *line;
 	UINT8 b;
@@ -326,7 +319,7 @@ struct CustomSound_interface svision_sound_interface =
 static MACHINE_DRIVER_START( svision )
 	/* basic machine hardware */
 	MDRV_CPU_ADD(M65C02, 4000000)        /* ? stz used! speed? */
-	MDRV_CPU_PROGRAM_MAP(readmem, writemem)
+	MDRV_CPU_PROGRAM_MAP(svision_mem, 0)
 	MDRV_CPU_VBLANK_INT(svision_frame_int, 1)
 	MDRV_FRAMES_PER_SECOND(60)
 	MDRV_VBLANK_DURATION(DEFAULT_REAL_60HZ_VBLANK_DURATION)
