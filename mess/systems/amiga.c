6a68c0a4a3ae3fd61f773b9535978257eae2c68f
npwoods <npwoods>
2006-04-18 00:10:12 +0000

    [AMIGA] Fixed 0.105u1 regression  [Dirk Best]

--- mess/systems/amiga.c
+++ mess/systems/amiga.c
@@ -10,12 +10,13 @@ ernesto@imagina.com
 
 #include "driver.h"
 #include "vidhrdw/generic.h"
+#include "sound/custom.h"
 #include "includes/amiga.h"
 #include "machine/amigafdc.h"
 #include "inputx.h"
 
 static ADDRESS_MAP_START(amiga_mem, ADDRESS_SPACE_PROGRAM, 16)
-    AM_RANGE( 0x000000, 0x07ffff) AM_RAMBANK(3)			/* Chip Ram - 1Mb / 512k */
+    AM_RANGE( 0x000000, 0x07ffff) AM_RAMBANK(1) AM_BASE(&amiga_chip_ram)		/* Chip Ram - 1Mb / 512k */
     AM_RANGE( 0xbfd000, 0xbfefff) AM_READWRITE( amiga_cia_r, amiga_cia_w )			/* 8510's CIA A and CIA B */
 //  { 0xc00000, 0xd7ffff, MRA8_BANK1 },          /* Internal Expansion Ram - 1.5 Mb */
     AM_RANGE( 0xdbf000, 0xdfffff) AM_READWRITE( amiga_custom_r, amiga_custom_w )	/* Custom Chips */
@@ -63,34 +64,40 @@ INPUT_PORTS_START( amiga )
     PORT_BIT( 0xff, 0x00, IPT_MOUSE_Y) PORT_SENSITIVITY(100) PORT_KEYDELTA(0) PORT_MINMAX(0,0) PORT_PLAYER(2)
 INPUT_PORTS_END
 
-static gfx_decode gfxdecodeinfo[] =
+static struct CustomSound_interface amiga_custom_interface =
 {
-    { -1 } /* end of array */
+	amiga_sh_start
 };
-
 
 static MACHINE_DRIVER_START( ntsc )
 	/* basic machine hardware */
 	MDRV_CPU_ADD( M68000, 7159090)        /* 7.15909 Mhz (NTSC) */
 	MDRV_CPU_PROGRAM_MAP(amiga_mem, 0)
 	MDRV_CPU_VBLANK_INT(amiga_irq, 262)
-	MDRV_FRAMES_PER_SECOND(60)
+	MDRV_FRAMES_PER_SECOND(59.997)
 	MDRV_VBLANK_DURATION(DEFAULT_60HZ_VBLANK_DURATION)
-	MDRV_INTERLEAVE(1)
 
 	MDRV_MACHINE_RESET( amiga )
 
     /* video hardware */
 	MDRV_VIDEO_ATTRIBUTES(VIDEO_TYPE_RASTER | VIDEO_UPDATE_BEFORE_VBLANK)
-	MDRV_SCREEN_SIZE(456, 262)
-	MDRV_VISIBLE_AREA(120, 456-1, 32, 262-1)
-	MDRV_GFXDECODE( gfxdecodeinfo )
+	MDRV_SCREEN_SIZE(512*2, 262)
+	MDRV_VISIBLE_AREA((129-8)*2, (449+8-1)*2, 44-8, 244+8-1)
 	MDRV_PALETTE_LENGTH(4096)
-	MDRV_COLORTABLE_LENGTH(4096)
 	MDRV_PALETTE_INIT( amiga )
 
 	MDRV_VIDEO_START( amiga )
 	MDRV_VIDEO_UPDATE( generic_bitmapped )
+
+	/* sound hardware */
+	MDRV_SPEAKER_STANDARD_STEREO("left", "right")
+
+	MDRV_SOUND_ADD(CUSTOM, 3579545)
+	MDRV_SOUND_CONFIG(amiga_custom_interface)
+	MDRV_SOUND_ROUTE(0, "left", 0.50)
+	MDRV_SOUND_ROUTE(1, "left", 0.50)
+	MDRV_SOUND_ROUTE(2, "right", 0.50)
+	MDRV_SOUND_ROUTE(3, "right", 0.50)
 MACHINE_DRIVER_END
 
 
@@ -107,21 +114,19 @@ static int amiga_cia_0_portA_r( void )
 	return ret; /* Gameport 1 and 0 buttons */
 }
 
-static void amiga_cia_0_portA_w( int data ) 
+static void amiga_cia_0_portA_w( int data )
 {
-	if ( (data & 1) == 1)
-	{
-		/* overlay enabled, map Amiga system ROM on 0x000000 */
-		memory_install_read16_handler(0, ADDRESS_SPACE_PROGRAM, 0x000000, 0x07ffff, 0, 0, MRA16_BANK3 );
-		memory_install_write16_handler(0, ADDRESS_SPACE_PROGRAM, 0x000000, 0x07ffff, 0, 0, MWA16_ROM );
-	}
-	else if ( ((data & 1) == 0))
-	{
+	/* switch banks as appropriate */
+	memory_set_bank(1, data & 1);
+
+	/* swap the write handlers between ROM and bank 1 based on the bit */
+	if ((data & 1) == 0)
 		/* overlay disabled, map RAM on 0x000000 */
-		memory_install_read16_handler(0, ADDRESS_SPACE_PROGRAM, 0x000000, 0x07ffff, 0, 0, MRA16_BANK4 );
-		memory_install_write16_handler(0, ADDRESS_SPACE_PROGRAM, 0x000000, 0x07ffff, 0, 0, MWA16_BANK4 );
-		memory_set_bankptr(4, memory_region(REGION_CPU1));
-	}
+		memory_install_write16_handler(0, ADDRESS_SPACE_PROGRAM, 0x000000, 0x07ffff, 0, 0, MWA16_BANK1);
+
+	else
+		/* overlay enabled, map Amiga system ROM on 0x000000 */
+		memory_install_write16_handler(0, ADDRESS_SPACE_PROGRAM, 0x000000, 0x07ffff, 0, 0, MWA16_ROM);
 
 	set_led_status( 0, ( data & 2 ) ? 0 : 1 ); /* bit 2 = Power Led on Amiga*/
 }
@@ -210,7 +215,8 @@ static DRIVER_INIT( amiga )
 	amiga_machine_config(&amiga_intf);
 
 	/* set up memory */
-	memory_set_bankptr(3, memory_region(REGION_USER1));
+	memory_configure_bank(1, 0, 1, amiga_chip_ram, 0);
+	memory_configure_bank(1, 1, 1, memory_region(REGION_USER1), 0);
 }
 
 /***************************************************************************
@@ -220,16 +226,12 @@ static DRIVER_INIT( amiga )
 ***************************************************************************/
 
 ROM_START( amiga )
-    ROM_REGION(0x200000, REGION_CPU1, 0) /* for ram, etc */
-
 	ROM_REGION(0x80000, REGION_USER1, 0)
 	ROM_LOAD16_WORD_SWAP ( "kick13.rom",  0x00000, 0x40000, CRC(c4f0f55f) SHA1(891e9a547772fe0c6c19b610baf8bc4ea7fcb785))
 	ROM_COPY( REGION_USER1, 0x00000, 0x40000, 0x40000 )
 ROM_END
 
 ROM_START( cdtv )
-    ROM_REGION(0x200000, REGION_CPU1, 0) /* for ram, etc */
-
 	ROM_REGION(0x80000, REGION_USER1, 0)
     ROM_LOAD16_WORD_SWAP ( "cdtv13.rom",  0x00000, 0x80000, CRC(42BAA124))
 ROM_END
