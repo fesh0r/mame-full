80a92998f41fabf225107a81264001316ad45204
wfp <wfp>
2006-04-19 18:53:25 +0000

    [WSwAN] Fix compile error

--- mess/systems/wswan.c
+++ mess/systems/wswan.c
@@ -127,7 +127,7 @@ static struct CustomSound_interface wswan_sound_interface =
 
 static MACHINE_DRIVER_START( wswan )
 	/* Basic machine hardware */
-	MDRV_CPU_ADD_TAG("main", V30MZ, 3072000)
+	MDRV_CPU_ADD_TAG("main", V30/*MZ*/, 3072000)
 	MDRV_CPU_PROGRAM_MAP(wswan_mem, 0)
 	MDRV_CPU_IO_MAP(wswan_io, 0)
 	MDRV_CPU_VBLANK_INT(wswan_scanline_interrupt, 160/*159?*/)	/* 1 int each scanline */
