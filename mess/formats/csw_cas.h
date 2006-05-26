0d0b161dcff8ec4fac4a9172952a7a96777350e5
gsjeff <gsjeff>
2006-05-26 17:47:14 +0000

    csw tape support

---
+++ mess/formats/csw_cas.h
@@ -0,0 +1,14 @@
+/*
+	csw_cas.h
+
+	Format code for csw cassette files
+*/
+
+#ifndef CSW_CAS_H
+#define CSW_CAS_H
+
+#include "formats/cassimg.h"
+
+CASSETTE_FORMATLIST_EXTERN(csw_cassette_formats);
+
+#endif /* CSW_CAS_H */
