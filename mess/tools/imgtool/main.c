33e113a2d5793ce2fe666e80039c0470d7cfe744
npwoods <npwoods>
2006-09-22 00:26:36 +0000

    Fixed the listfilters command.  (bug #945) [Tim Lindner]

--- mess/tools/imgtool/main.c
+++ mess/tools/imgtool/main.c
@@ -739,7 +739,7 @@ static int cmd_listfilters(const struct command *c, int argc, char *argv[])
 	{
 		fprintf(stdout, "  %-11s%s\n",
 			filter_get_info_string(filters[i], FILTINFO_STR_NAME),
-			filter_get_info_string(filters[i], FILTINFO_STR_NAME));
+			filter_get_info_string(filters[i], FILTINFO_STR_HUMANNAME));
 	}
 
 	return 0;
